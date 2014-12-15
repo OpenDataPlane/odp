98129ba55597c2b0faa18ac6fdb70a4ed20e8854
Maxim Uvarov <maxim.uvarov@linaro.org>
2014-12-15 14:22:54 +0300

    fix uninitialized buffer in pktin_dequeue
    CID 84870
    Coverity found uninitialized variable in case if
    classifier drops packets.
    
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>
    Reviewed-and-tested-by: Mike Holmes <mike.holmes@linaro.org>

--- platform/linux-generic/odp_packet_io.c
+++ platform/linux-generic/odp_packet_io.c
@@ -483,6 +483,9 @@ odp_buffer_hdr_t *pktin_dequeue(queue_entry_t *qentry)
 			tmp_hdr_tbl[j++] = buf_hdr;
 	}
 
+	if (0 == j)
+		return NULL;
+
 	if (j > 1)
 		queue_enq_multi(qentry, &tmp_hdr_tbl[1], j-1);
 	buf_hdr = tmp_hdr_tbl[0];
