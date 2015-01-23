c81d11c4bbd1d15f32298cc00d560f261878b93e
Anders Roxell <anders.roxell@linaro.org>
2015-01-23 21:30:27 +0100

    api: cpumask: doxygen: cpumask_first: use app language
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Reviewed-by: Robbie King <robking@cisco.com>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/api/odp_cpumask.h
+++ platform/linux-generic/include/api/odp_cpumask.h
@@ -132,7 +132,7 @@ int odp_cpumask_equal(const odp_cpumask_t *mask1,
 void odp_cpumask_copy(odp_cpumask_t *dest, const odp_cpumask_t *src);
 
 /**
- * Find first bit set in mask
+ * Find first CPU that is set in the mask
  *
  * @return cpu else -1 if no bits set in cpumask
  */
