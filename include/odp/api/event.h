90faa9548edee534dca2b4e94a64a2612e56e90e
Bill Fischofer <bill.fischofer@linaro.org>
2015-02-02 22:44:44 -0600

    api: events: add strong typing handle display function
    Add function odp_event_to_u64()
    
    Signed-off-by: Bill Fischofer <bill.fischofer@linaro.org>
    Reviewed-by: Petri Savolainen <petri.savolainen@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- include/odp/api/event.h
+++ include/odp/api/event.h
@@ -64,6 +64,18 @@ extern "C" {
  */
 int odp_event_type(odp_event_t event);
 
+/**
+ * Get printable value for an odp_event_t
+ *
+ * @param hdl  odp_event_t handle to be printed
+ * @return     uint64_t value that can be used to print/display this
+ *             handle
+ *
+ * @note This routine is intended to be used for diagnostic purposes
+ * to enable applications to generate a printable value that represents
+ * an odp_event_t handle.
+ */
+uint64_t odp_event_to_u64(odp_event_t hdl);
 
 /**
  * @}
