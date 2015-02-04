021a62f1d850aec64a3996601317c35cd7389291
Ola Liljedahl <ola.liljedahl@linaro.org>
2015-02-04 22:07:31 +0100

    api: odp_packet_io.h: clean up pktio parameter names and descriptions
    Use 'pktio' as the parameter name for all packet IO handles.
    Unify the descriptions to say "packet IO handle".
    
    Signed-off-by: Ola Liljedahl <ola.liljedahl@linaro.org>
    Reviewed-by: Petri Savolainen <petri.savolainen@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- include/odp/api/packet_io.h
+++ include/odp/api/packet_io.h
@@ -69,12 +69,12 @@ odp_pktio_t odp_pktio_open(const char *dev, odp_pool_t pool);
 /**
  * Close an ODP packet IO instance
  *
- * @param id  ODP packet IO handle
+ * @param pktio  ODP packet IO handle
  *
  * @retval 0 on success
  * @retval <0 on failure
  */
-int odp_pktio_close(odp_pktio_t id);
+int odp_pktio_close(odp_pktio_t pktio);
 
 /**
  * Return a packet IO handle for an already open device
@@ -89,144 +89,143 @@ odp_pktio_t odp_pktio_lookup(const char *dev);
 /**
  * Receive packets
  *
- * @param id          ODP packet IO handle
+ * @param pktio       ODP packet IO handle
  * @param pkt_table[] Storage for received packets (filled by function)
  * @param len         Length of pkt_table[], i.e. max number of pkts to receive
  *
  * @return Number of packets received
  * @retval <0 on failure
  */
-int odp_pktio_recv(odp_pktio_t id, odp_packet_t pkt_table[], int len);
+int odp_pktio_recv(odp_pktio_t pktio, odp_packet_t pkt_table[], int len);
 
 /**
  * Send packets
  *
- * @param id           ODP packet IO handle
+ * @param pktio        ODP packet IO handle
  * @param pkt_table[]  Array of packets to send
  * @param len          length of pkt_table[]
  *
  * @return Number of packets sent
  * @retval <0 on failure
  */
-int odp_pktio_send(odp_pktio_t id, odp_packet_t pkt_table[], int len);
+int odp_pktio_send(odp_pktio_t pktio, odp_packet_t pkt_table[], int len);
 
 /**
  * Set the default input queue to be associated with a pktio handle
  *
- * @param id 	ODP packet IO handle
- * @param queue default input queue set
+ * @param pktio		ODP packet IO handle
+ * @param queue		default input queue set
  * @retval  0 on success
  * @retval <0 on failure
  */
-int odp_pktio_inq_setdef(odp_pktio_t id, odp_queue_t queue);
+int odp_pktio_inq_setdef(odp_pktio_t pktio, odp_queue_t queue);
 
 /**
  * Get default input queue associated with a pktio handle
  *
- * @param id  ODP packet IO handle
+ * @param pktio  ODP packet IO handle
  *
  * @return Default input queue set
  * @retval ODP_QUEUE_INVALID on failure
  */
-odp_queue_t odp_pktio_inq_getdef(odp_pktio_t id);
+odp_queue_t odp_pktio_inq_getdef(odp_pktio_t pktio);
 
 /**
  * Remove default input queue (if set)
  *
- * @param id  ODP packet IO handle
+ * @param pktio  ODP packet IO handle
  *
  * @retval 0 on success
  * @retval <0 on failure
  */
-int odp_pktio_inq_remdef(odp_pktio_t id);
+int odp_pktio_inq_remdef(odp_pktio_t pktio);
 
 /**
  * Query default output queue
  *
- * @param id ODP packet IO handle
+ * @param pktio ODP packet IO handle
  *
  * @return Default out queue
  * @retval ODP_QUEUE_INVALID on failure
  */
-odp_queue_t odp_pktio_outq_getdef(odp_pktio_t id);
+odp_queue_t odp_pktio_outq_getdef(odp_pktio_t pktio);
 
 /**
  * Return the currently configured MTU value of a packet IO interface.
  *
- * @param[in] id  ODP packet IO handle.
+ * @param[in] pktio  ODP packet IO handle.
  *
  * @return MTU value on success
  * @retval <0 on failure
  */
-int odp_pktio_mtu(odp_pktio_t id);
+int odp_pktio_mtu(odp_pktio_t pktio);
 
 /**
  * Enable/Disable promiscuous mode on a packet IO interface.
  *
- * @param[in] id	ODP packet IO handle.
+ * @param[in] pktio	ODP packet IO handle.
  * @param[in] enable	1 to enable, 0 to disable.
  *
  * @retval 0 on success
  * @retval <0 on failure
  */
-int odp_pktio_promisc_mode_set(odp_pktio_t id, odp_bool_t enable);
+int odp_pktio_promisc_mode_set(odp_pktio_t pktio, odp_bool_t enable);
 
 /**
  * Determine if promiscuous mode is enabled for a packet IO interface.
  *
- * @param[in] id ODP packet IO handle.
+ * @param[in] pktio ODP packet IO handle.
  *
  * @retval  1 if promiscuous mode is enabled.
  * @retval  0 if promiscuous mode is disabled.
  * @retval <0 on failure
 */
-int odp_pktio_promisc_mode(odp_pktio_t id);
+int odp_pktio_promisc_mode(odp_pktio_t pktio);
 
 /**
  * Get the default MAC address of a packet IO interface.
  *
- * @param	id        ODP packet IO handle
+ * @param	pktio     ODP packet IO handle
  * @param[out]	mac_addr  Output buffer (use ODP_PKTIO_MACADDR_MAXSIZE)
  * @param       size Size of output buffer
  *
  * @return Number of bytes written (actual size of MAC address)
  * @retval <0 on failure
  */
-ssize_t odp_pktio_mac_addr(odp_pktio_t id, void *mac_addr, ssize_t size);
+ssize_t odp_pktio_mac_addr(odp_pktio_t pktio, void *mac_addr, ssize_t size);
 
 /**
  * Setup per-port default class-of-service.
  *
- * @param[in]	pktio_in	Ingress port identifier.
+ * @param[in]	pktio		Ingress port pktio handle.
  * @param[in]	default_cos	Class-of-service set to all packets arriving
- *				at the pktio_in ingress port,
+ *				at this ingress port,
  *				unless overridden by subsequent
  *				header-based filters.
  *
  * @retval			0 on success
  * @retval			<0 on failure
  */
-int odp_pktio_default_cos_set(odp_pktio_t pktio_in, odp_cos_t default_cos);
+int odp_pktio_default_cos_set(odp_pktio_t pktio, odp_cos_t default_cos);
 
 /**
  * Setup per-port error class-of-service
  *
- * @param[in]	pktio_in	Ingress port identifier.
+ * @param[in]	pktio		Ingress port pktio handle.
  * @param[in]	error_cos	class-of-service set to all packets arriving
- *				at the pktio_in ingress port
- *				that contain an error.
+ *				at this ingress port that contain an error.
  *
  * @retval			0 on success
  * @retval			<0 on failure
  *
  * @note Optional.
  */
-int odp_pktio_error_cos_set(odp_pktio_t pktio_in, odp_cos_t error_cos);
+int odp_pktio_error_cos_set(odp_pktio_t pktio, odp_cos_t error_cos);
 
 /**
  * Setup per-port header offset
  *
- * @param[in]	pktio_in	Ingress port identifier.
+ * @param[in]	pktio		Ingress port pktio handle.
  * @param[in]	offset		Number of bytes the classifier must skip.
  *
  * @retval			0 on success
@@ -234,12 +233,12 @@ int odp_pktio_error_cos_set(odp_pktio_t pktio_in, odp_cos_t error_cos);
  * @note  Optional.
  *
  */
-int odp_pktio_skip_set(odp_pktio_t pktio_in, uint32_t offset);
+int odp_pktio_skip_set(odp_pktio_t pktio, uint32_t offset);
 
 /**
  * Specify per-port buffer headroom
  *
- * @param[in]	pktio_in	Ingress port identifier.
+ * @param[in]	pktio		Ingress port pktio handle.
  * @param[in]	headroom	Number of bytes of space preceding
  *				packet data to reserve for use as headroom.
  *				Must not exceed the implementation
@@ -250,12 +249,12 @@ int odp_pktio_skip_set(odp_pktio_t pktio_in, uint32_t offset);
  *
  * @note Optional.
  */
-int odp_pktio_headroom_set(odp_pktio_t pktio_in, uint32_t headroom);
+int odp_pktio_headroom_set(odp_pktio_t pktio, uint32_t headroom);
 
 /**
  * Get printable value for an odp_pktio_t
  *
- * @param id   odp_pktio_t handle to be printed
+ * @param pktio   odp_pktio_t handle to be printed
  * @return     uint64_t value that can be used to print/display this
  *             handle
  *
@@ -263,7 +262,7 @@ int odp_pktio_headroom_set(odp_pktio_t pktio_in, uint32_t headroom);
  * to enable applications to generate a printable value that represents
  * an odp_pktio_t handle.
  */
-uint64_t odp_pktio_to_u64(odp_pktio_t id);
+uint64_t odp_pktio_to_u64(odp_pktio_t pktio);
 
 /**
  * @}
