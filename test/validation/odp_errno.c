7ad4a20880ccef1fb6595d732863c6bb6461e7c4
Mike Holmes <mike.holmes@linaro.org>
2015-02-26 16:34:12 -0500

    valididation: errno: add sunnyday test
    Call the public APIs for errno in a sunny day configuration.
    
    Reviewed-and-tested-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Mike Holmes <mike.holmes@linaro.org>

---
+++ test/validation/odp_errno.c
@@ -0,0 +1,28 @@
+/* Copyright (c) 2015, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include <odp.h>
+#include "odp_cunit_common.h"
+
+static void test_odp_errno_sunny_day(void)
+{
+	int my_errno;
+	odp_errno_zero();
+	my_errno = odp_errno();
+	CU_ASSERT_TRUE(my_errno == 0);
+	odp_errno_print("odp_errno");
+	CU_ASSERT_PTR_NOT_NULL(odp_errno_str(my_errno));
+}
+
+CU_TestInfo test_odp_errno[] = {
+	{"sunny day", test_odp_errno_sunny_day},
+	CU_TEST_INFO_NULL,
+};
+
+CU_SuiteInfo odp_testsuites[] = {
+	{"Errno", NULL, NULL, NULL, NULL, test_odp_errno},
+	CU_SUITE_INFO_NULL,
+};
