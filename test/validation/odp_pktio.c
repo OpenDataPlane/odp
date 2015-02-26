fdac7327486c4791f663f2ca56eac30aec59cb82
Taras Kondratiuk <taras.kondratiuk@linaro.org>
2015-02-26 14:37:19 +0200

    validation: pktio: fix odp_errno usage
    __odp_errno is a private variable.
    odp_errno() function should be used instead.
    
    Also, until we have defined the set of standard ODP errno values,
    just verify that odp_errno contains 'some' error.
    
    Signed-off-by: Taras Kondratiuk <taras.kondratiuk@linaro.org>
    Reviewed-by: Petri Savolainen <petri.savolainen@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/validation/odp_pktio.c
+++ test/validation/odp_pktio.c
@@ -568,7 +568,7 @@ static void test_odp_pktio_lookup(void)
 	CU_ASSERT(odp_pktio_lookup(iface_name[0]) == pktio);
 
 	pktio_inval = odp_pktio_open(iface_name[0], default_pkt_pool);
-	CU_ASSERT(__odp_errno == EEXIST);
+	CU_ASSERT(odp_errno() != 0);
 	CU_ASSERT(pktio_inval == ODP_PKTIO_INVALID);
 
 	CU_ASSERT(odp_pktio_close(pktio) == 0);
