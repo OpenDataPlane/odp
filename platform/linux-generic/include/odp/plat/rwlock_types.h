77a550918cfe6a5cc50c4ccd6111921578ae442f
Jerin Jacob <jerin.jacob@caviumnetworks.com>
2015-03-18 19:41:34 +0530

    rwlock: allow platform to override odp_rwlock_t
    Signed-off-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>
    Reviewed-and-tested-by: Bill Fischofer <bill.fischofer@linaro.org>
    Reviewed-by: Bala Manoharan<bala.manoharan@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ platform/linux-generic/include/odp/plat/rwlock_types.h
@@ -0,0 +1,48 @@
+/* Copyright (c) 2015, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+
+/**
+ * @file
+ *
+ * ODP rwlock
+ */
+
+#ifndef ODP_RWLOCK_TYPES_H_
+#define ODP_RWLOCK_TYPES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <odp/atomic.h>
+
+/**
+ * @internal
+ * ODP rwlock
+ */
+struct odp_rwlock_s {
+	odp_atomic_u32_t cnt; /**< lock count
+				0 lock not taken
+				-1 write lock taken
+				>0 read lock(s) taken */
+};
+
+/** @addtogroup odp_synchronizers
+ *  @{
+ */
+
+typedef struct odp_rwlock_s odp_rwlock_t;
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
