be57a997211c1cef9a3c10cb2e9dd9754aa9dc6b
Mike Holmes <mike.holmes@linaro.org>
2015-04-06 15:34:59 -0400

    linux-generic: use arch optimisations for time
    Switch to using configure time optimizations from compile time
    
    Signed-off-by: Mike Holmes <mike.holmes@linaro.org>
    Reviewed-by: Anders Roxell <anders.roxell@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ platform/linux-generic/arch/linux/odp_time.c
@@ -0,0 +1,38 @@
+/* Copyright (c) 2015, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#define _POSIX_C_SOURCE 199309L
+
+#include <stdlib.h>
+#include <time.h>
+
+#include <odp/time.h>
+#include <odp/hints.h>
+#include <odp/system_info.h>
+#include <odp_debug_internal.h>
+
+#define GIGA 1000000000
+
+uint64_t odp_time_cycles(void)
+{
+	struct timespec time;
+	uint64_t sec, ns, hz, cycles;
+	int ret;
+
+	ret = clock_gettime(CLOCK_MONOTONIC_RAW, &time);
+
+	if (ret != 0)
+		ODP_ABORT("clock_gettime failed\n");
+
+	hz  = odp_sys_cpu_hz();
+	sec = (uint64_t) time.tv_sec;
+	ns  = (uint64_t) time.tv_nsec;
+
+	cycles  = sec * hz;
+	cycles += (ns * hz) / GIGA;
+
+	return cycles;
+}
