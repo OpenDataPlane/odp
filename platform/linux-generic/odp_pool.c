64fd29e46803a0a391d3abcc68ea3d1c99ecf994
Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
2015-04-22 10:01:32 +0200

    linux-generic: pool: fix initialization of odp_buffer_hdr ref_count
    Signed-off-by: Nicolas Morey-Chaisemartin <nmorey@kalray.eu>
    Reviewed-and-tested-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/odp_pool.c
+++ platform/linux-generic/odp_pool.c
@@ -359,7 +359,7 @@ odp_pool_t odp_pool_create(const char *name,
 			tmp->flags.all = 0;
 			tmp->flags.zeroized = zeroized;
 			tmp->size = 0;
-			odp_atomic_store_u32(&tmp->ref_count, 0);
+			odp_atomic_init_u32(&tmp->ref_count, 0);
 			tmp->type = params->type;
 			tmp->pool_hdl = pool->s.pool_hdl;
 			tmp->udata_addr = (void *)udat;
