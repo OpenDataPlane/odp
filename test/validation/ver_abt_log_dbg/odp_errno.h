9b4b8ffa30ff7077ebc6012667f35b6078c0589c
Christophe Milard <christophe.milard@linaro.org>
2015-05-05 17:49:08 +0200

    validation: tests for module odp_ver_abt_log_dbg
    Created directory for module odp_ver_abt_log_dbg, populated
    with file odp_errno.c and odp_system.c which, despite its
    confusing name is not testing the system module but the sys*
    function of the odp_ver_abt_log_dbg module.
    
    Signed-off-by: Christophe Milard <christophe.milard@linaro.org>
    Reviewed-and-tested-by: Mike Holmes <mike.holmes@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ test/validation/ver_abt_log_dbg/odp_errno.h
@@ -0,0 +1,9 @@
+/* Copyright (c) 2015, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:	BSD-3-Clause
+ */
+
+#include "odp_cunit_common.h"
+
+extern CU_TestInfo test_odp_errno[];
