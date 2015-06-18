bb29f344f51ee74bccf2c0c8d6745b62d1ebc578
Christophe Milard <christophe.milard@linaro.org>
2015-06-18 18:18:28 +0200

    validation: own main in odp_classification.c
    Signed-off-by: Christophe Milard <christophe.milard@linaro.org>
    Reviewed-by: Stuart Haslam <stuart.haslam@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/validation/odp_classification.c
+++ test/validation/odp_classification.c
@@ -4,9 +4,11 @@
  * SPDX-License-Identifier:	BSD-3-Clause
  */
 
+#include <odp.h>
+#include <odp_cunit_common.h>
 #include "odp_classification_testsuites.h"
 
-CU_SuiteInfo odp_testsuites[] = {
+static CU_SuiteInfo classification_suites[] = {
 	{ .pName = "classification basic",
 			.pTests = classification_basic,
 	},
@@ -17,3 +19,14 @@ CU_SuiteInfo odp_testsuites[] = {
 	},
 	CU_SUITE_INFO_NULL,
 };
+
+static int classification_main(void)
+{
+	return odp_cunit_run(classification_suites);
+}
+
+/* the following main function will be separated when lib is created */
+int main(void)
+{
+	return classification_main();
+}
