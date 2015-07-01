a3385d8655d7eca1b1628cda0c6bacb3319b5126
Christophe Milard <christophe.milard@linaro.org>
2015-07-01 10:19:29 +0200

    validation: own main in odp_thread.c
    Signed-off-by: Christophe Milard <christophe.milard@linaro.org>
    Reviewed-by: Stuart Haslam <stuart.haslam@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/validation/odp_thread.c
+++ test/validation/odp_thread.c
@@ -28,14 +28,25 @@ static void test_odp_thread_count(void)
 	CU_PASS();
 }
 
-CU_TestInfo test_odp_thread[] = {
+static CU_TestInfo test_odp_thread[] = {
 	_CU_TEST_INFO(test_odp_cpu_id),
 	_CU_TEST_INFO(test_odp_thread_id),
 	_CU_TEST_INFO(test_odp_thread_count),
 	CU_TEST_INFO_NULL,
 };
 
-CU_SuiteInfo odp_testsuites[] = {
+static CU_SuiteInfo thread_suites[] = {
 	{"thread", NULL, NULL, NULL, NULL, test_odp_thread},
 	CU_SUITE_INFO_NULL,
 };
+
+static int thread_main(void)
+{
+	return odp_cunit_run(thread_suites);
+}
+
+/* the following main function will be separated when lib is created */
+int main(void)
+{
+	return thread_main();
+}
