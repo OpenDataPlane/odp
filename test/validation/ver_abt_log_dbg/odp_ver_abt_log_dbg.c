40503bd111383a0da4e78d42bf93200cc70f9c19
Christophe Milard <christophe.milard@linaro.org>
2015-07-02 15:06:24 +0200

    validation: removed errno from odp_ver_abt_log_dbg
    The tests for errno are removed from module odp_ver_abt_log_dbg.
    Errno tests will get their own module in next patches
    
    Signed-off-by: Christophe Milard <christophe.milard@linaro.org>
    Reviewed-by: Stuart Haslam <stuart.haslam@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/validation/ver_abt_log_dbg/odp_ver_abt_log_dbg.c
+++ test/validation/ver_abt_log_dbg/odp_ver_abt_log_dbg.c
@@ -6,11 +6,9 @@
 
 #include <odp.h>
 #include "odp_cunit_common.h"
-#include "odp_errno.h"
 #include "odp_system.h"
 
 CU_SuiteInfo odp_testsuites[] = {
-	{"Errno",	NULL, NULL, NULL, NULL, test_odp_errno},
 	{"System Info", NULL, NULL, NULL, NULL, test_odp_system},
 	CU_SUITE_INFO_NULL,
 };
