0303d31c4698641390d1179ad9f891b551e85166
Christophe Milard <christophe.milard@linaro.org>
2015-07-27 19:38:46 +0200

    validation: thread: lib populated
    test symbols are now exported in the module lib.
    
    Signed-off-by: Christophe Milard <christophe.milard@linaro.org>
    Reviewed-by: Stuart Haslam <stuart.haslam@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/validation/thread/thread.h
+++ test/validation/thread/thread.h
@@ -7,6 +7,27 @@
 #ifndef _ODP_TEST_THREAD_H_
 #define _ODP_TEST_THREAD_H_
 
+#include <odp.h>
+#include <CUnit/Basic.h>
+
+/* test functions: */
+#ifndef TEST_THRMASK
+#define TEST_THRMASK
+#endif
+#include "mask_common.h"
+void thread_test_odp_cpu_id(void);
+void thread_test_odp_thread_id(void);
+void thread_test_odp_thread_count(void);
+void thread_test_odp_thrmask_control(void);
+void thread_test_odp_thrmask_worker(void);
+
+/* test arrays: */
+extern CU_TestInfo thread_suite[];
+
+/* test registry: */
+extern CU_SuiteInfo thread_suites[];
+
+/* main test program: */
 int thread_main(void);
 
 #endif
