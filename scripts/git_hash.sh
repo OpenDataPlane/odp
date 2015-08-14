06537738ea438c3e339fc269dedb4ed6c5e48f07
Anders Roxell <anders.roxell@linaro.org>
2015-08-14 15:25:05 +0200

    scripts/git_hash: fix build from tar source
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Reviewed-and-tested-by: Mike Holmes <mike.holmes@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/git_hash.sh
+++ scripts/git_hash.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+if [ -z ${1} ]; then
+	echo "should be called with a path"
+	exit
+fi
+ROOTDIR=${1}
+
 repo=https://git.linaro.org/lng/odp.git
-hash=$(git describe | tr -d "\n")
-if git diff-index --name-only HEAD &>/dev/null ; then
-	dirty=-dirty
+if [ -d ${ROOTDIR}/.git ]; then
+	hash=$(git describe | tr -d "\n")
+	if git diff-index --name-only HEAD &>/dev/null ; then
+		dirty=-dirty
+	fi
+
+	echo -n "'${repo}' (${hash}${dirty})">${ROOTDIR}/.scmversion
 fi
 
-echo -n "'${repo}' (${hash}${dirty})"
+cat ${ROOTDIR}/.scmversion
