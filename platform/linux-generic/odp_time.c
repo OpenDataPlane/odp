fe67c25f71a1c327eecf852f33e4abb78fd47ec7
Ivan Khoronzhuk <ivan.khoronzhuk@linaro.org>
2015-10-05 15:58:12 +0300

    linux-generic: use cycles_diff for time API also
    Currently, time API reuses cpu API to get ticks, but uses it's own
    function to count diff but it can differ from cpu diff function.
    So, better to use the same function in order to eliminate possible
    difference in counting.
    
    Signed-off-by: Ivan Khoronzhuk <ivan.khoronzhuk@linaro.org>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/odp_time.c
+++ platform/linux-generic/odp_time.c
@@ -10,6 +10,7 @@
 #include <odp/hints.h>
 #include <odp/system_info.h>
 #include <odp/cpu.h>
+#include <odp_cpu_internal.h>
 
 #define GIGA 1000000000
 
@@ -20,10 +21,7 @@ uint64_t odp_time_cycles(void)
 
 uint64_t odp_time_diff_cycles(uint64_t t1, uint64_t t2)
 {
-	if (odp_likely(t2 >= t1))
-		return t2 - t1;
-
-	return t2 + (UINT64_MAX - t1);
+	return _odp_cpu_cycles_diff(t1, t2);
 }
 
 uint64_t odp_time_cycles_to_ns(uint64_t cycles)
