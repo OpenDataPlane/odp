091df80a7454ef4f4bb71e1315d43d9d3389aa3c
Anders Roxell <anders.roxell@linaro.org>
2015-11-10 22:32:11 +0100

    scripts: cleanup git_hash and use it instead of odp_version.
    This makes the version depend on git describe so there will be new deb
    packages names.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/git_hash.sh
+++ scripts/git_hash.sh
@@ -6,15 +6,23 @@ if [ -z ${1} ]; then
 fi
 ROOTDIR=${1}
 
-CUSTOM_STR=${CUSTOM_STR:-https://git.linaro.org/lng/odp.git}
 if [ -d ${ROOTDIR}/.git ]; then
 	hash=$(git --git-dir=${ROOTDIR}/.git describe | tr -d "\n")
 	if [[ $(git --git-dir=${ROOTDIR}/.git diff --shortstat 2> /dev/null \
 		| tail -n1) != "" ]]; then
-		dirty=-dirty
+		dirty=.dirty
 	fi
 
-	echo -n "'${CUSTOM_STR}' (${hash}${dirty})">${ROOTDIR}/.scmversion
+	echo -n "${hash}${dirty}">${ROOTDIR}/.scmversion
+
+	sed -i "s|-|.git|" ${ROOTDIR}/.scmversion
+	sed -i "s|-|.|g" ${ROOTDIR}/.scmversion
+	sed -i "s|^v||g" ${ROOTDIR}/.scmversion
+elif [ ! -d ${ROOTDIR}/.git -a ! -f ${ROOTDIR}/.scmversion ]; then
+	echo -n "File ROOTDIR/.scmversion not found, "
+	echo "and not inside a git repository"
+	echo "Bailing out! Not recoverable!"
+	exit 1
 fi
 
 cat ${ROOTDIR}/.scmversion
