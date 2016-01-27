a6ec6b79ce11c89972f95447b0dc4669446ef7f1
Hongbo Zhang <hongbo.zhang@linaro.org>
2016-01-27 16:56:07 +0800

    linux-generic: sysinfo: move MIPS system info codes to its plarform file
    This patch moves the MIPS system info codes into the newly added MIPS
    specific platform file.
    
    Signed-off-by: Hongbo Zhang <hongbo.zhang@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/odp_system_info.c
+++ platform/linux-generic/odp_system_info.c
@@ -122,53 +122,6 @@ static uint64_t odp_cpu_hz_current(int id ODP_UNUSED)
 	return -1;
 }
 
-#elif defined __OCTEON__
-
-static int odp_cpuinfo_parser(FILE *file, odp_system_info_t *sysinfo)
-{
-	char str[1024];
-	char *pos;
-	double mhz = 0.0;
-	int model = 0;
-	int count = 2;
-
-	while (fgets(str, sizeof(str), file) != NULL && count > 0) {
-		if (!mhz) {
-			pos = strstr(str, "BogoMIPS");
-
-			if (pos) {
-				sscanf(pos, "BogoMIPS : %lf", &mhz);
-				count--;
-			}
-		}
-
-		if (!model) {
-			pos = strstr(str, "cpu model");
-
-			if (pos) {
-				int len;
-				pos = strchr(str, ':');
-				strncpy(sysinfo->model_str[0], pos + 2,
-					sizeof(sysinfo->model_str[0]));
-				len = strlen(sysinfo->model_str[0]);
-				sysinfo->model_str[0][len - 1] = 0;
-				model = 1;
-				count--;
-			}
-		}
-	}
-
-	/* bogomips seems to be 2x freq */
-	sysinfo->cpu_hz[0] = (uint64_t)(mhz * 1000000.0 / 2.0);
-
-	return 0;
-}
-
-static uint64_t odp_cpu_hz_current(int id ODP_UNUSED)
-{
-	return -1;
-}
-
 #elif defined __powerpc__
 static int odp_cpuinfo_parser(FILE *file, odp_system_info_t *sysinfo)
 {
