fc051d3b1a9c1f060769583b42a7e311b775d30b
Petri Savolainen <petri.savolainen@nokia.com>
2016-01-29 13:25:39 +0200

    api: atomic: init functions are not atomic
    Highlight that application must ensure that there's no
    race while calling init functions. Also lock free call
    will never set op.init flag, since init call will not need
    to implement locking or other (lock free) synchronization.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-and-tested-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/odp_atomic.c
+++ platform/linux-generic/odp_atomic.c
@@ -16,8 +16,10 @@ int odp_atomic_lock_free_u64(odp_atomic_op_t *atomic_op)
 	return 0;
 #else
 	/* All operations are lock-free */
-	if (atomic_op)
+	if (atomic_op) {
 		atomic_op->all_bits = ~((uint32_t)0);
+		atomic_op->op.init  = 0;
+	}
 
 	return 2;
 #endif
