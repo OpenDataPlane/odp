b9eae5b899aed1c1dc1c680aa89a2cfa3639376c
Petri Savolainen <petri.savolainen@nokia.com>
2016-02-09 14:25:07 +0200

    helper: eth: added mac address parse
    Ethernet MAC address parse function is commonly needed by
    test applications. A common parse function harmonizes the
    definition of MAC address as a command line parameter.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Juha-Matti Tilli <juha-matti.tilli@nokia.com>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ helper/test/odp_parse.c
@@ -0,0 +1,205 @@
+/* Copyright (c) 2016, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include <test_debug.h>
+
+#include <odp.h>
+#include <odp/helper/eth.h>
+
+#include <stdio.h>
+#include <string.h>
+
+static int different_mac(odph_ethaddr_t *mac1, odph_ethaddr_t *mac2)
+{
+	return mac1->addr[0] != mac2->addr[0] ||
+	       mac1->addr[1] != mac2->addr[1] ||
+	       mac1->addr[2] != mac2->addr[2] ||
+	       mac1->addr[3] != mac2->addr[3] ||
+	       mac1->addr[4] != mac2->addr[4] ||
+	       mac1->addr[5] != mac2->addr[5];
+}
+
+static int test_mac(void)
+{
+	odph_ethaddr_t mac;
+	odph_ethaddr_t ref;
+
+	memset(&ref, 0, sizeof(odph_ethaddr_t));
+	memset(&mac, 0, sizeof(odph_ethaddr_t));
+
+	/*
+	 * Erroneous strings
+	 */
+
+	/* String must not start with other chars */
+	if (!odph_eth_addr_parse(&mac, "foo 01:02:03:04:05:06")) {
+		LOG_ERR("Accepted bad string\n");
+		return -1;
+	}
+
+	/* Missing digit */
+	if (!odph_eth_addr_parse(&mac, "01:02:03:04:05:")) {
+		LOG_ERR("Accepted bad string\n");
+		return -1;
+	}
+
+	/* Missing colon */
+	if (!odph_eth_addr_parse(&mac, "01:02:03:04:05 06")) {
+		LOG_ERR("Accepted bad string\n");
+		return -1;
+	}
+
+	/* Too large value */
+	if (!odph_eth_addr_parse(&mac, "01:02:03:04:05:1ff")) {
+		LOG_ERR("Accepted bad string\n");
+		return -1;
+	}
+
+	/* Negative value */
+	if (!odph_eth_addr_parse(&mac, "-1:02:03:04:05:06")) {
+		LOG_ERR("Accepted bad string\n");
+		return -1;
+	}
+
+	/* Failed function call must not store address */
+	if (different_mac(&mac, &ref)) {
+		LOG_ERR("Modified address when failed\n");
+		return -1;
+	}
+
+	ref.addr[0] = 1;
+	ref.addr[1] = 2;
+	ref.addr[2] = 3;
+	ref.addr[3] = 4;
+	ref.addr[4] = 5;
+	ref.addr[5] = 6;
+
+	/* Zero pre-fixed */
+	memset(&mac, 0, sizeof(odph_ethaddr_t));
+	if (odph_eth_addr_parse(&mac, "01:02:03:04:05:06")) {
+		LOG_ERR("Parse call failed\n");
+		return -1;
+	}
+
+	if (different_mac(&mac, &ref)) {
+		LOG_ERR("Bad parse result\n");
+		return -1;
+	}
+
+	/* Not zero pre-fixed */
+	memset(&mac, 0, sizeof(odph_ethaddr_t));
+	if (odph_eth_addr_parse(&mac, "1:2:3:4:5:6")) {
+		LOG_ERR("Parse call failed\n");
+		return -1;
+	}
+
+	if (different_mac(&mac, &ref)) {
+		LOG_ERR("Bad parse result\n");
+		return -1;
+	}
+
+	/* String may continue with other chars */
+	memset(&mac, 0, sizeof(odph_ethaddr_t));
+	if (odph_eth_addr_parse(&mac, "01:02:03:04:05:06 foobar")) {
+		LOG_ERR("Parse call failed\n");
+		return -1;
+	}
+
+	if (different_mac(&mac, &ref)) {
+		LOG_ERR("Bad parse result\n");
+		return -1;
+	}
+
+	ref.addr[0] = 0xa;
+	ref.addr[1] = 0xb;
+	ref.addr[2] = 0xc;
+	ref.addr[3] = 0xd;
+	ref.addr[4] = 0xe;
+	ref.addr[5] = 0xf;
+
+	/* Zero pre-fixed */
+	memset(&mac, 0, sizeof(odph_ethaddr_t));
+	if (odph_eth_addr_parse(&mac, "0a:0b:0c:0d:0e:0f")) {
+		LOG_ERR("Parse call failed\n");
+		return -1;
+	}
+
+	if (different_mac(&mac, &ref)) {
+		LOG_ERR("Bad parse result\n");
+		return -1;
+	}
+
+	/* Not zero pre-fixed */
+	memset(&mac, 0, sizeof(odph_ethaddr_t));
+	if (odph_eth_addr_parse(&mac, "a:b:c:d:e:f")) {
+		LOG_ERR("Parse call failed\n");
+		return -1;
+	}
+
+	if (different_mac(&mac, &ref)) {
+		LOG_ERR("Bad parse result\n");
+		return -1;
+	}
+
+	ref.addr[0] = 0x1a;
+	ref.addr[1] = 0x2b;
+	ref.addr[2] = 0x3c;
+	ref.addr[3] = 0x4d;
+	ref.addr[4] = 0x5e;
+	ref.addr[5] = 0x6f;
+
+	/* Dual digits */
+	memset(&mac, 0, sizeof(odph_ethaddr_t));
+	if (odph_eth_addr_parse(&mac, "1a:2b:3c:4d:5e:6f")) {
+		LOG_ERR("Parse call failed\n");
+		return -1;
+	}
+
+	if (different_mac(&mac, &ref)) {
+		LOG_ERR("Bad parse result\n");
+		return -1;
+	}
+
+	memset(&ref, 0, sizeof(odph_ethaddr_t));
+
+	/* All zeros */
+	memset(&mac, 0, sizeof(odph_ethaddr_t));
+	if (odph_eth_addr_parse(&mac, "00:00:00:00:00:00")) {
+		LOG_ERR("Parse call failed\n");
+		return -1;
+	}
+
+	if (different_mac(&mac, &ref)) {
+		LOG_ERR("Bad parse result\n");
+		return -1;
+	}
+
+	memset(&ref, 0xff, sizeof(odph_ethaddr_t));
+
+	/* All ones */
+	memset(&mac, 0, sizeof(odph_ethaddr_t));
+	if (odph_eth_addr_parse(&mac, "ff:ff:ff:ff:ff:ff")) {
+		LOG_ERR("Parse call failed\n");
+		return -1;
+	}
+
+	if (different_mac(&mac, &ref)) {
+		LOG_ERR("Bad parse result\n");
+		return -1;
+	}
+
+	printf("MAC address parse test successful\n");
+	return 0;
+}
+
+int main(void)
+{
+	int ret = 0;
+
+	ret += test_mac();
+
+	return ret;
+}
