d5a1e49686b6ba4e624ce690c56d36e10d3ab6d2
Christophe Milard <christophe.milard@linaro.org>
2016-02-11 15:21:44 +0100

    api: odp.h to ease transition period
    The former odp.h (now called odp_api.h) is recreated here and just
    includes odp_api.h.
    This is to ease the transition period for ODP applications, but
    this file (odp.h) is meant to be removed in the future.
    ODP applications should include odp_api.h, but those
    including odp.h will keep working during this transition period.
    
    Signed-off-by: Christophe Milard <christophe.milard@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ include/odp.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2016, Linaro Limited
+ * All rights reserved
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * WARNING: THIS FILE IS DEPRECATED AND WILL BE REMOVED!!!
+ * ODP APPLICATIONS SHOULD NOW INCLUDE odp_api.h INSTEAD.
+ *
+ * This file is here to ease the transition period but will be removed.
+ * This change has been made to enable the creation of other ODP interfaces.
+ *
+ */
+
+#ifndef ODP_H_
+#define ODP_H_
+
+#include <odp_api.h>
+
+#endif
