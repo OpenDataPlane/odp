489335f2f1de612babbcc4aaaf0e6c8554dff534
Matias Elo <matias.elo@nokia.com>
2016-02-15 12:49:52 +0200

    linux-generic: pktio: initial dpdk pktio implementation
    Initial implementation of the DPDK pktio type.
    Initialization code copied from the odp-dpdk branch.
    
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp_packet_dpdk.h
+++ platform/linux-generic/include/odp_packet_dpdk.h
@@ -10,10 +10,34 @@
 #include <odp/api/packet_io.h>
 #include <odp/api/pool.h>
 
+#include <net/if.h>
+
+#ifdef ODP_PKTIO_DPDK
+#include <rte_config.h>
+#include <rte_mbuf.h>
+
+#define DPDK_MEMORY_MB 512
+#define DPDK_NB_MBUF 16384
+#define DPDK_MBUF_BUF_SIZE RTE_MBUF_DEFAULT_BUF_SIZE
+#define DPDK_MEMPOOL_CACHE_SIZE 32
+#define DPDK_NM_RX_DESC  128
+#define DPDK_NM_TX_DESC  512
+
+_ODP_STATIC_ASSERT(DPDK_NB_MBUF % DPDK_MEMPOOL_CACHE_SIZE == 0 &&
+		   DPDK_MEMPOOL_CACHE_SIZE <= RTE_MEMPOOL_CACHE_MAX_SIZE &&
+		   DPDK_MEMPOOL_CACHE_SIZE <= DPDK_MBUF_BUF_SIZE / 1.5
+		   , "DPDK mempool cache size failure");
+#endif
+
 /** Packet IO using DPDK interface */
 typedef struct {
 	odp_pool_t pool;		  /**< pool to alloc packets from */
+	struct rte_mempool *pkt_pool;	  /**< DPDK packet pool */
 	odp_pktio_capability_t	capa;	  /**< interface capabilities */
+	uint32_t data_room;		  /**< maximum packet length */
+	/** DPDK packet pool name (pktpool_<ifname>) */
+	char pool_name[IF_NAMESIZE + 8];
+	uint8_t port_id;		  /**< DPDK port identifier */
 } pkt_dpdk_t;
 
 #endif
