355558f53162cd6bdf3c06e96acc8767466edaf1
Hongbo Zhang <hongbo.zhang@linaro.org>
2016-02-23 16:12:48 +0800

    linux-generic: separate PowerPC ODP_CACHE_LINE_SIZE to its arch file
    Currently all ODP_CACHE_LINE_SIZE macros for different architectures are
    held in one header file, they should be moved to their own arch file.
    This patch moves ODP_CACHE_LINE_SIZE for PowerPC.
    The previous arch/powerpc/odp/cpu_arch.h was a symlink to the generic
    arch/linux/odp/cpu_arch.h, but now this PowerPC header file has more
    specific content than the generic one, so a real file is created and the
    ODP_CACHE_LINE_SIZE is added to it.
    
    Signed-off-by: Hongbo Zhang <hongbo.zhang@linaro.org>
    Reviewed-and-tested-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/arch/powerpc/odp/api/cpu_arch.h
+++ platform/linux-generic/arch/powerpc/odp/api/cpu_arch.h
@@ -1,1 +1,32 @@
-../../linux/odp/cpu_arch.h
+/* Copyright (c) 2016, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef ODP_PLAT_CPU_ARCH_H_
+#define ODP_PLAT_CPU_ARCH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @ingroup odp_compiler_optim
+ *  @{
+ */
+
+#define ODP_CACHE_LINE_SIZE 64
+
+/**
+ * @}
+ */
+
+static inline void odp_cpu_pause(void)
+{
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
