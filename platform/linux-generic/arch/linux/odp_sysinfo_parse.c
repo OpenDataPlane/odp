df4789d50f85af895dbeb6fbbd97f8d78f965318
Hongbo Zhang <hongbo.zhang@linaro.org>
2016-02-23 16:12:53 +0800

    linux-generic: sysinfo clean up for ARM
    The arch/arm/odp_sysinfo_parse.c is currently a symlink to
    arch/linux/odp_sysinfo_parse.c, but in fact there should be defferences
    between them.
    
    A separated real arch/arm/odp_sysinfo_parse.c is created for ARM, and
    the model_str is set to a general "ARM", which is much better then the
    general defaul "UNKNOWN" although not so accurate in detailed ARM CPU
    model string.
    
    ODP_DBG("TODO: true values should be implemented when possible\n") is
    added for ARM too, this means when possible true values can be implemented
    for ARM, and only ARM odp_cpuinfo_parser() is updated, while the general
    defaul odp_cpuinfo_parser() should be leave there unchanged. In other
    words the current implementation for ARM is a temporary work around, but
    for the generic default dummy data has to be accepted.
    
    ODP_DBG("Refer to https://bugs.linaro.org/show_bug.cgi?id=1870\n") is
    deleted, because this bug describes another thing, eg in previous code,
    only cpu_hz_max[0] and model_str[0] were set, the others such as
    cpu_hz_max[1...n-1] and model_str[1...n-1] were missing, it isn't related
    with the value itself is dummy or not, and this bug was already fixed.
    
    Signed-off-by: Hongbo Zhang <hongbo.zhang@linaro.org>
    Reviewed-and-tested-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/arch/linux/odp_sysinfo_parse.c
+++ platform/linux-generic/arch/linux/odp_sysinfo_parse.c
@@ -13,7 +13,6 @@ int odp_cpuinfo_parser(FILE *file ODP_UNUSED, odp_system_info_t *sysinfo)
 	int i;
 
 	ODP_DBG("Warning: use dummy values for freq and model string\n");
-	ODP_DBG("Refer to https://bugs.linaro.org/show_bug.cgi?id=1870\n");
 	for (i = 0; i < MAX_CPU_NUMBER; i++) {
 		sysinfo->cpu_hz_max[i] = 1400000000;
 		strcpy(sysinfo->model_str[i], "UNKNOWN");
