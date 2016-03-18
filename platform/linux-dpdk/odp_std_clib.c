16fe86ae0d3b3f4011ab24f0666256ee7a7cb802
Nikolay Nikolaev <n.nikolaev@virtualopensystems.com>
2016-03-18 18:23:56 +0000

    linux-dpdk: std_lib: dpdk_memcpy definition for arm/arm64
    DPDK for arm/arm64 uses a #define for the rte_memcpy, which is not suitable
    to get as a function pointer. For those platforms, wrap it in a static function
    and pass its address as dpdk_memcpy.
    
    Signed-off-by: Nikolay Nikolaev <n.nikolaev@virtualopensystems.com>
    Signed-off-by: Zoltan Kiss <zoltan.kiss@linaro.org>

--- platform/linux-dpdk/odp_std_clib.c
+++ platform/linux-dpdk/odp_std_clib.c
@@ -10,7 +10,16 @@ extern "C" {
 
 #include <rte_memcpy.h>
 
+#if defined(__arm__) || defined(__aarch64__)
+static void *_rte_memcpy(void *dst, const void *src, size_t n)
+{
+	return rte_memcpy(dst, src, n);
+}
+
+void* (*const dpdk_memcpy)(void*, const void*, size_t) = &_rte_memcpy;
+#else
 void* (*const dpdk_memcpy)(void*, const void*, size_t) = &rte_memcpy;
+#endif
 
 #ifdef __cplusplus
 }
