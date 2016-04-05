aabd1faab212e7b662125bb4f1f29f1e2fd6acc9
Matias Elo <matias.elo@nokia.com>
2016-04-05 15:27:45 +0300

    example: switch: add validation test
    Add validation test for the switch example using pcap pktio.
    The sizes of the pcap files are compared, but the actual
    contents are not, because the timestamps in the files
    differ. Only packet broadcast is currently tested as
    switching requires further pcap pktio development.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Yogesh Tillu <yogesh.tillu@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ example/switch/switch_run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright (c) 2016, Linaro Limited
+# All rights reserved.
+#
+# SPDX-License-Identifier:     BSD-3-Clause
+#
+
+NUM_RX_PORT=3
+RETVAL=0
+
+PCAP_IN=`find . ${TEST_DIR} $(dirname $0) -name udp64.pcap -print -quit`
+
+echo "Switch test using PCAP_IN = ${PCAP_IN}"
+
+RX_PORTS=""
+for i in `seq 1 $NUM_RX_PORT`;
+do
+	RX_PORTS="${RX_PORTS},pcap:out=pcapout${i}.pcap"
+done
+
+./odp_switch -i pcap:in=${PCAP_IN}${RX_PORTS} &
+
+sleep 1
+kill $!
+wait $!
+STATUS=$?
+
+if [ "$STATUS" -ne 143 ]; then
+  echo "Error: status was: $STATUS, expected 143"
+  RETVAL=1
+fi
+
+for i in `seq 1 $NUM_RX_PORT`;
+do
+	if [ `stat -c %s pcapout${i}.pcap` -ne `stat -c %s udp64.pcap` ]; then
+		echo "Error: Output file $i size not matching"
+		RETVAL=1
+	fi
+	rm -f pcapout${i}.pcap
+done
+
+exit $RETVAL
