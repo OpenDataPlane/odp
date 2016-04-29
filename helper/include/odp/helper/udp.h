0e1645087dea2c7744b2384e8c4d790d79e3125e
Barry Spinney <spinney@mellanox.com>
2016-04-28 22:35:41 -0500

    helper: add a more complete and correct checksum implementation
    This patch adds a file called chksum.c which implements a complete
    TCP/UDP over either IPv4 or IPV6 checksum generation / verification
    capability.  In addition it can deal with any form of packet segmentation
    including tiny segments, segments not aligned to a 2 byte boundary, etc.
    
    Signed-off-by: Barry Spinney <spinney@mellanox.com>
    Signed-off-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- helper/include/odp/helper/udp.h
+++ helper/include/odp/helper/udp.h
@@ -18,6 +18,7 @@ extern "C" {
 #endif
 
 #include <odp_api.h>
+#include <odp/helper/chksum.h>
 
 /** @addtogroup odph_header ODPH HEADER
  *  @{
@@ -37,53 +38,18 @@ typedef struct ODP_PACKED {
 /**
  * UDP checksum
  *
- * This function uses odp packet to calc checksum
+ * This function calculates the UDP checksum given an odp packet.
  *
  * @param pkt  calculate chksum for pkt
  * @return  checksum value in BE endianness
  */
 static inline uint16_t odph_ipv4_udp_chksum(odp_packet_t pkt)
 {
-	odph_ipv4hdr_t	*iph;
-	odph_udphdr_t	*udph;
-	uint32_t	sum;
-	uint16_t	udplen, *buf;
-	union {
-		uint8_t v8[2];
-		uint16_t v16;
-	} val;
+	uint16_t chksum;
+	int      rc;
 
-	if (odp_packet_l4_offset(pkt) == ODP_PACKET_OFFSET_INVALID)
-		return 0;
-	iph = (odph_ipv4hdr_t *)odp_packet_l3_ptr(pkt, NULL);
-	udph = (odph_udphdr_t *)odp_packet_l4_ptr(pkt, NULL);
-	/* 32-bit sum of UDP pseudo-header, seen as a series of 16-bit words */
-	sum = (iph->src_addr & 0xFFFF) + (iph->src_addr >> 16) +
-			(iph->dst_addr & 0xFFFF) + (iph->dst_addr >> 16) +
-			udph->length;
-	val.v8[0] = 0;
-	val.v8[1] = iph->proto;
-	sum += val.v16;
-	/* 32-bit sum of UDP header (checksum field cleared) and UDP data, seen
-	 * as a series of 16-bit words */
-	udplen = odp_be_to_cpu_16(udph->length);
-	buf = (uint16_t *)((void *)udph);
-	for ( ; udplen > 1; udplen -= 2)
-		sum += *buf++;
-	/* Length is not a multiple of 2 bytes */
-	if (udplen) {
-		val.v8[0] = *buf;
-		val.v8[1] = 0;
-		sum += val.v16;
-	}
-	/* Fold sum to 16 bits */
-	sum = (sum & 0xFFFF) + (sum >> 16);
-	/* Add carrier (0/1) to result */
-	sum += (sum >> 16);
-	/* 1's complement */
-	sum = ~sum;
-	/* Return checksum in BE endianness */
-	return (sum == 0x0) ? 0xFFFF : sum;
+	rc = odph_udp_tcp_chksum(pkt, ODPH_CHKSUM_RETURN, &chksum);
+	return (rc == 0) ? chksum : 0;
 }
 
 /** @internal Compile time assert */
