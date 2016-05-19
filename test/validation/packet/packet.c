23edd52f14448057186cf320c56f6138c9b9f256
Bill Fischofer <bill.fischofer@linaro.org>
2016-05-19 08:53:34 -0500

    test: packet: remove unused values
    Resolve bug https://bugs.linaro.org/show_bug.cgi?id=2220 by removing
    unused values.
    
    Signed-off-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/validation/packet/packet.c
+++ test/validation/packet/packet.c
@@ -1159,7 +1159,7 @@ void packet_test_align(void)
 		CU_ASSERT(odp_packet_align(&pkt, 0, pkt_len, 0) < 0);
 
 		offset = seg_len - 5;
-		pkt_data = odp_packet_offset(pkt, offset, &seg_len, NULL);
+		(void)odp_packet_offset(pkt, offset, &seg_len, NULL);
 
 		/* Realign for addressability */
 		CU_ASSERT(odp_packet_align(&pkt, offset,
@@ -1167,8 +1167,7 @@ void packet_test_align(void)
 
 		/* Alignment doesn't change packet length or contents */
 		CU_ASSERT(odp_packet_len(pkt) == pkt_len);
-		aligned_data = odp_packet_offset(pkt, offset,
-						 &aligned_seglen, NULL);
+		(void)odp_packet_offset(pkt, offset, &aligned_seglen, NULL);
 		_packet_compare_offset(pkt, offset,
 				       segmented_test_packet, offset,
 				       aligned_seglen);
