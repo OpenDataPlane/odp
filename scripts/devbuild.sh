753dba7e00187d30202657d85baf2d55c34cd048
Zoltan Kiss <zoltan.kiss@linaro.org>
2016-05-25 15:38:47 +0100

    linux-dpdk: devbuild: enable both static and dynamic version of library
    Developers must make sure both build works.
    
    Signed-off-by: Zoltan Kiss <zoltan.kiss@linaro.org>

--- scripts/devbuild.sh
+++ scripts/devbuild.sh
@@ -7,7 +7,9 @@ export CHECK_ODP_DIR=${CHECK_ODP_DIR:-$REPOS/check-odp}
 export ROOT_DIR_DPDK=${ROOT_DIR_DPDK:-$REPOS/dpdk}
 export GENERIC_BUILDDIR=${GENERIC_BUILDDIR:-$REPOS/odp-dpdk}
 # These are passed to ODP configure
-export CONFIGURE_FLAGS="${CONFIGURE_FLAGS:- --enable-debug --enable-debug-print --enable-cunit-support --enable-test-vald --enable-shared=no --enable-user-guides}"
+export CONFIGURE_FLAGS="${CONFIGURE_FLAGS:- --enable-debug \
+--enable-debug-print --enable-cunit-support --enable-test-vald \
+--enable-shared=yes --enable-static=yes --enable-user-guides}"
 # where to mount huge pages
 export HUGEPAGEDIR=${HUGEPAGEDIR:-/mnt/huge}
 # don't build CUnit for us
