ebb00a1982041fb66a95439abedff978454f2324
Zoltan Kiss <zoltan.kiss@linaro.org>
2016-06-07 17:21:59 +0100

    linux-dpdk: test: fix hugetlbfs mounting in wrapper
    Make it a separate function, and handle every failure so we can fall back
    as much as we can.
    
    Signed-off-by: Zoltan Kiss <zoltan.kiss@linaro.org>

--- platform/linux-dpdk/test/wrapper-script.sh
+++ platform/linux-dpdk/test/wrapper-script.sh
@@ -15,41 +15,48 @@ ctrl_c() {
 	fi
 }
 
-if [ ! -d $HUGEPAGEDIR ]; then
-	sudo mkdir -p $HUGEPAGEDIR
-fi
-if grep -qs "$HUGEPAGEDIR" /proc/mounts; then
-	echo "Umounting hugetlbfs from previous run!"
-	sudo umount -a -t hugetlbfs
-fi
-echo "Mounting hugetlbfs"
-sudo mount -t hugetlbfs -o pagesize=1G nodev $HUGEPAGEDIR 2>/dev/null
-res=$?
-if [ $res -ne 0 ]; then
-	echo "Using 2MB pages"
-	sudo mount -t hugetlbfs nodev $HUGEPAGEDIR
+function mount_and_reserve() {
+	export PATH_NR="/sys/devices/system/node/node0/hugepages/hugepages-${SIZE_KB}kB/nr_hugepages"
+	export PATH_FREE="/sys/devices/system/node/node0/hugepages/hugepages-${SIZE_KB}kB/free_hugepages"
+	if grep -qs "$HUGEPAGEDIR" /proc/mounts; then
+		echo "Umounting hugetlbfs from previous use!"
+		sudo umount -a -t hugetlbfs
+	fi
+	echo "Trying $SIZE pages"
+	sudo mount -t hugetlbfs -o pagesize=$SIZE nodev $HUGEPAGEDIR 2>/dev/null
 	res=$?
 	if [ $res -ne 0 ]; then
 		echo "ERROR: can't mount hugepages"
+		return $res
+	fi
+	sudo sh -c "echo $RESERVE > $PATH_NR"
+	if [ `cat $PATH_NR` -lt 1 ]; then
+		echo "Failed to reserve at least 1 huge page!"
+		return 1
+	else
+		echo "Total number: `cat $PATH_NR`"
+		echo "Free pages: `cat $PATH_FREE`"
+	fi
+}
+
+if [ ! -d $HUGEPAGEDIR ]; then
+	sudo mkdir -p $HUGEPAGEDIR
+fi
+echo "Mounting hugetlbfs"
+export SIZE=1G
+export SIZE_KB=1048576
+export RESERVE=1
+mount_and_reserve
+res=$?
+if [ $res -ne 0 ]; then
+	export SIZE=2MB
+	export SIZE_KB=2048
+	export RESERVE=256
+	mount_and_reserve
+	res=$?
+	if [ $res -ne 0 ]; then
+		echo "ERROR: can't mount hugepages with any size"
 		exit $res
-	fi
-	sudo sh -c 'echo 256 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages'
-	if [ `cat /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages` -lt 1 ]; then
-		echo "Failed to reserve at least 1 huge page!"
-		exit 1
-	else
-		echo "Total number: `cat /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages`"
-		echo "Free pages: `cat /sys/devices/system/node/node0/hugepages/hugepages-2048kB/free_hugepages`"
-	fi
-else
-	echo "Using 1GB pages"
-	sudo sh -c 'echo 1 > /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages'
-	if [ `cat /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages` -lt 1 ]; then
-		echo "Failed to reserve at least 1 huge page!"
-		exit 1
-	else
-		echo "Total number: `cat /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/nr_hugepages`"
-		echo "Free pages: `cat /sys/devices/system/node/node0/hugepages/hugepages-1048576kB/free_hugepages`"
 	fi
 fi
 echo "running $1!"
