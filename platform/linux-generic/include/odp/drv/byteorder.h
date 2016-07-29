90e456db5f983f3fff9126dd70f6fea907fed361
Christophe Milard <christophe.milard@linaro.org>
2016-07-29 08:57:24 +0200

    linux-generic: drv: fixed macro typo
    Signed-off-by: Christophe Milard <christophe.milard@linaro.org>
    Reviewed-and-tested-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp/drv/byteorder.h
+++ platform/linux-generic/include/odp/drv/byteorder.h
@@ -10,8 +10,8 @@
  * ODPDRV byteorder
  */
 
-#ifndef ODPDRVP_PLAT_BYTEORDER_H_
-#define ODPDRVP_PLAT_BYTEORDER_H_
+#ifndef ODPDRV_PLAT_BYTEORDER_H_
+#define ODPDRV_PLAT_BYTEORDER_H_
 
 #ifdef __cplusplus
 extern "C" {
@@ -27,7 +27,7 @@ extern "C" {
 
 static inline uint16_t odpdrv_be_to_cpu_16(odpdrv_u16be_t be16)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return __odpdrv_builtin_bswap16((__odpdrv_force uint16_t)be16);
 #else
 	return (__odpdrv_force uint16_t)be16;
@@ -36,7 +36,7 @@ static inline uint16_t odpdrv_be_to_cpu_16(odpdrv_u16be_t be16)
 
 static inline uint32_t odpdrv_be_to_cpu_32(odpdrv_u32be_t be32)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return __builtin_bswap32((__odpdrv_force uint32_t)be32);
 #else
 	return (__odpdrv_force uint32_t)be32;
@@ -45,7 +45,7 @@ static inline uint32_t odpdrv_be_to_cpu_32(odpdrv_u32be_t be32)
 
 static inline uint64_t odpdrv_be_to_cpu_64(odpdrv_u64be_t be64)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return __builtin_bswap64((__odpdrv_force uint64_t)be64);
 #else
 	return (__odpdrv_force uint64_t)be64;
@@ -54,7 +54,7 @@ static inline uint64_t odpdrv_be_to_cpu_64(odpdrv_u64be_t be64)
 
 static inline odpdrv_u16be_t odpdrv_cpu_to_be_16(uint16_t cpu16)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return (__odpdrv_force odpdrv_u16be_t)__odpdrv_builtin_bswap16(cpu16);
 #else
 	return (__odpdrv_force odpdrv_u16be_t)cpu16;
@@ -63,7 +63,7 @@ static inline odpdrv_u16be_t odpdrv_cpu_to_be_16(uint16_t cpu16)
 
 static inline odpdrv_u32be_t odpdrv_cpu_to_be_32(uint32_t cpu32)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return (__odpdrv_force odpdrv_u32be_t)__builtin_bswap32(cpu32);
 #else
 	return (__odpdrv_force odpdrv_u32be_t)cpu32;
@@ -72,7 +72,7 @@ static inline odpdrv_u32be_t odpdrv_cpu_to_be_32(uint32_t cpu32)
 
 static inline odpdrv_u64be_t odpdrv_cpu_to_be_64(uint64_t cpu64)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return (__odpdrv_force odpdrv_u64be_t)__builtin_bswap64(cpu64);
 #else
 	return (__odpdrv_force odpdrv_u64be_t)cpu64;
@@ -81,7 +81,7 @@ static inline odpdrv_u64be_t odpdrv_cpu_to_be_64(uint64_t cpu64)
 
 static inline uint16_t odpdrv_le_to_cpu_16(odpdrv_u16le_t le16)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return (__odpdrv_force uint16_t)le16;
 #else
 	return __odpdrv_builtin_bswap16((__odpdrv_force uint16_t)le16);
@@ -90,7 +90,7 @@ static inline uint16_t odpdrv_le_to_cpu_16(odpdrv_u16le_t le16)
 
 static inline uint32_t odpdrv_le_to_cpu_32(odpdrv_u32le_t le32)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return (__odpdrv_force uint32_t)le32;
 #else
 	return __builtin_bswap32((__odpdrv_force uint32_t)le32);
@@ -99,7 +99,7 @@ static inline uint32_t odpdrv_le_to_cpu_32(odpdrv_u32le_t le32)
 
 static inline uint64_t odpdrv_le_to_cpu_64(odpdrv_u64le_t le64)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return (__odpdrv_force uint64_t)le64;
 #else
 	return __builtin_bswap64((__odpdrv_force uint64_t)le64);
@@ -108,7 +108,7 @@ static inline uint64_t odpdrv_le_to_cpu_64(odpdrv_u64le_t le64)
 
 static inline odpdrv_u16le_t odpdrv_cpu_to_le_16(uint16_t cpu16)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return (__odpdrv_force odpdrv_u16le_t)cpu16;
 #else
 	return (__odpdrv_force odpdrv_u16le_t)__odpdrv_builtin_bswap16(cpu16);
@@ -117,7 +117,7 @@ static inline odpdrv_u16le_t odpdrv_cpu_to_le_16(uint16_t cpu16)
 
 static inline odpdrv_u32le_t odpdrv_cpu_to_le_32(uint32_t cpu32)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return (__odpdrv_force odpdrv_u32le_t)cpu32;
 #else
 	return (__odpdrv_force odpdrv_u32le_t)__builtin_bswap32(cpu32);
@@ -126,7 +126,7 @@ static inline odpdrv_u32le_t odpdrv_cpu_to_le_32(uint32_t cpu32)
 
 static inline odpdrv_u64le_t odpdrv_cpu_to_le_64(uint64_t cpu64)
 {
-#if ODPDRVP_BYTE_ORDER == ODPDRVP_LITTLE_ENDIAN
+#if ODPDRV_BYTE_ORDER == ODPDRV_LITTLE_ENDIAN
 	return (__odpdrv_force odpdrv_u64le_t)cpu64;
 #else
 	return (__odpdrv_force odpdrv_u64le_t)__builtin_bswap64(cpu64);
