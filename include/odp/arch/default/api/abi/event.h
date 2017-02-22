d4627e38c1750ef3a0f063c72bbc0e8c3f1775a8
Petri Savolainen <petri.savolainen@linaro.org>
2017-02-22 17:08:22 +0200

    abi: event: add ODP_EVENT_IPSEC_RESULT
    Update ABI spec with the new IPSEC event type.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@linaro.org>
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- include/odp/arch/default/api/abi/event.h
+++ include/odp/arch/default/api/abi/event.h
@@ -28,7 +28,8 @@ typedef enum odp_event_type_t {
 	ODP_EVENT_BUFFER       = 1,
 	ODP_EVENT_PACKET       = 2,
 	ODP_EVENT_TIMEOUT      = 3,
-	ODP_EVENT_CRYPTO_COMPL = 4
+	ODP_EVENT_CRYPTO_COMPL = 4,
+	ODP_EVENT_IPSEC_RESULT = 5
 } odp_event_type_t;
 
 /**
