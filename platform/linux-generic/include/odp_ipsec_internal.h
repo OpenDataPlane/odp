ce4bd52afcef245b069acfe8dc53bd62aec576fd
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-03-09 14:57:14 +0300

    linux-generic: ipsec: draft IPsec implementation
    For now it's only a preview with the following limitation:
     - Only IPv4 support
     - No pipeline_cls and outer header retaining support
     - No zeroing of mutable IPv4 options for AH ICV calculation
     - No replay protection
     - No ESN support
     - No UDP encapsulation support
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp_ipsec_internal.h
+++ platform/linux-generic/include/odp_ipsec_internal.h
@@ -183,6 +183,14 @@ int _odp_ipsec_sa_update_stats(ipsec_sa_t *ipsec_sa, uint32_t len,
 			       odp_ipsec_op_status_t *status);
 
 /**
+ * Try inline IPsec processing of provided packet.
+ *
+ * @retval 0 if packet was processed and will be queue using IPsec inline
+ *           processing
+ */
+int _odp_ipsec_try_inline(odp_packet_t pkt);
+
+/**
  * @}
  */
 
