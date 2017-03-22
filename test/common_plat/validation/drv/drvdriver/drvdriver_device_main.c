b56dd29e0ceb58ea13523b69dc7d87c6edf83de2
Christophe Milard <christophe.milard@linaro.org>
2017-03-22 15:48:20 +0100

    test: drv: device creation and destruction
    Testing that devices can be created and removed from ODP.
    
    Signed-off-by: Christophe Milard <christophe.milard@linaro.org>
    Reviewed-and-tested-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ test/common_plat/validation/drv/drvdriver/drvdriver_device_main.c
@@ -0,0 +1,12 @@
+/* Copyright (c) 2017, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include "drvdriver_device.h"
+
+int main(int argc, char *argv[])
+{
+	return drvdriver_device_main(argc, argv);
+}
