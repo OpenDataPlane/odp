65f2f6313487bbdd96ac1c3c859dd3eb27d4dd98
Christophe Milard <christophe.milard@linaro.org>
2017-03-22 15:48:09 +0100

    linux-gen: adding compiler hints in the driver interface
    Just including the spec file from the linux-generic side, as usual.
    
    Signed-off-by: Christophe Milard <christophe.milard@linaro.org>

---
+++ platform/linux-generic/include/odp/drv/hints.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2017, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODPDRV compiler hints
+ */
+
+#ifndef ODPDRV_PLAT_HINTS_H_
+#define ODPDRV_PLAT_HINTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @ingroup odpdrv_compiler_optim
+ *  @{
+ */
+
+/**
+ * @}
+ */
+
+#include <odp/drv/spec/hints.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
