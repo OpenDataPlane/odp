cf4a8144d8a8fcbbc3a9d6d43cbaa479f5e2dbfb
Maxim Uvarov <maxim.uvarov@linaro.org>
2017-04-27 17:26:42 +0300

    test: tm: add paths to find tm binary
    Use the same algorithm as pktio_run.sh to find paths in
    different cases (in tree build, out of tree build, distcheck
    and etc).
    Fixes:
    https://bugs.linaro.org/show_bug.cgi?id=2969
    
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>
    Reviewed-and-tested-by: Bill Fischofer <bill.fischofer@linaro.org>

--- test/common_plat/validation/api/traffic_mngr/traffic_mngr.sh
+++ test/common_plat/validation/api/traffic_mngr/traffic_mngr.sh
@@ -6,13 +6,29 @@
 # SPDX-License-Identifier:	BSD-3-Clause
 #
 
-# directory where test binaries have been built
-TEST_DIR="${TEST_DIR:-$(dirname $0)}"
+# directories where traffic_mngr_main binary can be found:
+# -in the validation dir when running make check (intree or out of tree)
+# -in the script directory, when running after 'make install', or
+# -in the validation when running standalone (./traffic_mngr) intree.
+# -in the current directory.
+# running stand alone out of tree requires setting PATH
+PATH=${TEST_DIR}/api/traffic_mngr:$PATH
+PATH=$(dirname $0)/../../../../common_plat/validation/api/traffic_mngr:$PATH
+PATH=$(dirname $0):$PATH
+PATH=`pwd`:$PATH
+
+traffic_mngr_main_path=$(which traffic_mngr_main${EXEEXT})
+if [ -x "$traffic_mngr_main_path" ] ; then
+	echo "running with traffic_mngr_main: $traffic_mngr_run_path"
+else
+	echo "cannot find traffic_mngr_main: please set you PATH for it."
+	exit 1
+fi
 
 # exit codes expected by automake for skipped tests
 TEST_SKIPPED=77
 
-${TEST_DIR}/traffic_mngr_main${EXEEXT}
+traffic_mngr_main${EXEEXT}
 ret=$?
 
 SIGSEGV=139
