56d222994b3a49e6b219715c8f3168a1e18b212a
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-06-21 14:48:00 +0300

    api: crypto: add crypto packet operation interface
    Input and output of crypto operations are packets. API is more flexible
    for application and API pipelining when output is packet with additional
    metadata. Application or API pipeline stages which do not care about
    crypto results may work on basic packet metadata.
    
    Crypto result event type changes from ODP_EVENT_CRYPTO_COMPL to
    ODP_EVENT_PACKET. Event subtype (ODP_EVENT_PACKET_CRYPTO) can be
    used to identify packets with crypto metadata.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- example/ipsec/odp_ipsec_misc.h
+++ example/ipsec/odp_ipsec_misc.h
@@ -321,12 +321,12 @@ void ipv4_adjust_len(odph_ipv4hdr_t *ip, int adj)
 /**
  * Verify crypto operation completed successfully
  *
- * @param status  Pointer to cryto completion structure
+ * @param status  Pointer to crypto op status structure
  *
  * @return TRUE if all OK else FALSE
  */
 static inline
-odp_bool_t is_crypto_compl_status_ok(odp_crypto_compl_status_t *status)
+odp_bool_t is_crypto_op_status_ok(odp_crypto_op_status_t *status)
 {
 	if (status->alg_err != ODP_CRYPTO_ALG_ERR_NONE)
 		return FALSE;
