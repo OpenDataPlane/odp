cd097450cbf9d7bb6cd24654f8d43ceade5b114a
Petri Savolainen <petri.savolainen@linaro.org>
2017-06-26 13:24:28 +0300

    test: l2fwd: list not used features
    List not used features so that performance may be optimized
    on some platforms. E.g. on odp-linux implementation, timer
    polling causes high overhead, which is disabled when timers
    are not used.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/common_plat/performance/odp_l2fwd.c
+++ test/common_plat/performance/odp_l2fwd.c
@@ -1387,9 +1387,19 @@ int main(int argc, char *argv[])
 	odp_instance_t instance;
 	int num_groups;
 	odp_schedule_group_t group[MAX_PKTIOS];
+	odp_init_t init;
+
+	odp_init_param_init(&init);
+
+	/* List features not to be used (may optimize performance) */
+	init.not_used.feat.cls    = 1;
+	init.not_used.feat.crypto = 1;
+	init.not_used.feat.ipsec  = 1;
+	init.not_used.feat.timer  = 1;
+	init.not_used.feat.tm     = 1;
 
 	/* Init ODP before calling anything else */
-	if (odp_init_global(&instance, NULL, NULL)) {
+	if (odp_init_global(&instance, &init, NULL)) {
 		LOG_ERR("Error: ODP global init failed.\n");
 		exit(EXIT_FAILURE);
 	}
