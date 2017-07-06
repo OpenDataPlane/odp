62222c2add2b37b47c4bc84b2aca67749e667646
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-07-06 13:39:22 +0300

    linux-gen: pktio: loop: support IPsec inbound inline
    Add support for inbound inline IPsec packet processing on loop
    interface.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/pktio/loop.c
+++ platform/linux-generic/pktio/loop.c
@@ -135,6 +135,12 @@ static int loopback_recv(pktio_entry_t *pktio_entry, int index ODP_UNUSED,
 
 		packet_set_ts(pkt_hdr, ts);
 		pkt_hdr->input = pktio_entry->s.handle;
+
+		/* Try IPsec inline processing */
+		if (pktio_entry->s.config.inbound_ipsec &&
+		    odp_packet_has_ipsec(pkt))
+			_odp_ipsec_try_inline(pkt);
+
 		pktio_entry->s.stats.in_octets += pkt_len;
 		pkts[num_rx++] = pkt;
 	}
@@ -229,6 +235,7 @@ static int loopback_capability(pktio_entry_t *pktio_entry ODP_UNUSED,
 	odp_pktio_config_init(&capa->config);
 	capa->config.pktin.bit.ts_all = 1;
 	capa->config.pktin.bit.ts_ptp = 1;
+	capa->config.inbound_ipsec = 1;
 	capa->config.outbound_ipsec = 1;
 
 	return 0;
