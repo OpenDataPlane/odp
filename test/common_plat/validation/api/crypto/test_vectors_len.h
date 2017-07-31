8d21533c341c0624f693af554341dd389c358238
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-07-31 14:51:30 +0300

    validation: crypto: rework testsuite
    Currently crypto testsuite contains tons of similar c&p code setting
    keys, iv, data, etc. Data is scattered into different arrays. Refactor
    crypto testsuite so that all data is kept in the structured way,
    removing most of similar code from individual testing functions.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/common_plat/validation/api/crypto/test_vectors_len.h
+++ test/common_plat/validation/api/crypto/test_vectors_len.h
@@ -6,45 +6,40 @@
 #ifndef TEST_VECTORS_LEN_
 #define TEST_VECTORS_LEN_
 
-/* NULL */
-#define NULL_MAX_DATA_LEN   16
+/* Maximum */
+#define MAX_KEY_LEN         64
+#define MAX_IV_LEN          16
+#define MAX_DATA_LEN        128
+#define MAX_AAD_LEN         12
+#define MAX_DIGEST_LEN      64
 
 /* TDES-CBC */
 #define TDES_CBC_KEY_LEN        24
 #define TDES_CBC_IV_LEN         8
-#define TDES_CBC_MAX_DATA_LEN   16
 
 /* AES128-CBC */
 #define AES128_CBC_KEY_LEN        16
 #define AES128_CBC_IV_LEN         16
-#define AES128_CBC_MAX_DATA_LEN   64
 
-/* AES128-CBC */
+/* AES128-GCM */
 #define AES128_GCM_KEY_LEN        16
 #define AES128_GCM_IV_LEN         12
-#define AES128_GCM_MAX_DATA_LEN   106
 #define AES128_GCM_DIGEST_LEN     16
 
 /* HMAC-MD5 */
 #define HMAC_MD5_KEY_LEN        16
-#define HMAC_MD5_MAX_DATA_LEN   128
-#define HMAC_MD5_DIGEST_LEN     16
+#define HMAC_MD5_96_CHECK_LEN   12
 
 /* HMAC-SHA256 */
 #define HMAC_SHA256_KEY_LEN        32
-#define HMAC_SHA256_MAX_DATA_LEN   128
-#define HMAC_SHA256_DIGEST_LEN     32
+#define HMAC_SHA256_128_CHECK_LEN   16
 
 /* HMAC-SHA1 */
 #define HMAC_SHA1_KEY_LEN        20
-#define HMAC_SHA1_MAX_DATA_LEN   128
-#define HMAC_SHA1_DIGEST_LEN     20
 #define HMAC_SHA1_96_CHECK_LEN   12
 
 /* HMAC-SHA512 */
 #define HMAC_SHA512_KEY_LEN        64
-#define HMAC_SHA512_MAX_DATA_LEN   128
-#define HMAC_SHA512_DIGEST_LEN     64
 #define HMAC_SHA512_256_CHECK_LEN  32
 
 #endif
