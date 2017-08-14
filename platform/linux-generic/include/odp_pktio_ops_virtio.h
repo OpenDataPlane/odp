3c372d43de115af127c6fd2d34f365b73b87734b
Josep Puigdemont <josep.puigdemont@linaro.org>
2017-08-14 15:39:24 +0200

    virtio: update pktio structure
    Add device id and pci device structure to pktio's.
    
    Signed-off-by: Josep Puigdemont <josep.puigdemont@linaro.org>

--- platform/linux-generic/include/odp_pktio_ops_virtio.h
+++ platform/linux-generic/include/odp_pktio_ops_virtio.h
@@ -7,7 +7,11 @@
 #ifndef ODP_PACKET_VIRTIO_H
 #define ODP_PACKET_VIRTIO_H
 
+#include <drv_pci_internal.h>
+
 typedef struct {
+	struct pci_dev_t *pci_dev;
+	int dev_id;
 	char name[32]; /**< Unique identifier name */
 } pktio_ops_virtio_data_t;
 
