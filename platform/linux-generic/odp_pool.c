bd361ffd9d7ec6421fad382f57598a8e6097cd5c
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-08-16 18:43:10 +0300

    linux-gen: drop _ODP_PKTIO_IPC define
    Since 39b3a1681097638 ("linux-gen: remove pktio ipc option from
    configure") IPC pktio is enabled unconditionally. Drop define guarding
    conditional compilation of several remaining bits.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- platform/linux-generic/odp_pool.c
+++ platform/linux-generic/odp_pool.c
@@ -536,10 +536,8 @@ odp_pool_t odp_pool_create(const char *name, odp_pool_param_t *params)
 	if (check_params(params))
 		return ODP_POOL_INVALID;
 
-#ifdef _ODP_PKTIO_IPC
 	if (params && (params->type == ODP_POOL_PACKET))
 		shm_flags = ODP_SHM_PROC;
-#endif
 
 	return pool_create(name, params, shm_flags);
 }
