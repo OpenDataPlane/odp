4b4173303cf61432437caf89995df3ee972bcb9e
Kelvin Cheung <keguang.zhang@gmail.com>
2017-08-23 17:00:58 +0800

    linux-gen: system: correctly set CPU frequency
    When CPU frequency info under /sys is available,
    use the real value instead of default value.
    
    Signed-off-by: Kelvin Cheung <keguang.zhang@gmail.com>

--- platform/linux-generic/arch/arm/odp_sysinfo_parse.c
+++ platform/linux-generic/arch/arm/odp_sysinfo_parse.c
@@ -11,11 +11,29 @@
 int cpuinfo_parser(FILE *file ODP_UNUSED, system_info_t *sysinfo)
 {
 	int i;
+	FILE *fp;
+	char path[256], buffer[256], *endptr = NULL;
 
-	ODP_DBG("Warning: use dummy values for freq and model string\n");
 	for (i = 0; i < MAX_CPU_NUMBER; i++) {
 		sysinfo->cpu_hz_max[i] = 1400000000;
 		strcpy(sysinfo->model_str[i], "UNKNOWN");
+
+		snprintf(path, sizeof(path),
+			 "/sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_max_freq",
+			 i);
+
+		fp = fopen(path, "r");
+		if (fp == NULL) {
+			ODP_DBG("Warning: use dummy values for freq\n");
+			continue;
+		}
+
+		if (fgets(buffer, sizeof(buffer), fp) == NULL)
+			ODP_DBG("Warning: use dummy values for freq\n");
+		else
+			sysinfo->cpu_hz_max[i] = strtoull(buffer, &endptr, 0);
+
+		fclose(fp);
 	}
 
 	return 0;
@@ -23,7 +41,24 @@ int cpuinfo_parser(FILE *file ODP_UNUSED, system_info_t *sysinfo)
 
 uint64_t odp_cpu_hz_current(int id ODP_UNUSED)
 {
-	return 0;
+	FILE *fp;
+	char path[256], buffer[256], *endptr = NULL;
+
+	snprintf(path, sizeof(path),
+		 "/sys/devices/system/cpu/cpu%d/cpufreq/cpuinfo_cur_freq", id);
+
+	fp = fopen(path, "r");
+	if (fp == NULL)
+		return 0;
+
+	if (fgets(buffer, sizeof(buffer), fp) == NULL) {
+		fclose(fp);
+		return 0;
+	}
+
+	fclose(fp);
+
+	return strtoull(buffer, &endptr, 0);
 }
 
 void sys_info_print_arch(void)
