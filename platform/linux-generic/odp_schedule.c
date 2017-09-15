16bf7fb7a31206b3548b9e3dcfe407dd16ddca1a
Balasubramanian Manoharan <bala.manoharan@linaro.org>
2017-09-15 14:28:38 +0530

    linux-generic: schedule: implement schedule order unlock lock api
    Implements odp_schedule_order_unlock_lock() api. This api combines schedule
    order unlock and lock into a single api for performance optimization in HW.
    
    Signed-off-by: Balasubramanian Manoharan <bala.manoharan@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/odp_schedule.c
+++ platform/linux-generic/odp_schedule.c
@@ -1145,6 +1145,13 @@ static void schedule_order_unlock(uint32_t lock_index)
 	odp_atomic_store_rel_u64(ord_lock, sched_local.ordered.ctx + 1);
 }
 
+static void schedule_order_unlock_lock(uint32_t unlock_index,
+				       uint32_t lock_index)
+{
+	schedule_order_unlock(unlock_index);
+	schedule_order_lock(lock_index);
+}
+
 static void schedule_pause(void)
 {
 	sched_local.pause = 1;
@@ -1430,5 +1437,6 @@ const schedule_api_t schedule_default_api = {
 	.schedule_group_thrmask   = schedule_group_thrmask,
 	.schedule_group_info      = schedule_group_info,
 	.schedule_order_lock      = schedule_order_lock,
-	.schedule_order_unlock    = schedule_order_unlock
+	.schedule_order_unlock    = schedule_order_unlock,
+	.schedule_order_unlock_lock    = schedule_order_unlock_lock
 };
