4e27e46d8a14377ac02bfbcf4b2730cda5c95785
Petri Savolainen <petri.savolainen@linaro.org>
2017-09-15 15:15:19 +0300

    validation: chksum: add test chksum suite
    Create chksum test suite and ones complement sum test
    in there.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ test/validation/api/chksum/chksum.h
@@ -0,0 +1,25 @@
+/* Copyright (c) 2017, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef _ODP_TEST_CHKSUM_H_
+#define _ODP_TEST_CHKSUM_H_
+
+#include <odp_cunit_common.h>
+
+/* test functions: */
+void chksum_ones_complement_ip(void);
+void chksum_ones_complement_udp(void);
+
+/* test arrays: */
+extern odp_testinfo_t chksum_suite[];
+
+/* test registry: */
+extern odp_suiteinfo_t chksum_suites[];
+
+/* main test program: */
+int chksum_main(int argc, char *argv[]);
+
+#endif
