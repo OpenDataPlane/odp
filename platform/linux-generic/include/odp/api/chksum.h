d95e91d3ec45dc3cfc4e25507ce1336d25ce5cc5
Petri Savolainen <petri.savolainen@linaro.org>
2017-09-15 11:48:42 +0300

    api: chksum: add ones complement sum function
    This is a low level function for IP/UDP/TCP checksum calculations
    on SW. HW cannot always calculate checksums, for that or other
    reasons application may need calculate ones compelement sums.
    
    This kind of low level function is easy to optimize e.g. with CPU
    vector instructions. In addition to this, there may be higher
    (packet) level functions introduced later.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ platform/linux-generic/include/odp/api/chksum.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2017, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP checksum functions
+ */
+
+#ifndef ODP_PLAT_CHKSUM_H_
+#define ODP_PLAT_CHKSUM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @ingroup odp_chksum
+ *  @{
+ */
+
+/**
+ * @}
+ */
+
+#include <odp/api/spec/chksum.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
