609693ec852215bcd6b9423a692c15c29a57ba7e
Bogdan Pricope <bogdan.pricope@linaro.org>
2017-09-22 14:34:35 +0300

    linux-gen: pktio: netmap: use generic pktio_ops data storage
    Signed-off-by: Bogdan Pricope <bogdan.pricope@linaro.org>

--- platform/linux-generic/include/odp_pktio_ops_subsystem.h
+++ platform/linux-generic/include/odp_pktio_ops_subsystem.h
@@ -79,7 +79,6 @@ typedef ODP_MODULE_CLASS(pktio_ops) {
 } pktio_ops_module_t;
 
 /* All implementations of this subsystem */
-#include <odp_pktio_ops_netmap.h>
 #include <odp_pktio_ops_pcap.h>
 
 /* Per implementation private data
@@ -87,7 +86,6 @@ typedef ODP_MODULE_CLASS(pktio_ops) {
  */
 typedef union {
 	void *dpdk;
-	pktio_ops_netmap_data_t netmap;
 	pktio_ops_pcap_data_t pcap;
 } pktio_ops_data_t;
 
