09ae060ccf184602d3dd9763d7cf73bd2c2ac5de
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-10-04 21:15:33 +0300

    linux-gen: more unification of arch-specific code
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- platform/linux-generic/arch/arm/odp_cpu_idling.h
+++ platform/linux-generic/arch/arm/odp_cpu_idling.h
@@ -13,39 +13,33 @@
 #error This file should not be included directly, please include odp_cpu.h
 #endif
 
+#ifndef CONFIG_WFE
+
+#include "../default/odp_cpu_idling.h"
+
+#else /* CONFIG_WFE */
+
 static inline void sevl(void)
 {
-#ifdef CONFIG_WFE
 	__asm__ volatile("sevl" : : : );
-#endif
 }
 
 static inline int wfe(void)
 {
-#ifdef CONFIG_WFE
 	__asm__ volatile("wfe" : : : "memory");
-#endif
 	return 1;
 }
 
 static inline void doze(void)
 {
-#ifndef CONFIG_WFE
 	/* When using WFE do not stall the pipeline using other means */
 	odp_cpu_pause();
-#endif
 }
 
-#ifdef CONFIG_WFE
 #define monitor128(addr, mo) lld((addr), (mo))
 #define monitor64(addr, mo) ll64((addr), (mo))
 #define monitor32(addr, mo) ll32((addr), (mo))
 #define monitor8(addr, mo) ll8((addr), (mo))
-#else
-#define monitor128(addr, mo) __atomic_load_n((addr), (mo))
-#define monitor64(addr, mo) __atomic_load_n((addr), (mo))
-#define monitor32(addr, mo) __atomic_load_n((addr), (mo))
-#define monitor8(addr, mo) __atomic_load_n((addr), (mo))
-#endif
+#endif /* CONFIG_WFE */
 
 #endif  /* PLATFORM_LINUXGENERIC_ARCH_ARM_CPU_IDLING_H */
