8999344484a2c3b4c08b84c5b27e2b355fa312d5
Bill Fischofer <bill.fischofer@linaro.org>
2017-10-05 15:34:27 -0500

    linux-generic: packet: remove odp_packet_unshared_len() implementation
    Signed-off-by: Bill Fischofer <bill.fischofer@linaro.org>
    Reviewed-by: Balasubramanian Manoharan <bala.manoharan@linaro.org>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Nikhil Agarwal <nikhil.agarwal@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp_packet_internal.h
+++ platform/linux-generic/include/odp_packet_internal.h
@@ -124,7 +124,6 @@ typedef struct {
 	odp_pktio_t input;
 
 	uint32_t frame_len;
-	uint32_t shared_len;
 
 	uint16_t headroom;
 	uint16_t tailroom;
@@ -225,7 +224,6 @@ static inline void packet_init(odp_packet_hdr_t *pkt_hdr, uint32_t len)
 	* segment occupied by the allocated length.
 	*/
 	pkt_hdr->frame_len = len;
-	pkt_hdr->shared_len = 0;
 	pkt_hdr->headroom  = CONFIG_PACKET_HEADROOM;
 	pkt_hdr->tailroom  = pool->seg_len - seg_len + CONFIG_PACKET_TAILROOM;
 
