275e4b717c792218ff1dc4b31ca3412231c7fd8b
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-10-22 14:41:56 +0300

    linux-gen, include: switch sync.h to api+abi
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

---
+++ platform/linux-generic/include-abi/odp/api/abi/sync.h
@@ -0,0 +1,35 @@
+/* Copyright (c) 2015, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP barrier
+ */
+
+#ifndef ODP_API_ABI_SYNC_H_
+#define ODP_API_ABI_SYNC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @ingroup odp_barrier
+ *  @{
+ */
+
+#include <odp/api/plat/static_inline.h>
+#include <odp/api/plat/sync_inlines.h>
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
