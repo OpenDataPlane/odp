e7dcc90832bac617586e6455a86fe45e34ecb5fc
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-10-22 14:41:56 +0300

    linux-gen, include: switch std_types.h to api+abi
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- platform/linux-generic/include/odp/api/std_types.h
+++ platform/linux-generic/include/odp/api/std_types.h
@@ -10,28 +10,15 @@
  * Standard C language types and definitions for ODP.
  */
 
-#ifndef ODP_PLAT_STD_TYPES_H_
-#define ODP_PLAT_STD_TYPES_H_
+#ifndef ODP_API_STD_TYPES_H_
+#define ODP_API_STD_TYPES_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* uint64_t, uint32_t, etc */
-#include <stdint.h>
 
-/* true and false for odp_bool_t */
-#include <stdbool.h>
-
-/** @addtogroup odp_system ODP SYSTEM
- *  @{
- */
-
-typedef int odp_bool_t;
-
-/**
- * @}
- */
+#include <odp/api/abi/std_types.h>
 
 #include <odp/api/spec/std_types.h>
 
