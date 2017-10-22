a8ce5f13b5cbf1583fef04d687cef94d18357520
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-10-22 14:41:56 +0300

    linux-gen, include: switch thread.h to api+abi
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp/api/thread.h
+++ platform/linux-generic/include/odp/api/thread.h
@@ -10,14 +10,14 @@
  * ODP thread API
  */
 
-#ifndef ODP_PLAT_THREAD_H_
-#define ODP_PLAT_THREAD_H_
+#ifndef ODP_API_THREAD_H_
+#define ODP_API_THREAD_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <odp/api/plat/thread_types.h>
+#include <odp/api/abi/thread.h>
 
 #include <odp/api/spec/thread.h>
 
