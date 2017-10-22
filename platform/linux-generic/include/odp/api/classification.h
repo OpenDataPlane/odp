780e6e0375e501db96ab733416cd7569f2087923
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-10-22 14:41:56 +0300

    linux-gen, include: switch queue.h to api+abi
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- platform/linux-generic/include/odp/api/classification.h
+++ platform/linux-generic/include/odp/api/classification.h
@@ -22,7 +22,7 @@ extern "C" {
 #include <odp/api/abi/classification.h>
 #include <odp/api/plat/packet_types.h>
 #include <odp/api/plat/packet_io_types.h>
-#include <odp/api/plat/queue_types.h>
+#include <odp/api/abi/queue.h>
 
 #include <odp/api/spec/classification.h>
 
