6003039a6a2481d55697f2e20543e99153696d4e
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-10-22 14:41:56 +0300

    linux-gen, include: switch packet_io.h to api+abi
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- include/odp/api/abi-default/packet_io.h
+++ include/odp/api/abi-default/packet_io.h
@@ -4,29 +4,30 @@
  * SPDX-License-Identifier:     BSD-3-Clause
  */
 
-
 /**
  * @file
  *
  * ODP Packet IO
  */
 
-#ifndef ODP_PACKET_IO_TYPES_H_
-#define ODP_PACKET_IO_TYPES_H_
+#ifndef ODP_ABI_PACKET_IO_H_
+#define ODP_ABI_PACKET_IO_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include <odp/api/std_types.h>
-#include <odp/api/plat/strong_types.h>
+
+/** @internal Dummy type for strong typing */
+typedef struct { char dummy; /**< @internal Dummy */ } _odp_abi_pktio_t;
 
 /** @addtogroup odp_packet_io
  *  Operations on a packet.
  *  @{
  */
 
-typedef ODP_HANDLE_T(odp_pktio_t);
+typedef _odp_abi_pktio_t *odp_pktio_t;
 
 /** @internal */
 typedef struct odp_pktin_queue_t {
@@ -40,18 +41,12 @@ typedef struct odp_pktout_queue_t {
 	int index;         /**< @internal pktio queue index */
 } odp_pktout_queue_t;
 
-#define ODP_PKTIO_INVALID _odp_cast_scalar(odp_pktio_t, 0)
+#define ODP_PKTIO_INVALID ((odp_pktio_t)0)
 
 #define ODP_PKTIO_MACADDR_MAXSIZE 16
 
 #define ODP_PKTIN_NO_WAIT 0
 #define ODP_PKTIN_WAIT    UINT64_MAX
-
-/** Get printable format of odp_pktio_t */
-static inline uint64_t odp_pktio_to_u64(odp_pktio_t hdl)
-{
-	return _odp_pri(hdl);
-}
 
 /**
  * @}
