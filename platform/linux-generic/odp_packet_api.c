d35f27a2c613e5fa66a6d854b662b435f0e4f402
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-10-23 10:27:40 +0300

    linux-gen: split odp_packet/odp_packet_flags
    Split away inclusion of API implementation to the files selected by
    Automake.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ platform/linux-generic/odp_packet_api.c
@@ -0,0 +1,28 @@
+/* Copyright (c) 2013, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include "config.h"
+
+#include <odp/api/packet.h>
+#include <odp/api/plat/packet_inlines.h>
+#include <odp_packet_internal.h>
+#include <odp_debug_internal.h>
+#include <odp/api/hints.h>
+#include <odp/api/byteorder.h>
+
+#include <protocols/eth.h>
+#include <protocols/ip.h>
+#include <protocols/tcp.h>
+#include <protocols/udp.h>
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+/* Include non-inlined versions of API functions */
+#define _ODP_INLINE
+#include <odp/api/plat/packet_inlines_api.h>
