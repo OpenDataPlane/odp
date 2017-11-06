a72a1e82ad31d5555c644508d5f35c590b3c4073
Balasubramanian Manoharan <bala.manoharan@linaro.org>
2017-11-06 23:22:14 +0530

    api: threshold: add odp_threshold_t parameter
    odp_threshold_t is used to configure different threshold types
    
    Signed-off-by: Balasubramanian Manoharan <bala.manoharan@linaro.org>
    Reviewed-by: Nikhil Agarwal <nikhil.agarwal@linaro.org>
    Reviewed-by: Petri Savolainen <petri.savolainen@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ platform/linux-generic/include/odp/api/threshold.h
@@ -0,0 +1,34 @@
+/* Copyright (c) 2017, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP threshold API - platform specific header
+ */
+
+#ifndef ODP_PLAT_THRESHOLD_H_
+#define ODP_PLAT_THRESHOLD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @ingroup odp_threshold
+ *  @{
+ */
+
+/**
+ * @}
+ */
+
+#include <odp/api/spec/threshold.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
