79494b70d781207b3c424e7120448ce216cb2cc7
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-11-07 17:33:30 +0300

    helper: use new odp checksum API
    Use odp_chksum_ones_comp16 which may be platform-optimized. This removes
    now unnecessary odph_cksum() function.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- helper/include/odp/helper/chksum.h
+++ helper/include/odp/helper/chksum.h
@@ -35,33 +35,6 @@ typedef enum {
 } odph_chksum_op_t;
 
 /**
- * Checksum
- *
- * @param buffer calculate chksum for buffer
- * @param len    buffer length
- *
- * @return checksum value in network order
- */
-static inline odp_u16sum_t odph_chksum(void *buffer, int len)
-{
-	uint16_t *buf = (uint16_t *)buffer;
-	uint32_t sum = 0;
-	uint16_t result;
-
-	for (sum = 0; len > 1; len -= 2)
-		sum += *buf++;
-
-	if (len == 1)
-		sum += *(unsigned char *)buf;
-
-	sum = (sum >> 16) + (sum & 0xFFFF);
-	sum += (sum >> 16);
-	result = ~sum;
-
-	return  (__odp_force odp_u16sum_t) result;
-}
-
-/**
  * General Purpose TCP/UDP checksum function
  *
  * This function handles all the different checksum operations like
