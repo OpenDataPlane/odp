0e8647378bfbe730101e5f089e134356dea00cda
Kevin Wang <kevin.wang@arm.com>
2017-11-22 15:21:28 +0800

    linux-gen: pool: rearrange pool_t to separate control and data plane
    Separate the pool's control plane and data plane data structures.
    This avoids sharing control plane related data and dataplane
    related data on the same cache line.
    
    Signed-off-by: Kevin Wang <kevin.wang@arm.com>
    Reviewed-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
    Reviewed-by: Ola Liljedahl <ola.Liljedahl@arm.com>
    Reviewed-by: Brian Brooks <brian.brooks@arm.com>
    Reviewed-by: Yi He <yi.he@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>

--- platform/linux-generic/include/odp_pool_internal.h
+++ platform/linux-generic/include/odp_pool_internal.h
@@ -54,34 +54,33 @@ typedef struct pool_t {
 	char             name[ODP_POOL_NAME_LEN];
 	odp_pool_param_t params;
 	odp_pool_t       pool_hdl;
-	uint32_t         pool_idx;
-	uint32_t         ring_mask;
 	odp_shm_t        shm;
 	odp_shm_t        uarea_shm;
-	int              reserved;
 	uint32_t         num;
 	uint32_t         align;
+	uint32_t         shm_size;
+	uint32_t         uarea_shm_size;
+	odp_shm_t        ring_shm;
+	/* Used by DPDK zero-copy pktio */
+	pool_destroy_cb_fn ext_destroy;
+	void            *ext_desc;
+
+	/* Below is for data plane */
+	uint8_t         *base_addr ODP_ALIGNED_CACHE;
+	uint8_t         *uarea_base_addr;
+	pool_ring_t     *ring;
 	uint32_t         headroom;
 	uint32_t         tailroom;
 	uint32_t         seg_len;
 	uint32_t         max_len;
 	uint32_t         uarea_size;
 	uint32_t         block_size;
-	uint32_t         shm_size;
-	uint32_t         uarea_shm_size;
-	uint8_t         *base_addr;
-	uint8_t         *uarea_base_addr;
-
-	/* Used by DPDK zero-copy pktio */
+	uint32_t         pool_idx;
+	uint32_t         ring_mask;
+	uint8_t          reserved;
 	uint8_t		mem_from_huge_pages;
-	pool_destroy_cb_fn ext_destroy;
-	void            *ext_desc;
 
 	pool_cache_t     local_cache[ODP_THREAD_COUNT_MAX];
-
-	odp_shm_t        ring_shm;
-	pool_ring_t     *ring;
-
 } pool_t;
 
 typedef struct pool_table_t {
