a4f2c5f38c8fd737c49abc717d69c2ff0491a8fe
Petri Savolainen <petri.savolainen@linaro.org>
2017-12-12 15:12:37 +0200

    linux-gen: queue: lock-free implementation
    Simple implementation of non-blocking, lock-free plain queues.
    Enqueues are done freely to any free ring node with an
    atomically increasing counter value. Dequeue operation finds
    the node with lowest counter value. Implementation requires
    lockfree 128 bit atomics. Lock-free queues are not supported
    when those are not available.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp_queue_internal.h
+++ platform/linux-generic/include/odp_queue_internal.h
@@ -54,6 +54,7 @@ struct queue_entry_s {
 	odp_queue_param_t param;
 	odp_pktin_queue_t pktin;
 	odp_pktout_queue_t pktout;
+	void             *queue_lf;
 	char              name[ODP_QUEUE_NAME_LEN];
 };
 
