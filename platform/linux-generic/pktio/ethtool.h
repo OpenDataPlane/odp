fcbc819def4620fbd4a261536e3fec4a213bde65
Mykyta Iziumtsev <mykyta.iziumtsev@linaro.org>
2017-12-20 14:44:51 +0100

    linux-gen: extend ethtool helper functions
    Add helper function to query RX/TX queue lengths on network interface to
    facilitate upcoming native and mediated device drivers.
    
    Signed-off-by: Mykyta Iziumtsev <mykyta.iziumtsev@linaro.org>

--- platform/linux-generic/pktio/ethtool.h
+++ platform/linux-generic/pktio/ethtool.h
@@ -7,9 +7,32 @@
 #ifndef ODP_PKTIO_ETHTOOL_H_
 #define ODP_PKTIO_ETHTOOL_H_
 
+#include <linux/ethtool.h>
+
 /**
- * Get ethtool statistics of a packet socket
+ * Get statistics for a network interface
+ *
+ * @param fd		Arbitrary socket file descriptor
+ * @param netif_name    Network interface name
+ * @param stats[out]    Output buffer for counters
+ *
+ * @retval 0 on success
+ * @retval != 0 on failure
  */
-int ethtool_stats_get_fd(int fd, const char *name, odp_pktio_stats_t *stats);
+int ethtool_stats_get_fd(int fd, const char *netif_name,
+			 odp_pktio_stats_t *stats);
+
+/**
+ * Get RX/TX queue parameters for a network interface
+ *
+ * @param fd		Arbitrary socket file descriptor
+ * @param netif_name	Network interface name
+ * @param param[out]	Output buffer for parameters
+ *
+ * @retval 0 on success
+ * @retval != 0 on failure
+ */
+int ethtool_ringparam_get_fd(int fd, const char *netif_name,
+			     struct ethtool_ringparam *param);
 
 #endif /*ODP_PKTIO_ETHTOOL_H_*/
