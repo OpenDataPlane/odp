2bbecec83bc1d8ca1b45f98214d2dbbe13950e25
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2017-12-30 03:44:34 +0300

    include: inline compiler.h spec
    compiler.h header contains one internal definition used only in
    platform-optimized headers. This definition, being internal, is not a
    part of the specification, so let's merge it into linux-gen plat file
    and drop compiler.h headers.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp/api/plat/byteorder_inlines.h
+++ platform/linux-generic/include/odp/api/plat/byteorder_inlines.h
@@ -21,11 +21,27 @@ extern "C" {
 #define __odp_force
 #endif
 
-#include <odp/api/compiler.h>
-
 /** @ingroup odp_compiler_optim
  *  @{
  */
+
+/** @internal GNU compiler version */
+#define GCC_VERSION (__GNUC__ * 10000 \
+			+ __GNUC_MINOR__ * 100 \
+			+ __GNUC_PATCHLEVEL__)
+
+/**
+ * @internal
+ * Compiler __builtin_bswap16() is not available on all platforms
+ * until GCC 4.8.0 - work around this by offering __odp_builtin_bswap16()
+ * Don't use this function directly, instead see odp_byteorder.h
+ */
+#if GCC_VERSION < 40800
+#define __odp_builtin_bswap16(u16) ((((u16)&0x00ff) << 8) | \
+				    (((u16)&0xff00) >> 8))
+#else
+#define __odp_builtin_bswap16(u16) __builtin_bswap16(u16)
+#endif
 
 _ODP_INLINE uint16_t odp_be_to_cpu_16(odp_u16be_t be16)
 {
