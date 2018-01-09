dc227c3fe06af689569b48170c3cb377b39c6f6a
Bill Fischofer <bill.fischofer@linaro.org>
2018-01-09 17:40:32 -0600

    linux-generic: remove camelcase from icmp protocol
    Change ODP_IPPROTO_ICMPv4 and ODP_IPPROTO_ICMPv6 to
    ODP_IPPROTO_ICMPV4 and ODP_IPPROTO_ICMPV6 to eliminate camelcase
    issues in ODP code.
    
    Signed-off-by: Bill Fischofer <bill.fischofer@linaro.org>

--- platform/linux-generic/include/protocols/ip.h
+++ platform/linux-generic/include/protocols/ip.h
@@ -157,7 +157,7 @@ typedef struct ODP_PACKED {
  * IP protocol values (IPv4:'proto' or IPv6:'next_hdr')
  * @{*/
 #define _ODP_IPPROTO_HOPOPTS 0x00 /**< IPv6 hop-by-hop options */
-#define _ODP_IPPROTO_ICMPv4  0x01 /**< Internet Control Message Protocol (1) */
+#define _ODP_IPPROTO_ICMPV4  0x01 /**< Internet Control Message Protocol (1) */
 #define _ODP_IPPROTO_IPIP    0x04 /**< IP Encapsulation within IP (4) */
 #define _ODP_IPPROTO_TCP     0x06 /**< Transmission Control Protocol (6) */
 #define _ODP_IPPROTO_UDP     0x11 /**< User Datagram Protocol (17) */
@@ -166,7 +166,7 @@ typedef struct ODP_PACKED {
 #define _ODP_IPPROTO_FRAG    0x2C /**< IPv6 Fragment (44) */
 #define _ODP_IPPROTO_AH      0x33 /**< Authentication Header (51) */
 #define _ODP_IPPROTO_ESP     0x32 /**< Encapsulating Security Payload (50) */
-#define _ODP_IPPROTO_ICMPv6  0x3A /**< Internet Control Message Protocol (58) */
+#define _ODP_IPPROTO_ICMPV6  0x3A /**< Internet Control Message Protocol (58) */
 #define _ODP_IPPROTO_DEST    0x3C /**< IPv6 Destination header (60) */
 #define _ODP_IPPROTO_SCTP    0x84 /**< Stream Control Transmission protocol
 				       (132) */
