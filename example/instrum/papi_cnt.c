bf73df07c34e893e29922984cacaa702d4747b57
Bogdan Pricope <bogdan.pricope@linaro.org>
2018-01-15 09:55:51 +0200

    example: instrum: configure papi event set
    Configure the set of papi counters to be acquired via an
    environment variable.
    
    Signed-off-by: Bogdan Pricope <bogdan.pricope@linaro.org>

--- example/instrum/papi_cnt.c
+++ example/instrum/papi_cnt.c
@@ -6,17 +6,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
 #include <papi.h>
 #include <papi_cnt.h>
 
-static int papi_event_tab[SAMPLE_COUNTER_TAB_SIZE] = {PAPI_BR_CN, PAPI_L2_DCM};
+#define PAPI_EVENTS_ENV "ODP_INSTRUM_PAPI_EVENTS"
+
+#define PAPI_EVENT_TAB_SIZE_DFLT 2
+int papi_event_tab_dflt[PAPI_EVENT_TAB_SIZE_DFLT] = {PAPI_BR_CN, PAPI_L2_DCM};
+
+static int papi_event_tab[SAMPLE_COUNTER_TAB_SIZE];
+static int papi_event_tab_size;
 
 static __thread int event_set = PAPI_NULL;
 
 int papi_init(void)
 {
 	int retval, i;
+	char *papi_events_env = NULL;
 
 	retval = PAPI_library_init(PAPI_VER_CURRENT);
 	if (retval != PAPI_VER_CURRENT) {
@@ -35,7 +43,28 @@ int papi_init(void)
 		goto err_shutdown;
 	}
 
-	for (i = 0; i < SAMPLE_COUNTER_TAB_SIZE; i++) {
+	papi_events_env = getenv(PAPI_EVENTS_ENV);
+	if (papi_events_env) {
+		char *tk = strtok(papi_events_env, ",");
+		int papi_event;
+
+		while (tk != NULL &&
+		       papi_event_tab_size < SAMPLE_COUNTER_TAB_SIZE) {
+			if (PAPI_event_name_to_code(tk, &papi_event) == PAPI_OK)
+				papi_event_tab[papi_event_tab_size++] =
+					papi_event;
+
+			tk = strtok(NULL, ",");
+		}
+	}
+
+	if (!papi_event_tab_size) {
+		for (i = 0; i < PAPI_EVENT_TAB_SIZE_DFLT; i++)
+			papi_event_tab[i] = papi_event_tab_dflt[i];
+		papi_event_tab_size = PAPI_EVENT_TAB_SIZE_DFLT;
+	}
+
+	for (i = 0; i < papi_event_tab_size; i++) {
 		retval = PAPI_query_event(papi_event_tab[i]);
 		if (retval != PAPI_OK) {
 			printf("PAPI_query_event %d - error\n", i);
@@ -75,7 +104,7 @@ int papi_init_local(void)
 	}
 
 	retval = PAPI_add_events(event_set, papi_event_tab,
-				 SAMPLE_COUNTER_TAB_SIZE);
+				 papi_event_tab_size);
 	if (retval != PAPI_OK) {
 		printf("PAPI_add_events error: %d\n", retval);
 		goto err_clean_evset;
@@ -103,7 +132,7 @@ int papi_term_local(void)
 	if (PAPI_stop(event_set, last_counters) == PAPI_OK) {
 		int i;
 
-		for (i = 0; i < SAMPLE_COUNTER_TAB_SIZE; i++)
+		for (i = 0; i < papi_event_tab_size; i++)
 			printf("Counter[%d] = %lld\n", i, last_counters[i]);
 	}
 
@@ -111,6 +140,11 @@ int papi_term_local(void)
 	PAPI_destroy_eventset(&event_set);
 
 	return 0;
+}
+
+int papi_counters_cnt(void)
+{
+	return papi_event_tab_size;
 }
 
 int papi_sample_start(profiling_sample_t *spl)
@@ -134,7 +168,7 @@ int papi_sample_end(profiling_sample_t *spl)
 		return -1;
 	}
 
-	for (i = 0; i < SAMPLE_COUNTER_TAB_SIZE; i++)
+	for (i = 0; i < papi_event_tab_size; i++)
 		spl->counters[i] = end_counters[i] - spl->counters[i];
 
 	spl->diff_cyc = end_cyc - spl->diff_cyc;
