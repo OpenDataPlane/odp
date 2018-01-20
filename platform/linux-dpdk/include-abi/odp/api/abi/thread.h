184ecc43ef67401b14caab9168b1bf8b242c3679
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-01-20 18:36:07 +0300

    linux-dpdk: add ABI files
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

---
+++ platform/linux-dpdk/include-abi/odp/api/abi/thread.h
@@ -0,0 +1,9 @@
+/* Copyright (c) 2017, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include <odp/api/abi-default/thread.h>
+
+#include <odp/api/plat/thread_inlines.h>
