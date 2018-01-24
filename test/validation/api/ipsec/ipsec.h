e721aed9b240d2bd1a5a25f27b1917fa9c3b72b5
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-01-24 08:03:19 +0300

    validation: ipsec: add ChaCha20-Poly1305 test vectors
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- test/validation/api/ipsec/ipsec.h
+++ test/validation/api/ipsec/ipsec.h
@@ -91,5 +91,6 @@ int ipsec_check_esp_aes_gcm_128(void);
 int ipsec_check_esp_aes_gcm_256(void);
 int ipsec_check_ah_aes_gmac_128(void);
 int ipsec_check_esp_null_aes_gmac_128(void);
+int ipsec_check_esp_chacha20_poly1305(void);
 
 #endif
