fb6a1bdc107c576d1ca64271bda6fe2691ca6a97
Matias Elo <matias.elo@nokia.com>
2018-02-02 13:12:34 +0200

    linux-dpdk: errno: add missing copyright and include guards
    Signed-off-by: Matias Elo <matias.elo@nokia.com>

--- platform/linux-dpdk/include/odp_errno_define.h
+++ platform/linux-dpdk/include/odp_errno_define.h
@@ -1,4 +1,28 @@
+/* Copyright (c) 2018, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP error number define
+ */
+
+#ifndef ODP_ERRNO_DEFINE_H_
+#define ODP_ERRNO_DEFINE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <rte_errno.h>
 
 #define __odp_errno (rte_errno)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
