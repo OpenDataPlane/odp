0c897c3a8ea85956c98dad03814b786f040e5793
Matias Elo <matias.elo@nokia.com>
2018-02-08 15:09:15 +0200

    Port moving of packet_to_buffer() function
    Port a38ea82 "linux-gen: move packet_to_buffer to packet_inlines.h"
    Port 886e541 "linux-generic: packet: add missing doxygen from inline func"
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-dpdk/include/odp/api/plat/packet_inlines.h
+++ platform/linux-dpdk/include/odp/api/plat/packet_inlines.h
@@ -251,6 +251,12 @@ static inline void _odp_packet_prefetch(odp_packet_t pkt, uint32_t offset, uint3
 		rte_prefetch0(addr + ofs);
 }
 
+/** @internal Inline function @param pkt @return */
+static inline odp_buffer_t packet_to_buffer(odp_packet_t pkt)
+{
+	return (odp_buffer_t)pkt;
+}
+
 /* Include inlined versions of API functions */
 #include <odp/api/plat/static_inline.h>
 
--- platform/linux-dpdk/odp_packet.c
+++ platform/linux-dpdk/odp_packet.c
@@ -83,11 +83,6 @@ odp_packet_t _odp_packet_from_buf_hdr(odp_buffer_hdr_t *buf_hdr)
 	return (odp_packet_t)buf_hdr;
 }
 
-static inline odp_buffer_t packet_to_buffer(odp_packet_t pkt)
-{
-	return (odp_buffer_t)pkt;
-}
-
 static odp_packet_t packet_alloc(pool_t *pool, uint32_t len)
 {
 	odp_packet_t pkt;
