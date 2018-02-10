3c39f7df4fe10ee2430f750ca69337a773f800be
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-02-10 11:30:01 +0300

    shippable: run tests with all possible schedulers
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- scripts/shippable-post.sh
+++ scripts/shippable-post.sh
@@ -2,8 +2,14 @@
 
 wget https://raw.githubusercontent.com/shawnliang/cunit-to-junit/master/cunit-to-junit.xsl
 
+mkdir -p "$SHIPPABLE_BUILD_DIR/shippable/testresults"
+
+SCHED=${1:-default}
+echo $SCHED
+
 for FILE in `find  ./test ./platform/ -name  "*.xml"`; do
 	bname="`basename $FILE`";
+	echo Processing $FILE as ${SCHED}-${bname}
 	xsltproc --novalid cunit-to-junit.xsl "$FILE" > \
-		"$SHIPPABLE_BUILD_DIR/shippable/testresults/${bname}"
+		"$SHIPPABLE_BUILD_DIR/shippable/testresults/${SCHED}-${bname}"
 done
