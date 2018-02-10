d8dd54f2325cf859703721e7a17b6cbe734d857a
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-02-10 03:57:24 +0300

    test: misc: use C++ I/O instead of C
    To verify that this test is really compiled using C++ compiler use cout
    instead of printf.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/miscellaneous/odp_api_from_cpp.cpp
+++ test/miscellaneous/odp_api_from_cpp.cpp
@@ -1,11 +1,11 @@
-#include <cstdio>
+#include <iostream>
 #include <odp_api.h>
 #include <odp/helper/odph_api.h>
 
 int main(int argc ODP_UNUSED, const char *argv[] ODP_UNUSED)
 {
-	printf("\tODP API version: %s\n", odp_version_api_str());
-	printf("\tODP implementation version: %s\n", odp_version_impl_str());
+	std::cout << "\tODP API version: " << odp_version_api_str() << std::endl;
+	std::cout << "\tODP implementation version: " << odp_version_impl_str() << std::endl;
 
 	return 0;
 }
