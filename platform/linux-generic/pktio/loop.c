144834277c34949cd207c8a4bef448badb03b277
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-02-14 11:03:14 +0300

    linux-gen: pktio: loop: adapt to packet flags internal changes
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- platform/linux-generic/pktio/loop.c
+++ platform/linux-generic/pktio/loop.c
@@ -156,7 +156,7 @@ static int loopback_recv(pktio_entry_t *pktio_entry, int index ODP_UNUSED,
 
 		/* Try IPsec inline processing */
 		if (pktio_entry->s.config.inbound_ipsec &&
-		    !pkt_hdr->p.error_flags.ip_err &&
+		    !pkt_hdr->p.flags.ip_err &&
 		    _odp_packet_has_ipsec(pkt))
 			_odp_ipsec_try_inline(&pkt);
 
