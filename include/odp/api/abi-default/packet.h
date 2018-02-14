570fb3e6e0fdc1b73ce813d205a57ba001261e82
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-02-14 10:06:58 +0300

    api: packet add L2 packet type to complement L3 and L4 types
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Reviewed-by: Balasubramanian Manoharan <bala.manoharan@linaro.org>
    Reviewed-by: Petri Savolainen <petri.savolainen@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- include/odp/api/abi-default/packet.h
+++ include/odp/api/abi-default/packet.h
@@ -30,6 +30,11 @@ typedef _odp_abi_packet_seg_t *odp_packet_seg_t;
 #define ODP_PACKET_SEG_INVALID    ((odp_packet_seg_t)0xffffffff)
 #define ODP_PACKET_OFFSET_INVALID 0xffff
 
+typedef uint8_t odp_proto_l2_type_t;
+
+#define ODP_PROTO_L2_TYPE_NONE   0
+#define ODP_PROTO_L2_TYPE_ETH    1
+
 typedef uint8_t odp_proto_l3_type_t;
 
 #define ODP_PROTO_L3_TYPE_NONE   0
