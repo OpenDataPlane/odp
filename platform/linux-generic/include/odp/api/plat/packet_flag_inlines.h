37cfa868cf1c881ac3c29306b5b8ff24c5e36b52
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-04-22 20:05:34 +0300

    linux-gen: packet_flags: remove separate API inlining header
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>

--- platform/linux-generic/include/odp/api/plat/packet_flag_inlines.h
+++ platform/linux-generic/include/odp/api/plat/packet_flag_inlines.h
@@ -14,7 +14,7 @@
 #define _ODP_PLAT_PACKET_FLAG_INLINES_H_
 
 #include <odp/api/abi/packet.h>
-#include <odp/api/plat/packet_inlines.h>
+#include <odp/api/plat/packet_inline_types.h>
 #include <odp/api/hints.h>
 
 /** @internal Inline function offsets */
@@ -26,8 +26,21 @@ static inline uint64_t _odp_packet_input_flags(odp_packet_t pkt)
 	return _odp_pkt_get(pkt, uint64_t, input_flags);
 }
 
+#ifndef _ODP_NO_INLINE
+	/* Inline functions by default */
+	#define _ODP_INLINE static inline
+	#define odp_packet_has_l2 __odp_packet_has_l2
+	#define odp_packet_has_eth __odp_packet_has_eth
+	#define odp_packet_has_jumbo __odp_packet_has_jumbo
+	#define odp_packet_has_flow_hash __odp_packet_has_flow_hash
+	#define odp_packet_has_ts __odp_packet_has_ts
+	#define odp_packet_has_ipsec __odp_packet_has_ipsec
+#else
+	#define _ODP_INLINE
+#endif
+
 /** @internal Inline function @param pkt @return */
-static inline int _odp_packet_has_l2(odp_packet_t pkt)
+_ODP_INLINE int odp_packet_has_l2(odp_packet_t pkt)
 {
 	_odp_packet_input_flags_t flags;
 
@@ -36,7 +49,7 @@ static inline int _odp_packet_has_l2(odp_packet_t pkt)
 }
 
 /** @internal Inline function @param pkt @return */
-static inline int _odp_packet_has_eth(odp_packet_t pkt)
+_ODP_INLINE int odp_packet_has_eth(odp_packet_t pkt)
 {
 	_odp_packet_input_flags_t flags;
 
@@ -45,7 +58,7 @@ static inline int _odp_packet_has_eth(odp_packet_t pkt)
 }
 
 /** @internal Inline function @param pkt @return */
-static inline int _odp_packet_has_jumbo(odp_packet_t pkt)
+_ODP_INLINE int odp_packet_has_jumbo(odp_packet_t pkt)
 {
 	_odp_packet_input_flags_t flags;
 
@@ -54,7 +67,7 @@ static inline int _odp_packet_has_jumbo(odp_packet_t pkt)
 }
 
 /** @internal Inline function @param pkt @return */
-static inline int _odp_packet_has_flow_hash(odp_packet_t pkt)
+_ODP_INLINE int odp_packet_has_flow_hash(odp_packet_t pkt)
 {
 	_odp_packet_input_flags_t flags;
 
@@ -63,7 +76,7 @@ static inline int _odp_packet_has_flow_hash(odp_packet_t pkt)
 }
 
 /** @internal Inline function @param pkt @return */
-static inline int _odp_packet_has_ts(odp_packet_t pkt)
+_ODP_INLINE int odp_packet_has_ts(odp_packet_t pkt)
 {
 	_odp_packet_input_flags_t flags;
 
@@ -72,7 +85,7 @@ static inline int _odp_packet_has_ts(odp_packet_t pkt)
 }
 
 /** @internal Inline function @param pkt @return */
-static inline int _odp_packet_has_ipsec(odp_packet_t pkt)
+_ODP_INLINE int odp_packet_has_ipsec(odp_packet_t pkt)
 {
 	_odp_packet_input_flags_t flags;
 
