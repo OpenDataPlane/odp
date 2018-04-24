da730188aa6fb9f58145a697ad002ecfd102f898
Balasubramanian Manoharan <bala.manoharan@linaro.org>
2018-04-24 20:09:37 +0530

    api: comp: compression specification
    ODP Compression specification
    
    Signed-off-by: Balasubramanian Manoharan <bala.manoharan@linaro.org>
    Signed-off-by: Shally Verma <shally.verma@cavium.com>
    Signed-off-by: Mahipal Challa <mahipal.challa@cavium.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@linaro.org>
    Reviewed-by: Bogdan Pricope <bogdan.pricope@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ include/odp/api/abi-default/comp.h
@@ -0,0 +1,35 @@
+/* Copyright (c) 2018, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef ODP_ABI_COMP_H_
+#define ODP_ABI_COMP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+/** @internal Dummy type for strong typing */
+typedef struct { char dummy; /**< @internal Dummy */ } _odp_abi_comp_session_t;
+
+/** @ingroup odp_compression
+ *  @{
+ */
+
+typedef _odp_abi_comp_session_t *odp_comp_session_t;
+
+#define ODP_COMP_SESSION_INVALID        ((odp_comp_session_t)0)
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
