ad3417a6fd2ba93616b937f9a2d8642eceeb0bf8
Bogdan Pricope <bogdan.pricope@linaro.org>
2018-05-10 13:52:43 +0300

    linux-gen: pktio: dpdk: integrate csum with packet parser
    Merge csum validation and packet parser to reduce branching.
    
    Signed-off-by: Bogdan Pricope <bogdan.pricope@linaro.org>
    Reviewed-and-tested-by: Matias Elo <matias.elo@nokia.com>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp_packet_dpdk.h
+++ platform/linux-generic/include/odp_packet_dpdk.h
@@ -78,16 +78,18 @@ int dpdk_packet_parse_common(packet_parser_t *pkt_hdr,
 			     uint32_t pkt_len,
 			     uint32_t seg_len,
 			     struct rte_mbuf *mbuf,
-			     int layer);
+			     int layer,
+			     odp_pktin_config_opt_t pktin_cfg);
 
 static inline int dpdk_packet_parse_layer(odp_packet_hdr_t *pkt_hdr,
 					  struct rte_mbuf *mbuf,
-					  odp_pktio_parser_layer_t layer)
+					  odp_pktio_parser_layer_t layer,
+					  odp_pktin_config_opt_t pktin_cfg)
 {
 	uint32_t seg_len = pkt_hdr->buf_hdr.seg[0].len;
 	void *base = pkt_hdr->buf_hdr.seg[0].data;
 
 	return dpdk_packet_parse_common(&pkt_hdr->p, base, pkt_hdr->frame_len,
-					seg_len, mbuf, layer);
+					seg_len, mbuf, layer, pktin_cfg);
 }
 #endif
