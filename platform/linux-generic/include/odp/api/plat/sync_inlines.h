7299adcc939a3d55b2c694744037ea9ea1dd7835
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-05-15 00:26:21 +0300

    linux-gen: sync: rework code to follow inlining style
    Rework sync module to follow _ODP_INLINE style of code used by the rest
    of ODP codebase.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp/api/plat/sync_inlines.h
+++ platform/linux-generic/include/odp/api/plat/sync_inlines.h
@@ -17,9 +17,17 @@
 extern "C" {
 #endif
 
-/** @ingroup odp_barrier
- *  @{
- */
+/** @cond _ODP_HIDE_FROM_DOXYGEN_ */
+
+#ifndef _ODP_NO_INLINE
+	/* Inline functions by default */
+	#define _ODP_INLINE static inline
+	#define odp_mb_release __odp_mb_release
+	#define odp_mb_acquire __odp_mb_acquire
+	#define odp_mb_full __odp_mb_full
+#else
+	#define _ODP_INLINE
+#endif
 
 _ODP_INLINE void odp_mb_release(void)
 {
@@ -36,9 +44,7 @@ _ODP_INLINE void odp_mb_full(void)
 	__atomic_thread_fence(__ATOMIC_SEQ_CST);
 }
 
-/**
- * @}
- */
+/** @endcond */
 
 #ifdef __cplusplus
 }
