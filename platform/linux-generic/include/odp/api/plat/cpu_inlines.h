42a8b2f0dbaee653953170cbd0a7fa659bc32b38
Petri Savolainen <petri.savolainen@linaro.org>
2018-05-25 11:25:15 +0300

    linux-gen: cpu: inline cpu_cycles_diff
    Moved the last function from .c file to inline header and
    removed .c file.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- platform/linux-generic/include/odp/api/plat/cpu_inlines.h
+++ platform/linux-generic/include/odp/api/plat/cpu_inlines.h
@@ -7,6 +7,8 @@
 #ifndef ODP_PLAT_CPU_INLINES_H_
 #define ODP_PLAT_CPU_INLINES_H_
 
+#include <odp/api/hints.h>
+
 /** @cond _ODP_HIDE_FROM_DOXYGEN_ */
 
 #ifndef _ODP_NO_INLINE
@@ -16,11 +18,20 @@
 	#define odp_cpu_cycles            __odp_cpu_cycles
 	#define odp_cpu_cycles_max        __odp_cpu_cycles_max
 	#define odp_cpu_cycles_resolution __odp_cpu_cycles_resolution
+	#define odp_cpu_cycles_diff       __odp_cpu_cycles_diff
 #else
 	#define _ODP_INLINE
 #endif
 
 #include <odp/api/abi/cpu_inlines.h>
+
+_ODP_INLINE uint64_t odp_cpu_cycles_diff(uint64_t c2, uint64_t c1)
+{
+	if (odp_likely(c2 >= c1))
+		return c2 - c1;
+
+	return c2 + (odp_cpu_cycles_max() - c1) + 1;
+}
 
 /** @endcond */
 
