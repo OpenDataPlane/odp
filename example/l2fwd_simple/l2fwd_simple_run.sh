199625f1dd7fef3e6f9d6e16e9dbac049497f1c2
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-06-05 14:55:12 +0300

    example: l2fwd_simple remove predefined sleep
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- example/l2fwd_simple/l2fwd_simple_run.sh
+++ example/l2fwd_simple/l2fwd_simple_run.sh
@@ -10,11 +10,7 @@ PCAP_IN=`find . ${TEST_DIR} $(dirname $0) -name udp64.pcap -print -quit`
 echo "using PCAP_IN = ${PCAP_IN}"
 
 ./odp_l2fwd_simple${EXEEXT} pcap:in=${PCAP_IN} pcap:out=pcapout.pcap \
-	02:00:00:00:00:01 02:00:00:00:00:02 &
-
-sleep 2
-kill -s SIGINT $!
-wait $!
+	02:00:00:00:00:01 02:00:00:00:00:02 -t 2
 STATUS=$?
 
 if [ "$STATUS" -ne 0 ]; then
@@ -30,15 +26,11 @@ fi
 rm -f pcapout.pcap
 
 ./odp_l2fwd_simple${EXEEXT} null:0 null:1 \
-	02:00:00:00:00:01 02:00:00:00:00:02 &
-
-sleep 2
-kill -s SIGINT $!
-wait $!
+	02:00:00:00:00:01 02:00:00:00:00:02 -t 2
 STATUS=$?
 
-if [ "$STATUS" -ne 255 ]; then
-  echo "Error: status was: $STATUS, expected 255"
+if [ "$STATUS" -ne 0 ]; then
+  echo "Error: status was: $STATUS, expected 0"
   exit 1
 fi
 
