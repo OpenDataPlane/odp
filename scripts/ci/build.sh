97fc51b1e5b3fcea9db99ba63c10bda47f79341a
Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
2018-08-27 04:39:25 +0300

    ci: do compile-after-install test
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -6,6 +6,17 @@ cd "$(dirname "$0")"/../..
 ./configure \
 	--host=${TARGET_ARCH} --build=x86_64-linux-gnu \
 	--enable-dpdk \
+	--prefix=/opt/odp \
 	${CONF}
 
 make -j 8
+
+make install
+
+pushd ${HOME}
+${CC} ${CFLAGS} ${OLDPWD}/example/hello/odp_hello.c -o odp_hello_inst_dynamic `PKG_CONFIG_PATH=/opt/odp/lib/pkgconfig:${PKG_CONFIG_PATH} pkg-config --cflags --libs libodp-linux`
+if [ -z "$TARGET_ARCH" ]
+then
+	LD_LIBRARY_PATH="/opt/odp/lib:$LD_LIBRARY_PATH" ./odp_hello_inst_dynamic
+fi
+popd
