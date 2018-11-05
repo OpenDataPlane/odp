8dddf48f616101d28d4dd8406ccdb9f6f1b05d84
Petri Savolainen <petri.savolainen@linaro.org>
2018-11-05 15:44:05 +0200

    linux-gen: ipsec: use new auth_key_extra
    Change implementation and validation test to use the new
    authentication algorithm extra keying material. It is set only
    in GMAC test cases.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@linaro.org>
    Reviewed-by: Dmitry Eremin-Solenikov <dmitry.ereminsolenikov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- test/performance/odp_ipsec.c
+++ test/performance/odp_ipsec.c
@@ -345,7 +345,7 @@ static ipsec_alg_config_t algs_config[] = {
 				.data = test_key16,
 				.length = sizeof(test_key16)
 			},
-			.cipher_key_extra = {
+			.auth_key_extra = {
 				.data = test_salt,
 				.length = 4,
 			},
