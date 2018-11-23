6f9209f69f69ba8cfe51ff440f978b7d82d13245
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-11-23 22:34:16 +0300

    travis: check.sh request huge pages at early start
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>
    Reviewed-by: Bill Fischofer <bill.fischofer@linaro.org>

--- scripts/ci/check.sh
+++ scripts/ci/check.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-"`dirname "$0"`"/build_x86_64.sh
-
-cd "$(dirname "$0")"/../..
-
 echo 1000 | tee /proc/sys/vm/nr_hugepages
 mkdir -p /mnt/huge
 mount -t hugetlbfs nodev /mnt/huge
+
+"`dirname "$0"`"/build_x86_64.sh
+
+cd "$(dirname "$0")"/../..
 
 # Ignore possible failures there because these tests depends on measurements
 # and systems might differ in performance.
