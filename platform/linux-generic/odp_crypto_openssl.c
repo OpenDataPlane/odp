73113175e9701586380f9af9e392fc1b1d7e928c
Dmitry Eremin-Solenikov <deremin-solenikov@cavium.com>
2019-04-06 14:55:04 +0300

    linux-gen: crypto: add HMAC-SHA224 algorithm support
    Signed-off-by: Dmitry Eremin-Solenikov <deremin-solenikov@cavium.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- platform/linux-generic/odp_crypto_openssl.c
+++ platform/linux-generic/odp_crypto_openssl.c
@@ -100,6 +100,9 @@ static const odp_crypto_auth_capability_t auth_capa_sha1_hmac[] = {
 {.digest_len = 12, .key_len = 20, .aad_len = {.min = 0, .max = 0, .inc = 0} },
 {.digest_len = 20, .key_len = 20, .aad_len = {.min = 0, .max = 0, .inc = 0} } };
 
+static const odp_crypto_auth_capability_t auth_capa_sha224_hmac[] = {
+{.digest_len = 28, .key_len = 28, .aad_len = {.min = 0, .max = 0, .inc = 0} } };
+
 static const odp_crypto_auth_capability_t auth_capa_sha256_hmac[] = {
 {.digest_len = 16, .key_len = 32, .aad_len = {.min = 0, .max = 0, .inc = 0} },
 {.digest_len = 32, .key_len = 32, .aad_len = {.min = 0, .max = 0, .inc = 0} } };
@@ -1358,6 +1361,7 @@ int odp_crypto_capability(odp_crypto_capability_t *capa)
 	capa->auths.bit.null         = 1;
 	capa->auths.bit.md5_hmac     = 1;
 	capa->auths.bit.sha1_hmac    = 1;
+	capa->auths.bit.sha224_hmac  = 1;
 	capa->auths.bit.sha256_hmac  = 1;
 	capa->auths.bit.sha384_hmac  = 1;
 	capa->auths.bit.sha512_hmac  = 1;
@@ -1457,6 +1461,10 @@ int odp_crypto_auth_capability(odp_auth_alg_t auth,
 	case ODP_AUTH_ALG_SHA1_HMAC:
 		src = auth_capa_sha1_hmac;
 		num = sizeof(auth_capa_sha1_hmac) / size;
+		break;
+	case ODP_AUTH_ALG_SHA224_HMAC:
+		src = auth_capa_sha224_hmac;
+		num = sizeof(auth_capa_sha224_hmac) / size;
 		break;
 	case ODP_AUTH_ALG_SHA256_HMAC:
 		src = auth_capa_sha256_hmac;
@@ -1676,6 +1684,9 @@ odp_crypto_session_create(odp_crypto_session_param_t *param,
 		break;
 	case ODP_AUTH_ALG_SHA1_HMAC:
 		rc = process_auth_hmac_param(session, EVP_sha1());
+		break;
+	case ODP_AUTH_ALG_SHA224_HMAC:
+		rc = process_auth_hmac_param(session, EVP_sha224());
 		break;
 #if ODP_DEPRECATED_API
 	case ODP_AUTH_ALG_SHA256_128:
