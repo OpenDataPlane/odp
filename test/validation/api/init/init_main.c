7218168b9bccd7ddcca7acf24883cb8c216f787e
Petri Savolainen <petri.savolainen@nokia.com>
2019-04-15 14:25:35 +0300

    validation: init: remove separate files per test case
    Combining all test cases into a single source file
    (init_main.c). Create (still) separate binaries per test
    case as some implementations may assume that an application
    process won't call odp_global_init() more than once.
    
    Added missing local init/term calls to all test cases. Removed
    non-API compatible assumptions from log test:
     - ODP_DEBUG_PRINT is not defined in the API
     - API does not require odp_init_global() to output anything
       to the log
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

---
+++ test/validation/api/init/init_main.c
@@ -0,0 +1,153 @@
+/* Copyright (c) 2015-2018, Linaro Limited
+ * Copyright (c) 2019, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include "config.h"
+
+#include <odp_api.h>
+#include <odp_cunit_common.h>
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+/* Replacement abort function */
+static void ODP_NORETURN my_abort_func(void)
+{
+	abort();
+}
+
+/* Replacement log function */
+ODP_PRINTF_FORMAT(2, 3)
+static int my_log_func(odp_log_level_t level __attribute__((unused)),
+		       const char *fmt, ...)
+{
+	va_list args;
+	int r;
+
+	va_start(args, fmt);
+	r = vfprintf(stderr, fmt, args);
+	va_end(args);
+
+	return r;
+}
+
+static void init_test_defaults(void)
+{
+	int ret;
+	odp_instance_t instance;
+	odp_init_t param;
+
+	odp_init_param_init(&param);
+
+	ret = odp_init_global(&instance, &param, NULL);
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_init_local(instance, ODP_THREAD_WORKER);
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_term_local();
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_term_global(instance);
+	CU_ASSERT(ret == 0);
+}
+
+static void init_test_abort(void)
+{
+	int ret;
+	odp_instance_t instance;
+	odp_init_t param;
+
+	odp_init_param_init(&param);
+	param.abort_fn = &my_abort_func;
+
+	ret = odp_init_global(&instance, &param, NULL);
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_init_local(instance, ODP_THREAD_WORKER);
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_term_local();
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_term_global(instance);
+	CU_ASSERT(ret == 0);
+}
+
+static void init_test_log(void)
+{
+	int ret;
+	odp_instance_t instance;
+	odp_init_t param;
+
+	odp_init_param_init(&param);
+	param.log_fn = &my_log_func;
+
+	ret = odp_init_global(&instance, &param, NULL);
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_init_local(instance, ODP_THREAD_WORKER);
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_term_local();
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_term_global(instance);
+	CU_ASSERT(ret == 0);
+}
+
+odp_testinfo_t testinfo[] = {
+	ODP_TEST_INFO(init_test_defaults),
+	ODP_TEST_INFO(init_test_abort),
+	ODP_TEST_INFO(init_test_log),
+};
+
+odp_testinfo_t init_suite[] = {
+	ODP_TEST_INFO_NULL,
+	ODP_TEST_INFO_NULL
+};
+
+odp_suiteinfo_t init_suites[] = {
+	{"Init", NULL, NULL, init_suite},
+	ODP_SUITE_INFO_NULL,
+};
+
+static int fill_testinfo(odp_testinfo_t *info, unsigned int test_case)
+{
+	if (test_case >= (sizeof(testinfo) / sizeof(odp_testinfo_t))) {
+		printf("Bad test case number %u\n", test_case);
+		return -1;
+	}
+
+	*info = testinfo[test_case];
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int ret;
+
+	if (fill_testinfo(&init_suite[0], INIT_TEST))
+		return -1;
+
+	/* Parse common options */
+	if (odp_cunit_parse_options(argc, argv))
+		return -1;
+
+	/* Prevent default ODP init */
+	odp_cunit_register_global_init(NULL);
+	odp_cunit_register_global_term(NULL);
+
+	/* Register the tests */
+	ret = odp_cunit_register(init_suites);
+
+	/* Run the tests */
+	if (ret == 0)
+		ret = odp_cunit_run();
+
+	return ret;
+}
