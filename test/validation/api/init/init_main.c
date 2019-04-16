f04a17f3bd25b8c488898ee0610c0d292d814c7b
Petri Savolainen <petri.savolainen@nokia.com>
2019-04-16 10:50:53 +0300

    validation: init: add features test case
    Added test case to set not used features flags in init parameters.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- test/validation/api/init/init_main.c
+++ test/validation/api/init/init_main.c
@@ -125,11 +125,41 @@ static void init_test_num_thr(void)
 	CU_ASSERT(ret == 0);
 }
 
+static void init_test_feature(void)
+{
+	int ret;
+	odp_instance_t instance;
+	odp_init_t param;
+
+	odp_init_param_init(&param);
+	param.not_used.all_feat      = 0;
+	param.not_used.feat.cls      = 1;
+	param.not_used.feat.crypto   = 1;
+	param.not_used.feat.ipsec    = 1;
+	param.not_used.feat.schedule = 1;
+	param.not_used.feat.time     = 1;
+	param.not_used.feat.timer    = 1;
+	param.not_used.feat.tm       = 1;
+
+	ret = odp_init_global(&instance, &param, NULL);
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_init_local(instance, ODP_THREAD_CONTROL);
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_term_local();
+	CU_ASSERT_FATAL(ret == 0);
+
+	ret = odp_term_global(instance);
+	CU_ASSERT(ret == 0);
+}
+
 odp_testinfo_t testinfo[] = {
 	ODP_TEST_INFO(init_test_defaults),
 	ODP_TEST_INFO(init_test_abort),
 	ODP_TEST_INFO(init_test_log),
-	ODP_TEST_INFO(init_test_num_thr)
+	ODP_TEST_INFO(init_test_num_thr),
+	ODP_TEST_INFO(init_test_feature)
 };
 
 odp_testinfo_t init_suite[] = {
