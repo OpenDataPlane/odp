8fd65600ec2635a260904798077895c289a1dd15
Petri Savolainen <petri.savolainen@nokia.com>
2019-06-07 11:11:52 +0300

    test: ipsec: run ipsec_api example in make check
    Run ipsec examples under linux-generic platform specific
    test cases, as these examples assume that loop1 and loop2
    interfaces are available.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Dmitry Eremin-Solenikov <deremin-solenikov@cavium.com>

--- example/ipsec_api/odp_ipsec_api_run_simple.sh
+++ example/ipsec_api/odp_ipsec_api_run_simple.sh
@@ -4,7 +4,12 @@
 #  - 2 loop interfaces
 #  - 10 packets
 #  - Specify API mode on command line
-./odp_ipsec_api -i loop1,loop2 \
+
+if [ -z "$IPSEC_EXAMPLE_PATH" ]; then
+IPSEC_EXAMPLE_PATH=.
+fi
+
+${IPSEC_EXAMPLE_PATH}/odp_ipsec_api -i loop1,loop2 \
 -r 192.168.222.2/32:loop2:08.00.27.F5.8B.DB \
 -s 192.168.111.2:192.168.222.2:loop1:loop2:10:100 \
 -c 2 "$@"
