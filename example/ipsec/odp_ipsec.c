3d618f4324066cc93ce271665e6d5c7b593a5b7e
Petri Savolainen <petri.savolainen@nokia.com>
2019-06-18 10:28:09 +0300

    example: ipsec: rename schedule function pointer
    Rename function pointer to schedule_fn. When the name differs from
    other schedule definitions it's easier to find references to it
    and code is more readable.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Dmitry Eremin-Solenikov <deremin-solenikov@cavium.com>

--- example/ipsec/odp_ipsec.c
+++ example/ipsec/odp_ipsec.c
@@ -242,7 +242,7 @@ typedef odp_queue_t (*queue_create_func_t)
 typedef odp_event_t (*schedule_func_t) (odp_queue_t *);
 
 static queue_create_func_t queue_create;
-static schedule_func_t schedule;
+static schedule_func_t schedule_fn;
 
 /**
  * odp_queue_create wrapper to enable polling versus scheduling
@@ -1062,7 +1062,7 @@ int pktio_thread(void *arg EXAMPLE_UNUSED)
 		odp_event_subtype_t subtype;
 
 		/* Use schedule to get event from any input queue */
-		ev = schedule(&dispatchq);
+		ev = schedule_fn(&dispatchq);
 
 		if (ev == ODP_EVENT_INVALID)
 			continue;
@@ -1211,12 +1211,12 @@ main(int argc, char *argv[])
 
 	/* create by default scheduled queues */
 	queue_create = odp_queue_create;
-	schedule = odp_schedule_cb;
+	schedule_fn = odp_schedule_cb;
 
 	/* check for using poll queues */
 	if (getenv("ODP_IPSEC_USE_POLL_QUEUES")) {
 		queue_create = polled_odp_queue_create;
-		schedule = polled_odp_schedule_cb;
+		schedule_fn = polled_odp_schedule_cb;
 	}
 
 	/* Let helper collect its own arguments (e.g. --odph_proc) */
