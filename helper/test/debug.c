0f963e978dbdf70d54d06eb196dc4ae4602df1d9
Petri Savolainen <petri.savolainen@nokia.com>
2019-08-19 13:21:43 +0300

    helper: debug: add test application for helper debug
    Test that macros are defined and use assert.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

---
+++ helper/test/debug.c
@@ -0,0 +1,31 @@
+/* Copyright (c) 2019, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include "config.h"
+
+#include <odp_api.h>
+#include <odp/helper/odph_api.h>
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	printf("\nHelper library version is: %s\n\n", odph_version_str());
+
+	printf("Helper debugging:\n");
+	printf("  ODPH_DEBUG:       %i\n", ODPH_DEBUG);
+	printf("  ODPH_DEBUG_PRINT: %i\n\n", ODPH_DEBUG_PRINT);
+
+	/* ASSERT(true) should work always */
+	ODPH_ASSERT(1);
+
+	/* ASSERT(false) should not abort when not debugging */
+	if (ODPH_DEBUG == 0)
+		ODPH_ASSERT(0);
+
+	return 0;
+}
