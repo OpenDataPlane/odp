f974cb930ffb6bc1f0084b23f8d2e877b75e44db
Matias Elo <matias.elo@nokia.com>
2020-02-11 15:37:45 +0200

    travis: change default ubuntu container version to 18.04
    Separate DPDK v18.11 test has been removed. DPDK v18.11 is used now by
    default in Ubuntu 18.04 x86 builds. DPDK v17.11 is still used in Ubuntu
    16.04 builds.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- scripts/ci/build_ppc64el.sh
+++ scripts/ci/build_ppc64el.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-export TARGET_ARCH=powerpc-linux-gnu
+export TARGET_ARCH=powerpc64le-linux-gnu
 if [ "${CC#clang}" != "${CC}" ] ; then
 	export CC="clang --target=${TARGET_ARCH}"
 	export CXX="clang++ --target=${TARGET_ARCH}"
+	# DPDK clang build broken
+	export CONF="${CONF} --disable-dpdk"
 else
 	export CC="${TARGET_ARCH}-gcc"
 	export CXX="${TARGET_ARCH}-g++"
 fi
-# No DPDK on PowerPC
-export CONF="${CONF} --disable-dpdk"
+export CPPFLAGS="-I/usr/include/${TARGET_ARCH}/dpdk"
 
 exec "$(dirname "$0")"/build.sh
