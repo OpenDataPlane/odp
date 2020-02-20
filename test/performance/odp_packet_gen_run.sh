1d09a45ec333bf237edff731e78fa1c68dc49092
Petri Savolainen <petri.savolainen@nokia.com>
2020-02-20 16:49:04 +0200

    test: packet_gen: test application during make check
    Packet_gen sends 500 packets through both loopback
    interfaces. Test lasts 5 sec. In total 1000 packets are sent
    and the same amount (or minus the last sent burst) should
    be received. Test passes when at least 800 packets are
    received.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-and-tested-by: Matias Elo <matias.elo@nokia.com>

---
+++ test/performance/odp_packet_gen_run.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+#
+# Copyright (c) 2020, Nokia
+# All rights reserved.
+#
+# SPDX-License-Identifier:	BSD-3-Clause
+#
+
+# directory where test binaries have been built
+TEST_DIR="${TEST_DIR:-$PWD}"
+
+# directory where test sources are, including scripts
+TEST_SRC_DIR=$(dirname $0)
+
+PATH=$TEST_DIR:$PATH
+
+# exit codes expected by automake for skipped tests
+TEST_SKIPPED=77
+
+VALIDATION_TESTDIR=platform/$ODP_PLATFORM/test/validation
+PLATFORM_VALIDATION=${TEST_SRC_DIR}/../../$VALIDATION_TESTDIR
+
+# Use installed pktio env or for make check take it from platform directory
+if [ -f "./pktio_env" ]; then
+	. ./pktio_env
+elif  [ "$ODP_PLATFORM" = "" ]; then
+	echo "$0: error: ODP_PLATFORM must be defined"
+	# not skipped as this should never happen via "make check"
+	exit 1
+elif [ -f ${PLATFORM_VALIDATION}/api/pktio/pktio_env ]; then
+	. ${PLATFORM_VALIDATION}/api/pktio/pktio_env
+else
+	echo "BUG: unable to find pktio_env!"
+	echo "pktio_env has to be in current directory or "
+	echo "in platform/\$ODP_PLATFORM/test."
+	echo "ODP_PLATFORM=\"$ODP_PLATFORM\""
+	exit 1
+fi
+
+run_packet_gen()
+{
+	setup_pktio_env clean # install trap to call cleanup_pktio_env
+
+	if [ $? -ne 0 ]; then
+		echo "setup_pktio_env error $?"
+		exit $TEST_SKIPPED
+	fi
+
+	# Runs 500 * 10ms = 5 sec
+	# Sends 500 packets through both interfaces => total 1000 packets
+	odp_packet_gen${EXEEXT} -i $IF0,$IF1 -b 1 -g 10000000 -q 500
+
+	ret=$?
+
+	if [ $ret -eq -1 ]; then
+		echo "FAIL: test failed"
+	elif [ $ret -eq -2 ]; then
+		echo "FAIL: too few packets received"
+	fi
+
+	cleanup_pktio_env
+
+	exit $ret
+}
+
+case "$1" in
+	setup)   setup_pktio_env   ;;
+	cleanup) cleanup_pktio_env ;;
+	*)       run_packet_gen ;;
+esac
