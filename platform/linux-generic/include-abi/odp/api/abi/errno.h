8bf6c9013601853413d64c2b6a481fa2e9a40d27
Stanislaw Kardach <skardach@marvell.com>
2020-05-12 15:39:36 +0200

    abi: errno: allow inlining
    Add abi header for errno to allow platforms to inline errno functions.
    
    Signed-off-by: Stanislaw Kardach <skardach@marvell.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

---
+++ platform/linux-generic/include-abi/odp/api/abi/errno.h
@@ -0,0 +1,18 @@
+/* Copyright (c) 2020, Marvell
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP errno
+ */
+
+#ifndef ODP_API_ABI_ERRNO_H_
+#define ODP_API_ABI_ERRNO_H_
+
+#include <odp/api/abi-default/errno.h>
+
+#endif
