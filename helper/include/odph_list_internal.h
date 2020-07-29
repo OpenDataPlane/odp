7a052dd3959aefb3537284b59730c402247b0f96
Stanislaw Kardach <skardach@marvell.com>
2020-07-29 16:21:48 +0200

    helper: fixup internal doxygen docs
    @internal is improperly used to hide the internal documentation of the
    helper. Newer doxygen (1.8.17) reports syntax errors on most of the
    doxytags in the file in this commit.
    Analysing the commit history it seems that most of the tags were added
    to remove doxygen warnings. Therefore switch to a method which is widely
    used in other places in ODP.
    
    Signed-off-by: Stanislaw Kardach <skardach@marvell.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>
    Reviewed-by: Nithin Dabilpuram <ndabilpuram@marvell.com>

--- helper/include/odph_list_internal.h
+++ helper/include/odph_list_internal.h
@@ -18,30 +18,22 @@
 extern "C" {
 #endif
 
-/** List object */
+/** @cond _ODP_HIDE_FROM_DOXYGEN_ */
+
 typedef struct odph_list_object {
-	/** Next element in list */
 	struct odph_list_object *next;
 
-	/** Previous element in list */
 	struct odph_list_object *prev;
 } odph_list_object;
 
-/** Head of list */
 typedef odph_list_object odph_list_head;
 
-/**
- * @internal Intiailize list head element
- *
- * @param list List object to be initialized
- */
 static inline void ODPH_INIT_LIST_HEAD(odph_list_object *list)
 {
 	list->next = list;
 	list->prev = list;
 }
 
-/** @internal Inline function @param new @param prev @param next */
 static inline void __odph_list_add(odph_list_object *new,
 				   odph_list_object *prev,
 				   odph_list_object *next)
@@ -52,20 +44,17 @@ static inline void __odph_list_add(odph_list_object *new,
 	prev->next = new;
 }
 
-/** @internal Inline function @param new @param head */
 static inline void odph_list_add(odph_list_object *new, odph_list_object *head)
 {
 	__odph_list_add(new, head, head->next);
 }
 
-/** @internal Inline function @param new @param head */
 static inline void odph_list_add_tail(struct odph_list_object *new,
 				      odph_list_object *head)
 {
 	__odph_list_add(new, head->prev, head);
 }
 
-/** @internal Inline function @param prev @param next */
 static inline void __odph_list_del(struct odph_list_object *prev,
 				   odph_list_object *next)
 {
@@ -73,28 +62,26 @@ static inline void __odph_list_del(struct odph_list_object *prev,
 	prev->next = next;
 }
 
-/** @internal Inline function @param entry */
 static inline void odph_list_del(struct odph_list_object *entry)
 {
 	__odph_list_del(entry->prev, entry->next);
 	ODPH_INIT_LIST_HEAD(entry);
 }
 
-/** @internal Inline function @param head @return */
 static inline int odph_list_empty(const struct odph_list_object *head)
 {
 	return head->next == head;
 }
 
-/** @internal */
 #define container_of(ptr, type, list_node) \
 		((type *)(void *)((char *)ptr - offsetof(type, list_node)))
 
-/** @internal */
 #define ODPH_LIST_FOR_EACH(pos, list_head, type, list_node) \
 	for (pos = container_of((list_head)->next, type, list_node); \
 		&pos->list_node != (list_head); \
 		pos = container_of(pos->list_node.next, type, list_node))
+
+/** @endcond */
 
 #ifdef __cplusplus
 }
