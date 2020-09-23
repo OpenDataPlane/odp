93a2b09aab17ec01c74e5e8a851ca30fd21a155e
Petri Savolainen <petri.savolainen@nokia.com>
2020-09-23 13:04:16 +0300

    validation: pktio: add LSO test suite
    Added LSO tests as a new test suite under pktio tests.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

---
+++ test/validation/api/pktio/lso.h
@@ -0,0 +1,19 @@
+/* Copyright (c) 2020, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef _ODP_TEST_PKTIO_LSO_H_
+#define _ODP_TEST_PKTIO_LSO_H_
+
+#include <odp_cunit_common.h>
+
+/* test array init/term functions: */
+int lso_suite_term(void);
+int lso_suite_init(void);
+
+/* test arrays: */
+extern odp_testinfo_t lso_suite[];
+
+#endif
