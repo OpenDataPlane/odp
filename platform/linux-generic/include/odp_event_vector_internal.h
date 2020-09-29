df2d73cc09bdc9467221ab25f54bf8c0dd69f65d
Matias Elo <matias.elo@nokia.com>
2020-09-29 15:16:50 +0300

    linux-gen: pool: implement vector pool type
    Implementation for the new vector pool type.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

---
+++ platform/linux-generic/include/odp_event_vector_internal.h
@@ -0,0 +1,35 @@
+/* Copyright (c) 2020, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP event vector descriptor - implementation internal
+ */
+
+#ifndef ODP_EVENT_VECTOR_INTERNAL_H_
+#define ODP_EVENT_VECTOR_INTERNAL_H_
+
+#include <stdint.h>
+#include <odp/api/packet.h>
+#include <odp_buffer_internal.h>
+
+/**
+ * Internal event vector header
+ */
+typedef struct {
+	/* Common buffer header */
+	odp_buffer_hdr_t buf_hdr;
+
+	/* Event vector size */
+	uint32_t size;
+
+	/* Vector of packet handles */
+	odp_packet_t packet[0];
+
+} odp_event_vector_hdr_t;
+
+#endif /* ODP_EVENT_VECTOR_INTERNAL_H_ */
