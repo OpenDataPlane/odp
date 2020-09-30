61f5780d95534d0fc3b196f858c44c8e26523051
Matias Elo <matias.elo@nokia.com>
2020-09-30 15:25:47 +0300

    linux-gen: packet_vector: implement slow path functions
    Implement remaining packet vector slow path functions:
      odp_packet_vector_valid()
      odp_packet_vector_print()
      odp_packet_vector_to_u64()
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- platform/linux-generic/odp_packet_vector.c
+++ platform/linux-generic/odp_packet_vector.c
@@ -10,10 +10,14 @@
 #include <odp/api/packet.h>
 #include <odp/api/pool.h>
 #include <odp/api/plat/packet_vector_inlines.h>
+#include <odp/api/plat/strong_types.h>
 
 #include <odp_debug_internal.h>
 #include <odp_event_vector_internal.h>
 #include <odp_pool_internal.h>
+
+#include <inttypes.h>
+#include <stdint.h>
 
 #include <odp/visibility_begin.h>
 
@@ -54,4 +58,64 @@ void odp_packet_vector_free(odp_packet_vector_t pktv)
 	pktv_hdr->size = 0;
 
 	odp_buffer_free(odp_buffer_from_event(ev));
+}
+
+int odp_packet_vector_valid(odp_packet_vector_t pktv)
+{
+	odp_event_vector_hdr_t *pktv_hdr = _odp_packet_vector_hdr(pktv);
+	pool_t *pool = pktv_hdr->buf_hdr.pool_ptr;
+	uint32_t i;
+
+	if (odp_unlikely(pktv == ODP_PACKET_VECTOR_INVALID))
+		return 0;
+
+	if (odp_unlikely(pktv_hdr->size > pool->params.vector.max_size))
+		return 0;
+
+	for (i = 0; i < pktv_hdr->size; i++) {
+		if (pktv_hdr->packet[i] == ODP_PACKET_INVALID)
+			return 0;
+	}
+
+	return 1;
+}
+
+void odp_packet_vector_print(odp_packet_vector_t pktv)
+{
+	int max_len = 4096;
+	char str[max_len];
+	int len = 0;
+	int n = max_len - 1;
+	uint32_t i;
+	odp_event_vector_hdr_t *pktv_hdr = _odp_packet_vector_hdr(pktv);
+
+	len += snprintf(&str[len], n - len, "Packet Vector\n");
+	len += snprintf(&str[len], n - len,
+			"  handle %p\n", pktv);
+	len += snprintf(&str[len], n - len,
+			"  size   %" PRIu32 "\n", pktv_hdr->size);
+
+	for (i = 0; i < pktv_hdr->size; i++) {
+		odp_packet_t pkt = pktv_hdr->packet[i];
+		char seg_str[max_len];
+		int str_len;
+
+		str_len = snprintf(seg_str, max_len,
+				   "    packet     %p  len %" PRIu32 "\n",
+				   pkt, odp_packet_len(pkt));
+
+		/* Prevent print buffer overflow */
+		if (n - len - str_len < 10) {
+			len += snprintf(&str[len], n - len, "    ...\n");
+			break;
+		}
+		len += snprintf(&str[len], n - len, "%s", seg_str);
+	}
+
+	ODP_PRINT("%s\n", str);
+}
+
+uint64_t odp_packet_vector_to_u64(odp_packet_vector_t pktv)
+{
+	return _odp_pri(pktv);
 }
