498c66667e27b93977a2e98a01e97e41bbe64757
Kiran Kumar K <kirankumark@marvell.com>
2020-10-05 08:17:27 +0530

    helper: add odph_gtp header description
    Adds GTP header description struct odph_gtphdr_t in helper directory.
    This structure is used for accessing GTPU header information from the
    packet.
    
    Signed-off-by: Kiran Kumar K <kirankumark@marvell.com>
    Signed-off-by: Satheesh Paul <psatheesh@marvell.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

---
+++ helper/include/odp/helper/gtp.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(C) 2020 Marvell.
+ */
+/**
+ * @file
+ *
+ * ODP GTP header
+ */
+#ifndef _ODPH_GTP_H_
+#define _ODPH_GTP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <odp_api.h>
+
+/**
+ * Simplified GTP protocol header.
+ * Contains 8-bit gtp_hdr_info, 8-bit msg_type,
+ * 16-bit plen, 32-bit teid.
+ * No optional fields and next extension header.
+ */
+typedef struct ODP_PACKED {
+	uint8_t gtp_hdr_info; /**< GTP header info */
+	uint8_t msg_type;     /**< GTP message type */
+	odp_u16be_t plen;     /**< Total payload length */
+	odp_u32be_t teid;     /**< Tunnel endpoint ID */
+} odph_gtphdr_t;
+
+/** GTP header length */
+#define ODP_GTP_HLEN sizeof(odph_gtphdr_t)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ODP_GTP_H_ */
