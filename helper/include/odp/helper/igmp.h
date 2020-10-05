76a42aaf34892e7e40504792d80bd692c30038ea
Kiran Kumar K <kirankumark@marvell.com>
2020-10-05 09:29:13 +0530

    helper: add odph_igmp header description
    Adds IGMP header description struct odph_igmphdr_t in helper directory.
    This structure is used for accessing IGMP header information from the
    packet.
    
    Signed-off-by: Kiran Kumar K <kirankumark@marvell.com>
    Signed-off-by: Satheesh Paul <psatheesh@marvell.com>

---
+++ helper/include/odp/helper/igmp.h
@@ -0,0 +1,41 @@
+/* Copyright (c) 2020, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP IGMP header
+ */
+#ifndef _ODPH_IGMP_H_
+#define _ODPH_IGMP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <odp_api.h>
+
+/**
+ * Simplified IGMP protocol header.
+ * Contains 8-bit type, 8-bit code,
+ * 16-bit csum, 32-bit group.
+ * No optional fields and next extension header.
+ */
+typedef struct ODP_PACKED {
+	uint8_t type; /** Message Type */
+	uint8_t code; /** Max response code */
+	odp_u16be_t csum; /** Checksum */
+	odp_u32be_t group; /** Group address */
+} odph_igmphdr_t;
+
+/** IGMP header length */
+#define ODP_IGMP_HLEN sizeof(odph_igmphdr_t)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ODP_IGMP_H_ */
