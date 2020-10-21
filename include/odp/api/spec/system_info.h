6e3a6e9c26107b1a0a7ebb40ec058d42036f505b
Petri Savolainen <petri.savolainen@nokia.com>
2020-10-21 11:30:59 +0300

    api: system: add system info call
    System info may be used to retrieve various system information
    with a single call. Content of the system info structure may be
    extended later, but it contains initially e.g. CPU ISA family
    and version.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Govindarajan Mohandoss <Govindarajan.Mohandoss@arm.com>

--- include/odp/api/spec/system_info.h
+++ include/odp/api/spec/system_info.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2013-2018, Linaro Limited
+ * Copyright (c) 2020, Nokia
  * All rights reserved.
  *
  * SPDX-License-Identifier:     BSD-3-Clause
@@ -22,6 +23,174 @@ extern "C" {
  *  System information.
  *  @{
  */
+
+/**
+ * CPU instruction set architecture (ISA) families
+ */
+typedef enum odp_cpu_arch_t {
+	/** Unknown CPU architecture */
+	ODP_CPU_ARCH_UNKNOWN = 0,
+
+	/** ARM */
+	ODP_CPU_ARCH_ARM,
+
+	/** MIPS */
+	ODP_CPU_ARCH_MIPS,
+
+	/** PowerPC */
+	ODP_CPU_ARCH_PPC,
+
+	/** RISC-V */
+	ODP_CPU_ARCH_RISCV,
+
+	/** x86 */
+	ODP_CPU_ARCH_X86
+
+} odp_cpu_arch_t;
+
+/**
+ * ARM ISA versions
+ *
+ * ISA versions are defined in ascending order.
+ */
+typedef enum odp_cpu_arch_arm_t {
+	/** Unknown ARM ISA version */
+	ODP_CPU_ARCH_ARM_UNKNOWN = 0,
+
+	/** ARMv6 ISA */
+	ODP_CPU_ARCH_ARMV6,
+
+	/** ARMv7-A ISA */
+	ODP_CPU_ARCH_ARMV7,
+
+	/** ARMv8.0-A ISA */
+	ODP_CPU_ARCH_ARMV8_0,
+
+	/** ARMv8.1-A ISA */
+	ODP_CPU_ARCH_ARMV8_1,
+
+	/** ARMv8.2-A ISA */
+	ODP_CPU_ARCH_ARMV8_2,
+
+	/** ARMv8.3-A ISA */
+	ODP_CPU_ARCH_ARMV8_3,
+
+	/** ARMv8.4-A ISA */
+	ODP_CPU_ARCH_ARMV8_4,
+
+	/** ARMv8.5-A ISA */
+	ODP_CPU_ARCH_ARMV8_5,
+
+	/** ARMv8.6-A ISA */
+	ODP_CPU_ARCH_ARMV8_6,
+
+} odp_cpu_arch_arm_t;
+
+/**
+ * MIPS ISA versions
+ */
+typedef enum odp_cpu_arch_mips_t {
+	/** Unknown MIPS ISA version */
+	ODP_CPU_ARCH_MIPS_UNKNOWN = 0
+
+} odp_cpu_arch_mips_t;
+
+/**
+ * PowerPC ISA versions
+ */
+typedef enum odp_cpu_arch_ppc_t {
+	/** Unknown PPC ISA version */
+	ODP_CPU_ARCH_PPC_UNKNOWN = 0
+
+} odp_cpu_arch_ppc_t;
+
+/**
+ * RISC-V ISA versions
+ */
+typedef enum odp_cpu_arch_riscv_t {
+	/** Unknown RISC-V ISA version */
+	ODP_CPU_ARCH_RISCV_UNKNOWN = 0
+
+} odp_cpu_arch_riscv_t;
+
+/**
+ * x86 ISA versions
+ */
+typedef enum odp_cpu_arch_x86_t {
+	/** Unknown x86 ISA version */
+	ODP_CPU_ARCH_X86_UNKNOWN = 0,
+
+	/** x86 32bit ISA */
+	ODP_CPU_ARCH_X86_I686,
+
+	/** x86 64bit ISA */
+	ODP_CPU_ARCH_X86_64
+
+} odp_cpu_arch_x86_t;
+
+/**
+ * CPU ISA versions
+ */
+typedef union odp_cpu_arch_isa_t {
+	/** ARM ISA versions */
+	odp_cpu_arch_arm_t arm;
+
+	/** MIPS ISA versions */
+	odp_cpu_arch_mips_t mips;
+
+	/** PowerPC ISA versions */
+	odp_cpu_arch_ppc_t ppc;
+
+	/** RISC-V ISA versions */
+	odp_cpu_arch_riscv_t riscv;
+
+	/** x86 ISA versions */
+	odp_cpu_arch_x86_t x86;
+
+} odp_cpu_arch_isa_t;
+
+/**
+ * System info
+ */
+typedef struct odp_system_info_t {
+	/**
+	 * CPU architecture
+	 *
+	 * Defines CPU ISA family: ARM, MIPS, PPC, RISC-V, x86 or unknown
+	 */
+	odp_cpu_arch_t cpu_arch;
+
+	/**
+	 * ISA version of ODP software
+	 *
+	 * Defines the ISA version that was used to build the ODP library. Depending on compiler
+	 * target architecture setting, the value may be lower than the ISA version supported by
+	 * the CPU hardware.
+	 */
+	odp_cpu_arch_isa_t cpu_isa_sw;
+
+	/**
+	 * ISA version of CPU hardware
+	 *
+	 * Defines the ISA version supported by the CPU hardware. The value is set to
+	 * ODP_CPU_ARCH_<arch>_UNKNOWN, when the ISA version cannot be determined.
+	 */
+	odp_cpu_arch_isa_t cpu_isa_hw;
+
+} odp_system_info_t;
+
+/**
+ * Retrieve system information
+ *
+ * Fills in system information structure on success. The call is not intended
+ * for fast path use.
+ *
+ * @param[out] info    Pointer to system info struct for output
+ *
+ * @retval  0 on success
+ * @retval <0 on failure
+ */
+int odp_system_info(odp_system_info_t *info);
 
 /**
  * Default system huge page size in bytes
