94806a0694931efda08d42fa45b7747902cd52bd
Petri Savolainen <petri.savolainen@nokia.com>
2020-10-22 15:54:04 +0300

    linux-gen: arch: implement older arm sysinfo
    Added sysinfo file for ARM versions older than aarch64. Changed to
    use arch/arm sysinfo file instead of the default. This is a copy of
    the default sysinfo file.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Govindarajan Mohandoss <Govindarajan.Mohandoss@arm.com>

---
+++ platform/linux-generic/arch/arm/odp_sysinfo_parse.c
@@ -0,0 +1,33 @@
+/* Copyright (c) 2020, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include <odp_global_data.h>
+#include <odp_sysinfo_internal.h>
+
+int cpuinfo_parser(FILE * file ODP_UNUSED, system_info_t *sysinfo)
+{
+	sysinfo->cpu_arch = ODP_CPU_ARCH_ARM;
+	sysinfo->cpu_isa_sw.arm = ODP_CPU_ARCH_ARM_UNKNOWN;
+	sysinfo->cpu_isa_hw.arm = ODP_CPU_ARCH_ARM_UNKNOWN;
+
+#if defined(__ARM_ARCH)
+	if (__ARM_ARCH == 6)
+		sysinfo->cpu_isa_sw.arm = ODP_CPU_ARCH_ARMV6;
+	else if (__ARM_ARCH == 7)
+		sysinfo->cpu_isa_sw.arm = ODP_CPU_ARCH_ARMV7;
+#endif
+
+	return _odp_dummy_cpuinfo(sysinfo);
+}
+
+void sys_info_print_arch(void)
+{
+}
+
+uint64_t odp_cpu_arch_hz_current(int id ODP_UNUSED)
+{
+	return odp_global_ro.system_info.default_cpu_hz;
+}
