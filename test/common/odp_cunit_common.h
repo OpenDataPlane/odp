50f65c91728ffcbadd1bcfe28aa1b175f4d425a7
Nithin Dabilpuram <ndabilpuram@marvell.com>
2020-11-04 22:41:56 +0530

    test: cunit: add API to deactivate all test cases
    In test suite init, it is possible to detect that a given test suite
    is not applicable. Hence provide an API to deactivate all the
    test cases of a given test suite.
    
    Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

--- test/common/odp_cunit_common.h
+++ test/common/odp_cunit_common.h
@@ -105,6 +105,7 @@ void odp_cunit_register_global_term(int (*func_term_ptr)(odp_instance_t inst));
 
 int odp_cunit_ret(int val);
 int odp_cunit_print_inactive(void);
+int odp_cunit_set_inactive(void);
 
 /*
  * Wrapper for CU_ASSERT_FATAL implementation to show the compiler that
