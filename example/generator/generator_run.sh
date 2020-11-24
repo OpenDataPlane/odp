fa78b206db952ad80e3526ed23e9ccfb0bc36525
Sunil Kumar Kori <skori@marvell.com>
2020-11-24 13:38:28 +0530

    example: generator: use platform defined pktio_env
    odp_generator uses hard coded null interface to validate functionality.
    Patch optimises it to use platform defined pktio_env.
    
    Signed-off-by: Sunil Kumar Kori <skori@marvell.com>

---
+++ example/generator/generator_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (c) 2018, Linaro Limited
+# All rights reserved.
+#
+# SPDX-License-Identifier:     BSD-3-Clause
+#
+
+if  [ -f ./pktio_env ]; then
+	. ./pktio_env
+else
+	echo "BUG: unable to find pktio_env!"
+	echo "pktio_env has to be in current directory"
+	exit 1
+fi
+
+setup_interfaces
+
+./odp_generator${EXEEXT} -w 1 -n 1 -I $IF0 -m u
+STATUS=$?
+
+if [ "$STATUS" -ne 0 ]; then
+  echo "Error: status was: $STATUS, expected 0"
+  exit 1
+fi
+
+validate_result
+
+cleanup_interfaces
+
+exit 0
