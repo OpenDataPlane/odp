7e4a186ce4521d87ab2792b42bb19d01a9f393a0
Aakash Sasidharan <asasidharan@marvell.com>
2020-12-02 10:43:14 +0000

    api: protocols: add new header
    Add protocols API header to accommodate protocol specific
    definitions.
    
    Signed-off-by: Aakash Sasidharan <asasidharan@marvell.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

---
+++ include/odp/api/protocols.h
@@ -0,0 +1,26 @@
+/* Copyright (c) 2020, Marvell
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+/**
+ * @file
+ *
+ * ODP protocols
+ */
+
+#ifndef ODP_API_PROTOCOLS_H_
+#define ODP_API_PROTOCOLS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <odp/api/spec/protocols.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
