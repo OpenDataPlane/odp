1e34c7ca0ff3ff43c2e7c8d4e12e24d318472746
Petri Savolainen <petri.savolainen@nokia.com>
2020-12-07 16:20:25 +0200

    test: timer_perf: run in both modes during make check
    Run odp_timer_perf test application with -m 0 and -m 1
    modes during make check.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

---
+++ test/performance/odp_timer_perf_run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# Copyright (c) 2020, Nokia
+# All rights reserved.
+#
+# SPDX-License-Identifier:	BSD-3-Clause
+#
+
+TEST_DIR="${TEST_DIR:-$(dirname $0)}"
+
+echo odp_timer_perf: odp_schedule overhead mode
+echo ===============================================
+
+$TEST_DIR/odp_timer_perf${EXEEXT} -m 0 -c 1
+
+RET_VAL=$?
+if [ $RET_VAL -ne 0 ]; then
+	echo odp_timer_perf -m 0: FAILED
+	exit $RET_VAL
+fi
+
+echo odp_timer_perf: timer set + cancel mode
+echo ===============================================
+
+$TEST_DIR/odp_timer_perf${EXEEXT} -m 1 -c 1 -t 10 -R 50
+
+RET_VAL=$?
+if [ $RET_VAL -ne 0 ]; then
+	echo odp_timer_perf -m 1: FAILED
+	exit $RET_VAL
+fi
+
+exit 0
