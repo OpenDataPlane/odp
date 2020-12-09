a232ac2e2960ee0517db23d5f75a12afd2a0c215
Govindarajan Mohandoss <govindarajan.mohandoss@arm.com>
2020-12-09 23:10:07 +0000

    api: atomic: add 128 bit atomic apis
    Add 128 bit atomic compare-and-swap operations.
    
    Signed-off-by: Govindarajan Mohandoss <govindarajan.mohandoss@arm.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- include/odp/api/abi-default/atomic.h
+++ include/odp/api/abi-default/atomic.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2015-2018, Linaro Limited
+ * Copyright (c) 2021, ARM Limited
  * All rights reserved.
  *
  * SPDX-License-Identifier:     BSD-3-Clause
@@ -56,6 +57,31 @@ typedef struct ODP_ALIGNED(sizeof(uint64_t)) odp_atomic_u64_s {
 	 * data types. We use a spin lock to ensure atomicity. */
 	char lock; /**< Spin lock (if needed) used to ensure atomic access */
 } odp_atomic_u64_t;
+
+#endif
+
+#ifdef _ODP_LOCK_FREE_128BIT_ATOMICS
+
+/**
+ * @internal
+ * Atomic 128-bit unsigned integer
+ */
+typedef struct ODP_ALIGNED(sizeof(odp_u128_t)) odp_atomic_u128_s {
+	odp_u128_t v; /**< Actual storage for the atomic variable */
+} odp_atomic_u128_t;
+
+#else
+
+/**
+ * @internal
+ * Atomic 128-bit unsigned integer
+ */
+typedef struct ODP_ALIGNED(sizeof(odp_u128_t)) odp_atomic_u128_s {
+	odp_u128_t v; /**< Actual storage for the atomic variable */
+	/* Some architectures do not support lock-free operations on 128-bit
+	 * data types. We use a spin lock to ensure atomicity. */
+	char lock; /**< Spin lock (if needed) used to ensure atomic access */
+} odp_atomic_u128_t;
 
 #endif
 
