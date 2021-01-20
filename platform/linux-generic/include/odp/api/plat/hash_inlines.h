d07a44c6ed222746d661f3c6b91d3c7f3b5f799b
Matias Elo <matias.elo@nokia.com>
2021-01-20 16:06:48 -0600

    linux-gen: hash: prepare for arch specific implementations
    Refactor build system to enable adding architecture specific hash function
    implementations.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

---
+++ platform/linux-generic/include/odp/api/plat/hash_inlines.h
@@ -0,0 +1,47 @@
+/* Copyright (c) 2021, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef ODP_PLAT_HASH_INLINES_H_
+#define ODP_PLAT_HASH_INLINES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <odp/api/abi/hash_crc32.h>
+
+#include <stdint.h>
+
+/** @cond _ODP_HIDE_FROM_DOXYGEN_ */
+
+#ifndef _ODP_NO_INLINE
+	/* Inline functions by default */
+	#define _ODP_INLINE static inline
+	#define odp_hash_crc32      __odp_hash_crc32
+	#define odp_hash_crc32c     __odp_hash_crc32c
+#else
+	#define _ODP_INLINE
+#endif
+
+_ODP_INLINE uint32_t odp_hash_crc32(const void *data, uint32_t data_len,
+				    uint32_t init_val)
+{
+	return _odp_hash_crc32(data, data_len, init_val);
+}
+
+_ODP_INLINE uint32_t odp_hash_crc32c(const void *data, uint32_t data_len,
+				     uint32_t init_val)
+{
+	return _odp_hash_crc32c(data, data_len, init_val);
+}
+
+/** @endcond */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ platform/linux-generic/odp_hash_api.c
@@ -0,0 +1,11 @@
+/* Copyright (c) 2021, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include <odp/api/hash.h>
+
+/* Non-inlined functions for ABI compat mode */
+#define _ODP_NO_INLINE
+#include <odp/api/plat/hash_inlines.h>
