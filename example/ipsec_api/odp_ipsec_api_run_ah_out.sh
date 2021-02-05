cce2b8fc7a38280810b62ab3b09b28aae9a4e908
Aakash Sasidharan <asasidharan@marvell.com>
2021-02-05 12:12:34 +0000

    example: ipsec_api: enhance runtime scripts
    The network interfaces and configurations required for
    the application change based on the application mode in
    which the test is being run. Introduce env variable
    IPSEC_APP_MODE which will be set by run script.
    
    The supported modes are:
    0 - standalone mode, 1 - live mode and 2 - router mode.
    
    Though linux-gen supports multiple "loop" devices like loop1
    and loop2, other ODP implementations may not support
    multiple loop interfaces. To allow flexibility for other
    platforms, introduce following env variables in the runtime
    scripts for standalone mode:
    * IF_LIST env variable lists all the interfaces used for
      standalone mode of test.
    * For outbound tests, stream entry has source interface as IN_IF
      and destination interface as OUT_IF.
    * For inbound tests, stream entry has source interfaces as OUT_IF
      and destination interfaces as IN_IF.
    * Introduce ROUTE_IF_INB and ROUTE_IF_OUTB for routing the IPSEC
      processed packets in standalone tests.
    
    Introduce platform specific pktio_env script to define these
    environment variables.
    
    Signed-off-by: Aakash Sasidharan <asasidharan@marvell.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- example/ipsec_api/odp_ipsec_api_run_ah_out.sh
+++ example/ipsec_api/odp_ipsec_api_run_ah_out.sh
@@ -4,9 +4,36 @@
 #  - 2 loop interfaces
 #  - 10 packets
 #  - Specify API mode on command line
-./odp_ipsec_api -i loop1,loop2 \
--r 192.168.222.2/32,loop2,08:00:27:F5:8B:DB \
--p 192.168.111.0/24,192.168.222.0/24,out,ah \
--a 192.168.111.2,192.168.222.2,md5,200,a731649644c5dee92cbd9c2e7e188ee6 \
--s 192.168.111.2,192.168.222.2,loop1,loop2,10,100 \
--c 2 "$@"
+
+# IPSEC_APP_MODE: 0 - STANDALONE, 1 - LIVE, 2 - ROUTER
+IPSEC_APP_MODE=0
+
+if  [ -f ./pktio_env ]; then
+	. ./pktio_env
+else
+	echo "BUG: unable to find pktio_env!"
+	echo "pktio_env has to be in current directory"
+	exit 1
+fi
+
+setup_interfaces
+
+./odp_ipsec_api -i $IF_LIST \
+	-r 192.168.222.2/32,$ROUTE_IF_OUTB,08:00:27:F5:8B:DB \
+	-p 192.168.111.0/24,192.168.222.0/24,out,ah \
+	-a 192.168.111.2,192.168.222.2,md5,200,a731649644c5dee92cbd9c2e7e188ee6 \
+	-s 192.168.111.2,192.168.222.2,$IN_IF,$OUT_IF,10,100 \
+	-c 2 "$@"
+
+STATUS=$?
+
+if [ ${STATUS} -ne 0 ]; then
+	echo "Error: status ${STATUS}"
+	exit 1
+fi
+
+validate_result
+
+cleanup_interfaces
+
+exit 0
--- example/ipsec_api/odp_ipsec_api_run_simple.sh
+++ example/ipsec_api/odp_ipsec_api_run_simple.sh
@@ -5,11 +5,37 @@
 #  - 10 packets
 #  - Specify API mode on command line
 
+# IPSEC_APP_MODE: 0 - STANDALONE, 1 - LIVE, 2 - ROUTER
+IPSEC_APP_MODE=0
+
+if  [ -f ./pktio_env ]; then
+	. ./pktio_env
+else
+	echo "BUG: unable to find pktio_env!"
+	echo "pktio_env has to be in current directory"
+	exit 1
+fi
+
+setup_interfaces
+
 if [ -z "$IPSEC_EXAMPLE_PATH" ]; then
 IPSEC_EXAMPLE_PATH=.
 fi
 
-${IPSEC_EXAMPLE_PATH}/odp_ipsec_api -i loop1,loop2 \
--r 192.168.222.2/32,loop2,08:00:27:F5:8B:DB \
--s 192.168.111.2,192.168.222.2,loop1,loop2,10,100 \
--c 2 "$@"
+${IPSEC_EXAMPLE_PATH}/odp_ipsec_api -i $IF_LIST \
+	-r 192.168.222.2/32,$ROUTE_IF_OUTB,08:00:27:F5:8B:DB \
+	-s 192.168.111.2,192.168.222.2,$IN_IF,$OUT_IF,10,100 \
+	-c 2 "$@"
+
+STATUS=$?
+
+if [ ${STATUS} -ne 0 ]; then
+	echo "Error: status ${STATUS}"
+	exit 1
+fi
+
+validate_result
+
+cleanup_interfaces
+
+exit 0
