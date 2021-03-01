7789799b9418bbd65d604c2a2f1b04de95491ca5
Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
2021-03-01 14:00:23 +0530

    test: call global and local init in odp_api_from_cpp
    Test odp_api_from_cpp may call odp_init_[global,local] so that
    platform specific initialization (eg tracing ODP API calls) can be
    executed as well.
    
    Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- test/miscellaneous/odp_api_from_cpp.cpp
+++ test/miscellaneous/odp_api_from_cpp.cpp
@@ -4,8 +4,16 @@
 
 int main(int argc ODP_UNUSED, const char *argv[] ODP_UNUSED)
 {
+	odp_instance_t inst;
+
+	odp_init_global(&inst, NULL, NULL);
+	odp_init_local(inst, ODP_THREAD_WORKER);
+
 	std::cout << "\tODP API version: " << odp_version_api_str() << std::endl;
 	std::cout << "\tODP implementation version: " << odp_version_impl_str() << std::endl;
+
+	odp_term_local();
+	odp_term_global(inst);
 
 	return 0;
 }
