7e84caf6991bf7d1c204b06549c758894f8d40c7
Petri Savolainen <petri.savolainen@nokia.com>
2021-03-02 09:25:36 +0200

    linux-gen: fix spelling errors
    Fix spelling errors found by checkpatch: TYPO_SPELLING and
    REPEATED_WORD.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- platform/linux-generic/odp_ipsec_sad.c
+++ platform/linux-generic/odp_ipsec_sad.c
@@ -64,7 +64,7 @@ typedef struct sa_thread_local_s {
 	odp_atomic_u32_t packet_quota;
 	/*
 	 * Bytes that can be processed in this thread before looking at
-	 * at the SA-global byte counter and checking hard and soft limits.
+	 * the SA-global byte counter and checking hard and soft limits.
 	 */
 	uint32_t byte_quota;
 	/*
