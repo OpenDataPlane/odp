38d14afdc3340cff4430fe27c6a23ff7495f43a1
Matias Elo <matias.elo@nokia.com>
2021-03-16 19:32:31 +0200

    linux-gen: atomic: move odp_atomic_lock_free implementations under arch
    Enables architecture specific implementations.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

---
+++ platform/linux-generic/arch/default/odp_atomic.c
@@ -0,0 +1,36 @@
+/* Copyright (c) 2015-2018, Linaro Limited
+ * Copyright (c) 2021, ARM Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include <odp/api/atomic.h>
+
+int odp_atomic_lock_free_u64(odp_atomic_op_t *atomic_op)
+{
+#if __GCC_ATOMIC_LLONG_LOCK_FREE < 2
+	/* All operations have locks */
+	if (atomic_op)
+		atomic_op->all_bits = 0;
+
+	return 0;
+#else
+	/* All operations are lock-free */
+	if (atomic_op) {
+		atomic_op->all_bits = ~((uint32_t)0);
+		atomic_op->op.init  = 0;
+	}
+
+	return 2;
+#endif
+}
+
+int odp_atomic_lock_free_u128(odp_atomic_op_t *atomic_op)
+{
+	/* All operations have locks */
+	if (atomic_op)
+		atomic_op->all_bits = 0;
+
+	return 0;
+}
