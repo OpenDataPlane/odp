d258bb6298adcdd18b9efbf088209af7426f9521
Petri Savolainen <petri.savolainen@nokia.com>
2021-03-31 16:54:57 +0300

    test: cpp: check function return value
    Check ODP init/term return values and exit on failure.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- test/miscellaneous/odp_api_from_cpp.cpp
+++ test/miscellaneous/odp_api_from_cpp.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <iostream>
 #include <odp_api.h>
 #include <odp/helper/odph_api.h>
@@ -6,14 +7,20 @@ int main(int argc ODP_UNUSED, const char *argv[] ODP_UNUSED)
 {
 	odp_instance_t inst;
 
-	odp_init_global(&inst, NULL, NULL);
-	odp_init_local(inst, ODP_THREAD_WORKER);
+	if (odp_init_global(&inst, NULL, NULL))
+		exit(EXIT_FAILURE);
+
+	if (odp_init_local(inst, ODP_THREAD_WORKER))
+		exit(EXIT_FAILURE);
 
 	std::cout << "\tODP API version: " << odp_version_api_str() << std::endl;
 	std::cout << "\tODP implementation version: " << odp_version_impl_str() << std::endl;
 
-	odp_term_local();
-	odp_term_global(inst);
+	if (odp_term_local())
+		exit(EXIT_FAILURE);
+
+	if (odp_term_global(inst))
+		exit(EXIT_FAILURE);
 
 	return 0;
 }
