823f10079497132a531d9a4c495832807f708a46
Matias Elo <matias.elo@nokia.com>
2021-06-17 15:49:17 +0300

    linux-gen: pktio: read extra statistics using linux sysfs
    Add internal functions for reading extra pktio statistics using Linux
    sysfs. These functions can be used by multiple pktio devices.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- platform/linux-generic/pktio/stats/sysfs_stats.c
+++ platform/linux-generic/pktio/stats/sysfs_stats.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2015-2018, Linaro Limited
+ * Copyright (c) 2021, Nokia
  * All rights reserved.
  *
  * SPDX-License-Identifier:     BSD-3-Clause
@@ -7,9 +8,13 @@
 #include <odp_api.h>
 #include <odp_sysfs_stats.h>
 #include <odp_errno_define.h>
+#include <dirent.h>
 #include <errno.h>
 #include <string.h>
 #include <inttypes.h>
+#include <linux/limits.h>
+
+#define SYSFS_DIR "/sys/class/net/%s/statistics"
 
 static int sysfs_get_val(const char *fname, uint64_t *val)
 {
@@ -80,6 +85,119 @@ int _odp_sysfs_stats(pktio_entry_t *pktio_entry,
 
 	sprintf(fname, "/sys/class/net/%s/statistics/tx_errors", dev);
 	ret -= sysfs_get_val(fname, &stats->out_errors);
+
+	return ret;
+}
+
+int _odp_sysfs_extra_stat_info(pktio_entry_t *pktio_entry,
+			       odp_pktio_extra_stat_info_t info[], int num)
+{
+	struct dirent *e;
+	DIR *dir;
+	char sysfs_dir[PATH_MAX];
+	int counters = 0;
+
+	snprintf(sysfs_dir, PATH_MAX, SYSFS_DIR, pktio_entry->s.name);
+	dir = opendir(sysfs_dir);
+	if (!dir) {
+		ODP_ERR("Failed to open sysfs dir: %s\n", sysfs_dir);
+		return -1;
+	}
+
+	while ((e = readdir(dir)) != NULL) {
+		/* Skip . and .. */
+		if (strncmp(e->d_name, ".", 1) == 0)
+			continue;
+
+		if (info && counters < num)
+			snprintf(info[counters].name,
+				 ODP_PKTIO_STATS_EXTRA_NAME_LEN, "%s",
+				 e->d_name);
+		counters++;
+	}
+	(void)closedir(dir);
+
+	return counters;
+}
+
+int _odp_sysfs_extra_stats(pktio_entry_t *pktio_entry, uint64_t stats[],
+			   int num)
+{
+	struct dirent *e;
+	DIR *dir;
+	char sysfs_dir[PATH_MAX];
+	char file_path[PATH_MAX];
+	int counters = 0;
+
+	snprintf(sysfs_dir, PATH_MAX, SYSFS_DIR, pktio_entry->s.name);
+	dir = opendir(sysfs_dir);
+	if (!dir) {
+		ODP_ERR("Failed to open dir: %s\n", sysfs_dir);
+		return -1;
+	}
+
+	while ((e = readdir(dir)) != NULL) {
+		uint64_t val;
+
+		/* Skip . and .. */
+		if (strncmp(e->d_name, ".", 1) == 0)
+			continue;
+
+		snprintf(file_path, PATH_MAX, "%s/%s", sysfs_dir, e->d_name);
+		if (sysfs_get_val(file_path, &val)) {
+			ODP_ERR("Failed to read file: %s/n", file_path);
+			counters = -1;
+			break;
+		}
+
+		if (stats && counters < num)
+			stats[counters] = val;
+
+		counters++;
+	}
+	(void)closedir(dir);
+
+	return counters;
+}
+
+int _odp_sysfs_extra_stat_counter(pktio_entry_t *pktio_entry, uint32_t id,
+				  uint64_t *stat)
+{
+	struct dirent *e;
+	DIR *dir;
+	char sysfs_dir[PATH_MAX];
+	char file_path[PATH_MAX];
+	uint32_t counters = 0;
+	int ret = -1;
+
+	snprintf(sysfs_dir, PATH_MAX, SYSFS_DIR, pktio_entry->s.name);
+	dir = opendir(sysfs_dir);
+	if (!dir) {
+		ODP_ERR("Failed to open dir: %s\n", sysfs_dir);
+		return -1;
+	}
+
+	while ((e = readdir(dir)) != NULL) {
+		/* Skip . and .. */
+		if (strncmp(e->d_name, ".", 1) == 0)
+			continue;
+
+		if (counters == id) {
+			uint64_t val;
+
+			snprintf(file_path, PATH_MAX, "%s/%s",
+				 sysfs_dir, e->d_name);
+			if (sysfs_get_val(file_path, &val)) {
+				ODP_ERR("Failed to read file: %s/n", file_path);
+			} else {
+				*stat = val;
+				ret = 0;
+			}
+			break;
+		}
+		counters++;
+	}
+	(void)closedir(dir);
 
 	return ret;
 }
