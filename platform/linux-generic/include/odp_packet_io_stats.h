1439f8af1baed7c80611024335fc24e07d8adfc7
Matias Elo <matias.elo@nokia.com>
2021-06-17 16:12:40 +0300

    linux-gen: pktio: add internal wrapper functions for extra statistics
    Add internal wrapper functions for socket based pktio devices for reading
    extra pktio statistics. The wrapper functions call either ethtool or
    sysfs implementations.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- platform/linux-generic/include/odp_packet_io_stats.h
+++ platform/linux-generic/include/odp_packet_io_stats.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2018, Linaro Limited
+ * Copyright (c) 2021, Nokia
  * All rights reserved.
  *
  * SPDX-License-Identifier:     BSD-3-Clause
@@ -24,6 +25,14 @@ int _odp_sock_stats_reset_fd(pktio_entry_t *pktio_entry, int fd);
 
 void _odp_sock_stats_capa(pktio_entry_t *pktio_entry,
 			  odp_pktio_capability_t *capa);
+
+int _odp_sock_extra_stat_info(pktio_entry_t *pktio_entry,
+			      odp_pktio_extra_stat_info_t info[], int num,
+			      int fd);
+int _odp_sock_extra_stats(pktio_entry_t *pktio_entry, uint64_t stats[], int num,
+			  int fd);
+int _odp_sock_extra_stat_counter(pktio_entry_t *pktio_entry, uint32_t id,
+				 uint64_t *stat, int fd);
 
 pktio_stats_type_t _odp_sock_stats_type_fd(pktio_entry_t *pktio_entry, int fd);
 
