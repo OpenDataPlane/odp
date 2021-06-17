3482a2d5fb60fb4ea44d7535b9750eaed58ea80f
Matias Elo <matias.elo@nokia.com>
2021-06-17 13:40:39 +0300

    linux-gen: pktio: read extra statistics using ethtool
    Add internal functions for reading extra pktio statistics using ethtool.
    These functions can be used by multiple pktio devices.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- platform/linux-generic/include/odp_ethtool_stats.h
+++ platform/linux-generic/include/odp_ethtool_stats.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2018, Linaro Limited
+ * Copyright (c) 2021, Nokia
  * All rights reserved.
  *
  * SPDX-License-Identifier:     BSD-3-Clause
@@ -18,6 +19,11 @@ extern "C" {
  * Get ethtool statistics of a packet socket
  */
 int _odp_ethtool_stats_get_fd(int fd, const char *name, odp_pktio_stats_t *stats);
+
+int _odp_ethtool_extra_stat_info(int fd, const char *name, odp_pktio_extra_stat_info_t info[],
+				 int num);
+int _odp_ethtool_extra_stats(int fd, const char *name, uint64_t stats[], int num);
+int _odp_ethtool_extra_stat_counter(int fd, const char *name, uint32_t id, uint64_t *stat);
 
 #ifdef __cplusplus
 }
