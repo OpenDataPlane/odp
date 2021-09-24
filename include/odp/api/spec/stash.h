590aa4fd6987e49e1b075eb4a89ae1ccf3366d62
Matias Elo <matias.elo@nokia.com>
2021-09-24 15:21:40 +0300

    api: stash: add 32-bit only put and get functions
    Add 32-bit only variants odp_stash_put_u32() and odp_stash_get_u32() of the
    generic put/get functions. The new functions are safer to use compared to
    the generic versions due to strict typing and the implementation may be
    more optimized.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- include/odp/api/spec/stash.h
+++ include/odp/api/spec/stash.h
@@ -307,6 +307,22 @@ uint64_t odp_stash_to_u64(odp_stash_t stash);
 int32_t odp_stash_put(odp_stash_t stash, const void *obj, int32_t num);
 
 /**
+ * Put 32-bit integers into a stash
+ *
+ * Otherwise like odp_stash_put(), except that this function operates on 32-bit
+ * integers. The stash must have been created with 'obj_size' of 4.
+ *
+ * @param stash  Stash handle
+ * @param u32    Points to an array of 32-bit integers to be stored. The array
+ *               must be 32-bit aligned in memory.
+ * @param num    Number of integers to store
+ *
+ * @return Number of integers actually stored (0 ... num)
+ * @retval <0 on failure
+ */
+int32_t odp_stash_put_u32(odp_stash_t stash, const uint32_t u32[], int32_t num);
+
+/**
  * Put 64-bit integers into a stash
  *
  * Otherwise like odp_stash_put(), except that this function operates on 64-bit
@@ -339,6 +355,22 @@ int32_t odp_stash_put_u64(odp_stash_t stash, const uint64_t u64[], int32_t num);
  * @retval <0 on failure
  */
 int32_t odp_stash_get(odp_stash_t stash, void *obj, int32_t num);
+
+/**
+ * Get 32-bit integers from a stash
+ *
+ * Otherwise like odp_stash_get(), except that this function operates on 32-bit
+ * integers. The stash must have been created with 'obj_size' of 4.
+ *
+ * @param      stash  Stash handle
+ * @param[out] u32    Points to an array of 32-bit integers for output. The
+ *                    array must be 32-bit aligned in memory.
+ * @param      num    Maximum number of integers to get from the stash
+ *
+ * @return Number of integers actually output (0 ... num) to 'u32' array
+ * @retval <0 on failure
+ */
+int32_t odp_stash_get_u32(odp_stash_t stash, uint32_t u32[], int32_t num);
 
 /**
  * Get 64-bit integers from a stash
