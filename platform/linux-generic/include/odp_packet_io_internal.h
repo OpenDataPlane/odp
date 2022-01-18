302ebc58f494fbde9a3ec7af10eb73bce9b588a2
Matias Elo <matias.elo@nokia.com>
2022-01-18 09:29:36 +0200

    linux-gen: pktio: follow new ODP_PKTOUT_MAX_QUEUES define
    Follow new ODP_PKTOUT_MAX_QUEUES API define.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- platform/linux-generic/include/odp_packet_io_internal.h
+++ platform/linux-generic/include/odp_packet_io_internal.h
@@ -38,7 +38,7 @@ extern "C" {
 #include <sys/select.h>
 #include <inttypes.h>
 
-#define PKTIO_MAX_QUEUES 64
+#define PKTIO_MAX_QUEUES ODP_PKTOUT_MAX_QUEUES
 #define PKTIO_LSO_PROFILES 16
 /* Assume at least Ethernet header per each segment */
 #define PKTIO_LSO_MIN_PAYLOAD_OFFSET 14
