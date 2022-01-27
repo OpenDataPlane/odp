2f82e6975317181943014e916f4e7ac8fdb7cec4
Janne Peltonen <janne.peltonen@nokia.com>
2022-01-27 12:15:13 +0200

    test: performance: shorten performance test durations in make check
    Shorten the run time of odp_cpu_bench, odp_crypto, odp_ipsec and
    odp_pktio_ordered performance tests in make check.
    
    The tests are run during make check just to check that they work. It
    is not useful to spend cycles and time in longer and more accurate
    measurements as the results are not used for anything.
    
    Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

---
+++ test/performance/odp_ipsec_run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Copyright (c) 2022, Nokia
+# All rights reserved.
+#
+# SPDX-License-Identifier:      BSD-3-Clause
+
+TEST_DIR="${TEST_DIR:-$(dirname $0)}"
+
+# Run with a small number of iterations in make check
+
+$TEST_DIR/odp_ipsec${EXEEXT} -i 100
+
+if [ $? -ne 0 ] ; then
+    echo Test FAILED
+    exit 1
+fi
+
+exit 0
