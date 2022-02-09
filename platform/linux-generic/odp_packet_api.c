a28a786e8aca1c24d8edf75b4c0cf6a2e31f5eea
Matias Elo <matias.elo@nokia.com>
2022-02-09 08:37:42 +0200

    linux-gen: packet: header include clean-up
    Remove unnecessary, group existing, and add missing header includes.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- platform/linux-generic/odp_packet_api.c
+++ platform/linux-generic/odp_packet_api.c
@@ -5,20 +5,6 @@
  */
 
 #include <odp/api/packet.h>
-#include <odp_packet_internal.h>
-#include <odp_debug_internal.h>
-#include <odp/api/hints.h>
-#include <odp/api/byteorder.h>
-
-#include <protocols/eth.h>
-#include <protocols/ip.h>
-#include <protocols/tcp.h>
-#include <protocols/udp.h>
-
-#include <errno.h>
-#include <string.h>
-#include <stdio.h>
-#include <inttypes.h>
 
 /* Prevent this header from being included again later */
 #include <odp/api/plat/packet_io_inlines.h>
