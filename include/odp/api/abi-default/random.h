2ef815d2144b2c307a77c427c40fce480815f5dd
Matias Elo <matias.elo@nokia.com>
2022-02-10 09:23:05 +0200

    api: random: split random types into a separate header
    Split random types into a separate header file to enable easier function
    inlining. No API changes.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

---
+++ include/odp/api/abi-default/random.h
@@ -0,0 +1,20 @@
+/* Copyright (c) 2022, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef ODP_ABI_RANDOM_H_
+#define ODP_ABI_RANDOM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Empty header required due to the packet inline functions */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
