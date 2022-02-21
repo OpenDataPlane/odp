84da48e1b7614caf85889e5e96cf814aa60c6c95
Tuomas Taipale <tuomas.taipale@nokia.com>
2022-02-21 13:06:24 +0000

    linux-gen: packet: implement packet aging API
    Implement API for handling aging for packets enqeued for transmission.
    
    Signed-off-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- platform/linux-generic/include/odp/api/plat/packet_inline_types.h
+++ platform/linux-generic/include/odp/api/plat/packet_inline_types.h
@@ -115,7 +115,7 @@ typedef union {
 	uint32_t all_flags;
 
 	struct {
-		uint32_t reserved1:      7;
+		uint32_t reserved1:      6;
 
 	/*
 	 * Init flags
@@ -133,6 +133,7 @@ typedef union {
 		uint32_t l4_chksum:      1; /* L4 chksum override */
 		uint32_t ts_set:         1; /* Set Tx timestamp */
 		uint32_t tx_compl:       1; /* Tx completion event requested */
+		uint32_t tx_aging:       1; /* Packet aging at Tx requested */
 		uint32_t shaper_len_adj: 8; /* Adjustment for traffic mgr */
 
 	/*
@@ -150,8 +151,8 @@ typedef union {
 
 	/* Flag groups */
 	struct {
-		uint32_t reserved2:      7;
-		uint32_t other:         17; /* All other flags */
+		uint32_t reserved2:      6;
+		uint32_t other:         18; /* All other flags */
 		uint32_t error:          8; /* All error flags */
 	} all;
 
