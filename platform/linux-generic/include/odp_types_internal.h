d6ae531b1fcdab3d1a5067a0c3be2c1638c1a22c
Jere Leppänen <jere.leppanen@nokia.com>
2022-03-03 14:49:16 +0200

    linux-gen: define a 128-bit integer type
    Define a 128-bit unsigned integer type _odp_u128_t in a new header
    file odp_types_internal.h.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

---
+++ platform/linux-generic/include/odp_types_internal.h
@@ -0,0 +1,24 @@
+/* Copyright (c) 2022, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:	BSD-3-Clause
+ */
+
+#ifndef ODP_TYPES_INTERNAL_H_
+#define ODP_TYPES_INTERNAL_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __SIZEOF_INT128__
+
+__extension__ typedef unsigned __int128 _odp_u128_t;
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
