bf79d7ededc36fa76e56699cbff91650fec17ee5
Matias Elo <matias.elo@nokia.com>
2022-03-08 17:00:52 +0200

    linux-gen: packet_flags: inline input flags getter functions
    Inline remaining packet input flags getter functions.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- platform/linux-generic/include/odp/api/plat/packet_flag_inlines.h
+++ platform/linux-generic/include/odp/api/plat/packet_flag_inlines.h
@@ -1,4 +1,5 @@
 /* Copyright (c) 2017-2018, Linaro Limited
+ * Copyright (c) 2022, Nokia
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -30,11 +31,28 @@ static inline uint64_t _odp_packet_input_flags(odp_packet_t pkt)
 	/* Inline functions by default */
 	#define _ODP_INLINE static inline
 	#define odp_packet_has_l2 __odp_packet_has_l2
+	#define odp_packet_has_l3 __odp_packet_has_l3
+	#define odp_packet_has_l4 __odp_packet_has_l4
 	#define odp_packet_has_eth __odp_packet_has_eth
 	#define odp_packet_has_jumbo __odp_packet_has_jumbo
 	#define odp_packet_has_flow_hash __odp_packet_has_flow_hash
 	#define odp_packet_has_ts __odp_packet_has_ts
 	#define odp_packet_has_ipsec __odp_packet_has_ipsec
+	#define odp_packet_has_eth_bcast __odp_packet_has_eth_bcast
+	#define odp_packet_has_eth_mcast __odp_packet_has_eth_mcast
+	#define odp_packet_has_vlan __odp_packet_has_vlan
+	#define odp_packet_has_vlan_qinq __odp_packet_has_vlan_qinq
+	#define odp_packet_has_arp __odp_packet_has_arp
+	#define odp_packet_has_ipv4 __odp_packet_has_ipv4
+	#define odp_packet_has_ipv6 __odp_packet_has_ipv6
+	#define odp_packet_has_ip_bcast __odp_packet_has_ip_bcast
+	#define odp_packet_has_ip_mcast __odp_packet_has_ip_mcast
+	#define odp_packet_has_ipfrag __odp_packet_has_ipfrag
+	#define odp_packet_has_ipopt __odp_packet_has_ipopt
+	#define odp_packet_has_udp __odp_packet_has_udp
+	#define odp_packet_has_tcp __odp_packet_has_tcp
+	#define odp_packet_has_sctp __odp_packet_has_sctp
+	#define odp_packet_has_icmp __odp_packet_has_icmp
 #else
 	#undef _ODP_INLINE
 	#define _ODP_INLINE
@@ -46,6 +64,22 @@ _ODP_INLINE int odp_packet_has_l2(odp_packet_t pkt)
 
 	flags.all = _odp_packet_input_flags(pkt);
 	return flags.l2;
+}
+
+_ODP_INLINE int odp_packet_has_l3(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.l3;
+}
+
+_ODP_INLINE int odp_packet_has_l4(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.l4;
 }
 
 _ODP_INLINE int odp_packet_has_eth(odp_packet_t pkt)
@@ -86,6 +120,126 @@ _ODP_INLINE int odp_packet_has_ipsec(odp_packet_t pkt)
 
 	flags.all = _odp_packet_input_flags(pkt);
 	return flags.ipsec;
+}
+
+_ODP_INLINE int odp_packet_has_eth_bcast(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.eth_bcast;
+}
+
+_ODP_INLINE int odp_packet_has_eth_mcast(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.eth_mcast;
+}
+
+_ODP_INLINE int odp_packet_has_vlan(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.vlan;
+}
+
+_ODP_INLINE int odp_packet_has_vlan_qinq(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.vlan_qinq;
+}
+
+_ODP_INLINE int odp_packet_has_arp(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.arp;
+}
+
+_ODP_INLINE int odp_packet_has_ipv4(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.ipv4;
+}
+
+_ODP_INLINE int odp_packet_has_ipv6(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.ipv6;
+}
+
+_ODP_INLINE int odp_packet_has_ip_bcast(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.ip_bcast;
+}
+
+_ODP_INLINE int odp_packet_has_ip_mcast(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.ip_mcast;
+}
+
+_ODP_INLINE int odp_packet_has_ipfrag(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.ipfrag;
+}
+
+_ODP_INLINE int odp_packet_has_ipopt(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.ipopt;
+}
+
+_ODP_INLINE int odp_packet_has_udp(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.udp;
+}
+
+_ODP_INLINE int odp_packet_has_tcp(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.tcp;
+}
+
+_ODP_INLINE int odp_packet_has_sctp(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.sctp;
+}
+
+_ODP_INLINE int odp_packet_has_icmp(odp_packet_t pkt)
+{
+	_odp_packet_input_flags_t flags;
+
+	flags.all = _odp_packet_input_flags(pkt);
+	return flags.icmp;
 }
 
 /** @endcond */
