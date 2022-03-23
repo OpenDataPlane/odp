e6abebc4af6edfc6a5b3aa60c768d50af43e49cc
Matias Elo <matias.elo@nokia.com>
2022-03-23 16:27:50 +0200

    abi: timer: add necessary files for function inlining
    Add necessary ABI headers for timer function inlining.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

---
+++ include/odp/api/abi-default/timer.h
@@ -0,0 +1,20 @@
+/* Copyright (c) 2022, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef ODP_ABI_TIMER_H_
+#define ODP_ABI_TIMER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Empty header required due to the timer inline functions */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ include/odp/arch/x86_32-linux/odp/api/abi/timer.h
@@ -0,0 +1,7 @@
+/* Copyright (c) 2022, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include <odp/api/abi-default/timer.h>
