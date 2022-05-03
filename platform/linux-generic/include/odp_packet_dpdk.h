cdfad45f29394026a7f14a8c4162a4302eda11d3
Jere Leppänen <jere.leppanen@nokia.com>
2022-05-03 18:00:28 +0300

    linux-gen: pktio: dpdk: remove packet parse functions
    Remove the dpdk specific packet parse functions. Call the generic
    _odp_packet_parse_common() from _odp_dpdk_packet_parse_common(). Use
    only the checksum validation results from mbuf.
    
    There doesn't appear to be practically any performance benefit from
    using packet_type from the mbuf. It's just as quick to read the same
    information from packet data, right next to other data that we parse
    anyway.
    
    As a side effect, the incorrect L4 offset with IPv6 with extension
    headers and UDP or TCP is fixed, but parsing those packets takes more
    time.
    
    bench_packet_parse (modified to call _odp_dpdk_packet_parse_common()
    and tested also with vlan and qinq packets) does not show much
    performance degradation due to this change. IPv6 with extension
    headers was not tested.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- platform/linux-generic/include/odp_packet_dpdk.h
+++ platform/linux-generic/include/odp_packet_dpdk.h
@@ -27,13 +27,12 @@ struct rte_mbuf;
 #define PTYPE_TCP       0x40
 
 /** Packet parser using DPDK interface */
-int _odp_dpdk_packet_parse_common(packet_parser_t *pkt_hdr,
+int _odp_dpdk_packet_parse_common(odp_packet_hdr_t *pkt_hdr,
 				  const uint8_t *ptr,
 				  uint32_t pkt_len,
 				  uint32_t seg_len,
 				  struct rte_mbuf *mbuf,
 				  int layer,
-				  uint32_t supported_ptypes,
 				  odp_pktin_config_opt_t pktin_cfg);
 
 #endif
