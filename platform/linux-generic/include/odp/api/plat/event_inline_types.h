d31ad31a4a750938ba21c55309610ac75ee91dc7
Matias Elo <matias.elo@nokia.com>
2022-05-20 13:16:15 +0300

    linux-gen: event: move event header inline accessor offsets
    Common event metadata (for all event types) has been moved from
    odp_buffer_hdr_t to _odp_event_hdr_t. Move inline accessor offsets to
    match this change.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>

---
+++ platform/linux-generic/include/odp/api/plat/event_inline_types.h
@@ -0,0 +1,37 @@
+/* Copyright (c) 2018, Linaro Limited
+ * Copyright (c) 2022, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef ODP_PLAT_EVENT_INLINE_TYPES_H_
+#define ODP_PLAT_EVENT_INLINE_TYPES_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+/** @cond _ODP_HIDE_FROM_DOXYGEN_ */
+
+/* Event header field accessor */
+#define _odp_event_hdr_field(event_hdr, cast, field) \
+	(*(cast *)(uintptr_t)((uint8_t *)event_hdr + \
+	 _odp_event_inline_offset.field))
+
+/* Event header field offsets for inline functions */
+typedef struct _odp_event_inline_offset_t {
+	uint16_t event_type;
+	uint16_t base_data;
+
+} _odp_event_inline_offset_t;
+
+/** @endcond */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
