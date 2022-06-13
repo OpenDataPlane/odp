768a080c37fd10e2a76ea4690fe38cb93330634b
Petri Savolainen <petri.savolainen@nokia.com>
2022-06-13 14:57:22 +0300

    api: stash: split header files
    Split stash API into separate header files for functions and
    types. This enables inline implementation of API functions
    in the future.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Ashwin Sekhar T K <asekhar@marvell.com>

--- include/odp/api/abi-default/stash.h
+++ include/odp/api/abi-default/stash.h
@@ -11,22 +11,7 @@
 extern "C" {
 #endif
 
-/** @internal Dummy type for strong typing */
-typedef struct { char dummy; /**< @internal Dummy */ } _odp_abi_stash_t;
-
-/** @ingroup odp_stash
- *  @{
- */
-
-typedef _odp_abi_stash_t *odp_stash_t;
-
-#define ODP_STASH_INVALID   ((odp_stash_t)0)
-
-#define ODP_STASH_NAME_LEN  32
-
-/**
- * @}
- */
+/* Empty header required due to the inline functions */
 
 #ifdef __cplusplus
 }
