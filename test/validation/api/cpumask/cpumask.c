5db92b1de339d2ba28b62adb98cd22b8b8289cae
Petri Savolainen <petri.savolainen@nokia.com>
2022-07-06 12:09:46 +0300

    validation: cpumask: re-write default cpumask tests
    Default CPU mask test were re-written to add tests that set
    worker/control mask pointer to NULL. Fixed couple of asserts
    to be more accurate on CPU counts. Added tests which have
    number of requested CPUs in between of 1 and max. Added tests
    for odp_cpumask_all_available().
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- test/validation/api/cpumask/cpumask.c
+++ test/validation/api/cpumask/cpumask.c
@@ -1,5 +1,5 @@
 /* Copyright (c) 2015-2018, Linaro Limited
- * Copyright (c) 2021, Nokia
+ * Copyright (c) 2021-2022, Nokia
  * All rights reserved.
  *
  * SPDX-License-Identifier:     BSD-3-Clause
@@ -10,82 +10,150 @@
 #include "odp_cunit_common.h"
 #include "mask_common.h"
 
-/* default worker parameter to get all that may be available */
-#define ALL_AVAILABLE 0
+static int cpumask_max_count(void)
+{
+	odp_cpumask_t mask;
+
+	odp_cpumask_setall(&mask);
+
+	return odp_cpumask_count(&mask);
+}
 
 static void cpumask_test_odp_cpumask_def_control(void)
 {
-	unsigned int num, max_num;
-	unsigned int mask_count;
-	unsigned int max_cpus = mask_capacity();
 	odp_cpumask_t mask;
+	int num, count, all;
+	int max = cpumask_max_count();
+	int request = 7;
 
-	num = odp_cpumask_default_control(&mask, ALL_AVAILABLE);
-	mask_count = odp_cpumask_count(&mask);
+	CU_ASSERT_FATAL(max > 1);
 
-	CU_ASSERT(mask_count == num);
+	if (request > max)
+		request = max - 1;
+
+	all = odp_cpumask_default_control(&mask, 0);
+	num = all;
+	count = odp_cpumask_count(&mask);
+
 	CU_ASSERT(num > 0);
-	CU_ASSERT(num <= max_cpus);
+	CU_ASSERT(num == count);
+	CU_ASSERT(num <= max);
 
-	max_num = odp_cpumask_default_control(&mask, max_cpus);
-	mask_count = odp_cpumask_count(&mask);
+	num = odp_cpumask_default_control(&mask, max);
+	count = odp_cpumask_count(&mask);
 
-	CU_ASSERT(max_num > 0);
-	CU_ASSERT(max_num == mask_count);
-	CU_ASSERT(max_num <= max_cpus);
-	CU_ASSERT(max_num <= num);
+	CU_ASSERT(num > 0);
+	CU_ASSERT(num == count);
+	CU_ASSERT(num <= max);
+	CU_ASSERT(num == all);
+
+	num = odp_cpumask_default_control(&mask, 1);
+	count = odp_cpumask_count(&mask);
+
+	CU_ASSERT(num == 1);
+	CU_ASSERT(num == count);
+
+	num = odp_cpumask_default_control(&mask, request);
+	count = odp_cpumask_count(&mask);
+
+	CU_ASSERT(num > 0);
+	CU_ASSERT(num <= request);
+	CU_ASSERT(num == count);
+
+	CU_ASSERT(odp_cpumask_default_control(NULL, request) == num);
+	CU_ASSERT(odp_cpumask_default_control(NULL, 0) == all);
+	CU_ASSERT(odp_cpumask_default_control(NULL, 1) == 1);
 }
 
 static void cpumask_test_odp_cpumask_def_worker(void)
 {
-	unsigned int num, max_num;
-	unsigned int mask_count;
-	unsigned int max_cpus = mask_capacity();
 	odp_cpumask_t mask;
+	int num, count, all;
+	int max = cpumask_max_count();
+	int request = 7;
 
-	num = odp_cpumask_default_worker(&mask, ALL_AVAILABLE);
-	mask_count = odp_cpumask_count(&mask);
+	CU_ASSERT_FATAL(max > 1);
 
-	CU_ASSERT(mask_count == num);
+	if (request > max)
+		request = max - 1;
+
+	all = odp_cpumask_default_worker(&mask, 0);
+	num = all;
+	count = odp_cpumask_count(&mask);
+
 	CU_ASSERT(num > 0);
-	CU_ASSERT(num <= max_cpus);
+	CU_ASSERT(num == count);
+	CU_ASSERT(num <= max);
 
-	max_num = odp_cpumask_default_worker(&mask, max_cpus);
-	mask_count = odp_cpumask_count(&mask);
+	num = odp_cpumask_default_worker(&mask, max);
+	count = odp_cpumask_count(&mask);
 
-	CU_ASSERT(max_num > 0);
-	CU_ASSERT(max_num == mask_count);
-	CU_ASSERT(max_num <= max_cpus);
-	CU_ASSERT(max_num <= num);
+	CU_ASSERT(num > 0);
+	CU_ASSERT(num == count);
+	CU_ASSERT(num <= max);
+	CU_ASSERT(num == all);
+
+	num = odp_cpumask_default_worker(&mask, 1);
+	count = odp_cpumask_count(&mask);
+
+	CU_ASSERT(num == 1);
+	CU_ASSERT(num == count);
+
+	num = odp_cpumask_default_worker(&mask, request);
+	count = odp_cpumask_count(&mask);
+
+	CU_ASSERT(num > 0);
+	CU_ASSERT(num <= request);
+	CU_ASSERT(num == count);
+
+	CU_ASSERT(odp_cpumask_default_worker(NULL, request) == num);
+	CU_ASSERT(odp_cpumask_default_worker(NULL, 0) == all);
+	CU_ASSERT(odp_cpumask_default_worker(NULL, 1) == 1);
 }
 
 static void cpumask_test_odp_cpumask_def(void)
 {
-	unsigned mask_count;
-	unsigned num_worker;
-	unsigned num_control;
-	unsigned max_cpus = mask_capacity();
-	unsigned available_cpus = odp_cpu_count();
-	unsigned requested_cpus;
-	odp_cpumask_t mask;
+	odp_cpumask_t mask, all_mask, overlap;
+	int count, all, num_worker, num_control, request;
+	int max = cpumask_max_count();
+	int cpu_count = odp_cpu_count();
 
-	CU_ASSERT(available_cpus <= max_cpus);
+	all = odp_cpumask_all_available(&all_mask);
+	count = odp_cpumask_count(&all_mask);
 
-	if (available_cpus > 1)
-		requested_cpus = available_cpus - 1;
-	else
-		requested_cpus = available_cpus;
-	num_worker = odp_cpumask_default_worker(&mask, requested_cpus);
-	mask_count = odp_cpumask_count(&mask);
-	CU_ASSERT(mask_count == num_worker);
+	CU_ASSERT_FATAL(cpu_count > 0);
+	CU_ASSERT_FATAL(all > 0);
+	CU_ASSERT(all == cpu_count);
+	CU_ASSERT(all <= max);
+	CU_ASSERT(all == count);
+
+	request = all - 1;
+	if (request == 0)
+		request = 1;
+
+	num_worker = odp_cpumask_default_worker(&mask, request);
+	count = odp_cpumask_count(&mask);
+	CU_ASSERT(num_worker > 0);
+	CU_ASSERT(num_worker <= request);
+	CU_ASSERT(num_worker == count);
+
+	/* Check that CPUs are in the all CPUs mask */
+	odp_cpumask_zero(&overlap);
+	odp_cpumask_and(&overlap, &mask, &all_mask);
+	CU_ASSERT(odp_cpumask_count(&overlap) == num_worker);
 
 	num_control = odp_cpumask_default_control(&mask, 1);
-	mask_count = odp_cpumask_count(&mask);
-	CU_ASSERT(mask_count == num_control);
+	count = odp_cpumask_count(&mask);
+	CU_ASSERT(num_control == 1);
+	CU_ASSERT(num_control == count);
 
-	CU_ASSERT(num_control >= 1);
-	CU_ASSERT(num_worker <= available_cpus);
-	CU_ASSERT(num_worker > 0);
+	odp_cpumask_zero(&overlap);
+	odp_cpumask_and(&overlap, &mask, &all_mask);
+	CU_ASSERT(odp_cpumask_count(&overlap) == num_control);
+
+	CU_ASSERT(odp_cpumask_default_worker(NULL, request) == num_worker);
+	CU_ASSERT(odp_cpumask_default_worker(NULL, 0) <= all);
+	CU_ASSERT(odp_cpumask_default_control(NULL, 0) <= all);
 }
 
 odp_testinfo_t cpumask_suite[] = {
