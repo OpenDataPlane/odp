6d2b2b1fa07bfa6c4f2bf82321bf9eb182a875d8
Petri Savolainen <petri.savolainen@nokia.com>
2022-07-06 13:38:28 +0300

    validation: use NULL when cpumask is not needed
    Set cpumask to NULL on odp_cpumask_default_worker() calls when
    only number of CPUs return value is used.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- test/validation/api/pool/pool.c
+++ test/validation/api/pool/pool.c
@@ -895,7 +895,6 @@ static int run_pool_test_create_after_fork(void *arg ODP_UNUSED)
 static void pool_test_create_after_fork(void)
 {
 	odp_shm_t shm;
-	odp_cpumask_t unused;
 	int num;
 
 	/* No single VA required since reserve is done before fork */
@@ -904,7 +903,7 @@ static void pool_test_create_after_fork(void)
 	global_mem = odp_shm_addr(shm);
 	CU_ASSERT_PTR_NOT_NULL_FATAL(global_mem);
 
-	num = odp_cpumask_default_worker(&unused, 0);
+	num = odp_cpumask_default_worker(NULL, 0);
 	if (num > MAX_WORKERS)
 		num = MAX_WORKERS;
 
