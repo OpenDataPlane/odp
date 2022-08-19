763559ba959d0c0d0bf74140ad5a4b1bce83eb93
Jere Leppänen <jere.leppanen@nokia.com>
2022-08-19 19:17:45 +0300

    test: misc: add API headers compilation test
    This test compiles ODP API headers and ODP Helper API headers with
    additional compiler options, such as warning options. This can be used
    to test application compilation with stricter options than those used
    in ODP compilation.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

---
+++ test/miscellaneous/odp_api_headers.c
@@ -0,0 +1,14 @@
+/* Copyright (c) 2022, Nokia
+ *
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include <odp_api.h>
+#include <odp/helper/odph_api.h>
+
+int main(void)
+{
+	return 0;
+}
