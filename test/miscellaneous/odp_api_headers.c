b00b8c06310602cc34bdebf6f3eed978ea95864d
Jere Leppänen <jere.leppanen@nokia.com>
2022-09-20 17:01:30 +0300

    test: misc: build and run a test application during installcheck
    During installcheck, build and run a test application
    (odp_api_headers), to verify the install.
    
    When cross compiling, the application is built, but not run.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- test/miscellaneous/odp_api_headers.c
+++ test/miscellaneous/odp_api_headers.c
@@ -10,5 +10,30 @@
 
 int main(void)
 {
+	odp_instance_t inst;
+
+	if (odp_init_global(&inst, NULL, NULL)) {
+		ODPH_ERR("Global init failed.\n");
+		return -1;
+	}
+
+	if (odp_init_local(inst, ODP_THREAD_CONTROL)) {
+		ODPH_ERR("Local init failed.\n");
+		return -1;
+	}
+
+	odp_sys_info_print();
+	printf("Helper library version: %s\n", odph_version_str());
+
+	if (odp_term_local()) {
+		ODPH_ERR("Local term failed.\n");
+		return -1;
+	}
+
+	if (odp_term_global(inst)) {
+		ODPH_ERR("Global term failed.\n");
+		return -1;
+	}
+
 	return 0;
 }
