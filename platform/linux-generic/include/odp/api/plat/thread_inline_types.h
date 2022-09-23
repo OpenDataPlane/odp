c9abb944a36c8669a0232a88fa8518c91b339e99
Matias Elo <matias.elo@nokia.com>
2022-09-23 16:53:14 +0300

    linux-gen: thread: move inline types into a separate header
    Move thread inline types into a separate header file. Enables usage of
    these types from debug_inlines.h header.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

---
+++ platform/linux-generic/include/odp/api/plat/thread_inline_types.h
@@ -0,0 +1,36 @@
+/* Copyright (c) 2018-2018, Linaro Limited
+ * Copyright (c) 2022, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#ifndef ODP_PLAT_THREAD_INLINE_TYPES_H_
+#define ODP_PLAT_THREAD_INLINE_TYPES_H_
+
+#include <odp/api/init.h>
+#include <odp/api/spec/thread_types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @cond _ODP_HIDE_FROM_DOXYGEN_ */
+
+typedef struct {
+	odp_log_func_t log_fn;
+	odp_thread_type_t type;
+	int thr;
+	int cpu;
+
+} _odp_thread_state_t;
+
+extern __thread _odp_thread_state_t *_odp_this_thread;
+
+/** @endcond */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
