0a8460c27a0592933cccefa3c16fe73dc2e3e50a
Matias Elo <matias.elo@nokia.com>
2022-10-28 13:14:42 +0300

    linux-gen: ipsec: inline event conversion functions
    Inline odp_ipsec_packet_from_event() and odp_ipsec_packet_to_event()
    function implementations.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

---
+++ platform/linux-generic/odp_ipsec_api.c
@@ -0,0 +1,11 @@
+/* Copyright (c) 2022, Nokia
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+#include <odp/api/ipsec.h>
+
+/* Non-inlined versions of API functions */
+#define _ODP_NO_INLINE
+#include <odp/api/plat/ipsec_inlines.h>
