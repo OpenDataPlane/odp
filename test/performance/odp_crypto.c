1af0ed908fb8b654329dce9c199bbbd14f53673d
Janne Peltonen <janne.peltonen@nokia.com>
2022-10-31 12:29:19 +0200

    test: performance: ipsec: authenticate cipher text instead of vice versa
    Authenticate the cipher text instead of the plain text since that order
    may be more common (it is used e.g. in IPsec) and perhaps more often
    supported by ODP implementations.
    
    Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

--- test/performance/odp_crypto.c
+++ test/performance/odp_crypto.c
@@ -611,6 +611,7 @@ create_session_from_config(odp_crypto_session_t *session,
 	odp_crypto_session_param_init(&params);
 	memcpy(&params, &config->session, sizeof(odp_crypto_session_param_t));
 	params.op = ODP_CRYPTO_OP_ENCODE;
+	params.auth_cipher_text = true;
 
 	/* Lookup the packet pool */
 	pkt_pool = odp_pool_lookup("packet_pool");
