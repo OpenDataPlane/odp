5e959a5204375bce20654ef5e1dcf33e9e8d010c
Janne Peltonen <janne.peltonen@nokia.com>
2023-02-20 19:29:38 +0200

    linux-gen: crypto: set hw_err status only if deprecated API enabled
    Prepare for the deprecation of the hw_err operation status field by
    setting it only when compiled with deprecated API enabled.
    
    Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>

--- platform/linux-generic/odp_crypto_ipsecmb.c
+++ platform/linux-generic/odp_crypto_ipsecmb.c
@@ -913,9 +913,11 @@ int crypto_int(odp_packet_t pkt_in,
 	packet_subtype_set(out_pkt, ODP_EVENT_PACKET_CRYPTO);
 	op_result = &packet_hdr(out_pkt)->crypto_op_result;
 	op_result->cipher_status.alg_err = rc_cipher;
-	op_result->cipher_status.hw_err = ODP_CRYPTO_HW_ERR_NONE;
 	op_result->auth_status.alg_err = rc_auth;
+#if ODP_DEPRECATED_API
+	op_result->cipher_status.hw_err = ODP_CRYPTO_HW_ERR_NONE;
 	op_result->auth_status.hw_err = ODP_CRYPTO_HW_ERR_NONE;
+#endif
 	op_result->ok =
 		(rc_cipher == ODP_CRYPTO_ALG_ERR_NONE) &&
 		(rc_auth == ODP_CRYPTO_ALG_ERR_NONE);
