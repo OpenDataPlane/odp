4baa11a74dd0f0fce6411fd94123e6ecf1e0c6ed
Matias Elo <matias.elo@nokia.com>
2023-03-23 14:43:15 +0200

    ci: add dpdk install directory to pkg-config path
    Add CentOS and Rocky Linux default DPDK install directory to
    PKG_CONFIG_PATH.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- scripts/ci/build_x86_64.sh
+++ scripts/ci/build_x86_64.sh
@@ -5,4 +5,7 @@ if [ "${CC#clang}" != "${CC}" ] ; then
 	export CXX="clang++"
 fi
 
+# Required by CentOS and Rocky Linux to find DPDK install
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib64/pkgconfig
+
 exec "$(dirname "$0")"/build.sh
