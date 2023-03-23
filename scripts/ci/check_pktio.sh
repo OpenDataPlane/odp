2ace3cf68390e7490a6f34a7ac62042f9fc17e7e
Matias Elo <matias.elo@nokia.com>
2023-03-23 14:59:28 +0200

    ci: mount huge pages before odp build
    Mounting huge pages fails randomly in GitHub Actions if VM disk is close to
    full. Maximize probability that mounting succeeds by mounting huge pages
    before ODP build.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- scripts/ci/check_pktio.sh
+++ scripts/ci/check_pktio.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-"`dirname "$0"`"/build_${ARCH}.sh
-
-cd "$(dirname "$0")"/../..
-
 echo 1000 | tee /proc/sys/vm/nr_hugepages
 mkdir -p /mnt/huge
 mount -t hugetlbfs nodev /mnt/huge
+
+"`dirname "$0"`"/build_${ARCH}.sh
+
+cd "$(dirname "$0")"/../..
 
 ./platform/linux-generic/test/validation/api/pktio/pktio_run.sh
 
