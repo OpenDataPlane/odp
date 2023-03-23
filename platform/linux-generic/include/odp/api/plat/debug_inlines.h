fd27ef522ab5c6c2d345da7efed30f8941d1d51a
Jere Leppänen <jere.leppanen@nokia.com>
2023-03-23 16:06:59 +0200

    linux-gen: debug: re-enable print format warnings
    Commit fc0820289bee ("linux-gen: debug: fix variadic macro build
    errors") also disabled print format warnings as a side effect. Remove
    the pragma used in that commit, and instead avoid the variadic macro
    warnings with '__extension__' keyword.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>

--- platform/linux-generic/include/odp/api/plat/debug_inlines.h
+++ platform/linux-generic/include/odp/api/plat/debug_inlines.h
@@ -30,19 +30,21 @@
 extern "C" {
 #endif
 
-/* Avoid "ISO C99 requires at least one argument for the "..."  in a variadic
- * macro" errors when building with 'pedantic' option. */
-#pragma GCC system_header
+#pragma GCC diagnostic push
+
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+#endif
 
 extern odp_log_func_t ODP_PRINTF_FORMAT(2, 3) _odp_log_fn;
 extern odp_abort_func_t _odp_abort_fn;
 
-#define _ODP_LOG_FN(level, fmt, ...) \
+#define _ODP_LOG_FN(level, ...) \
 	do { \
 		if (_odp_this_thread && _odp_this_thread->log_fn) \
-			_odp_this_thread->log_fn(level, fmt, ##__VA_ARGS__); \
+			_odp_this_thread->log_fn(level, ##__VA_ARGS__); \
 		else \
-			_odp_log_fn(level, fmt, ##__VA_ARGS__); \
+			_odp_log_fn(level, ##__VA_ARGS__); \
 	} while (0)
 
 /**
@@ -64,25 +66,33 @@ extern odp_abort_func_t _odp_abort_fn;
 /*
  * Print debug message to log, if ODP_DEBUG_PRINT flag is set (ignores CONFIG_DEBUG_LEVEL).
  */
-#define _ODP_DBG(fmt, ...) \
+#define _ODP_DBG(...) \
 	do { \
 		if (ODP_DEBUG_PRINT == 1) \
-			_ODP_LOG(ODP_LOG_DBG, fmt, ##__VA_ARGS__);\
+			__extension__ ({ \
+				_ODP_LOG(ODP_LOG_DBG, ##__VA_ARGS__); \
+			}); \
 	} while (0)
 
 /**
  * Log error message.
  */
-#define _ODP_ERR(fmt, ...) \
-	_ODP_LOG(ODP_LOG_ERR, fmt, ##__VA_ARGS__)
+#define _ODP_ERR(...) \
+	do { \
+		__extension__ ({ \
+			_ODP_LOG(ODP_LOG_ERR, ##__VA_ARGS__); \
+		}); \
+	} while (0)
 
 /**
  * Log abort message and then stop execution (by default call abort()).
  * This function should not return.
  */
-#define _ODP_ABORT(fmt, ...) \
+#define _ODP_ABORT(...) \
 	do { \
-		_ODP_LOG(ODP_LOG_ABORT, fmt, ##__VA_ARGS__); \
+		__extension__ ({ \
+			_ODP_LOG(ODP_LOG_ABORT, ##__VA_ARGS__); \
+		}); \
 		_odp_abort_fn(); \
 	} while (0)
 
@@ -90,8 +100,10 @@ extern odp_abort_func_t _odp_abort_fn;
  * Log print message when the application calls one of the ODP APIs
  * specifically for dumping internal data.
  */
-#define _ODP_PRINT(fmt, ...) \
-	_ODP_LOG_FN(ODP_LOG_PRINT, fmt, ##__VA_ARGS__)
+#define _ODP_PRINT(...) \
+	_ODP_LOG_FN(ODP_LOG_PRINT, ##__VA_ARGS__)
+
+#pragma GCC diagnostic pop
 
 #ifdef __cplusplus
 }
