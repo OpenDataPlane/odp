76160c1811e7372b4547ffe6441c6da2d21b0bfd
Matias Elo <matias.elo@nokia.com>
2023-03-29 08:56:26 +0300

    ci: add dpdk install directory to pkg-config path for arm64
    Add CentOS and Rocky Linux default DPDK install directory to
    PKG_CONFIG_PATH for native arm64 builds.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- scripts/ci/build_arm64.sh
+++ scripts/ci/build_arm64.sh
@@ -11,6 +11,10 @@ if [ "$TARGET_ARCH" == "$BUILD_ARCH" ]; then
 	if [ "${CC#clang}" != "${CC}" ] ; then
 		export CXX="clang++"
 	fi
+
+	# Required by CentOS and Rocky Linux to find DPDK install
+	export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib64/pkgconfig
+
 else
 	# Cross compilation
 	if [ "${CC#clang}" != "${CC}" ] ; then
