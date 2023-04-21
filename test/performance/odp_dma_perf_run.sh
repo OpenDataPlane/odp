4ca65d1f8e3e5032f6b367935067e55ba415d75a
Tuomas Taipale <tuomas.taipale@nokia.com>
2023-04-21 08:55:12 +0000

    test: performance: refactor odp_dma_perf run script
    Adjust `odp_dma_perf` run script according to tester changes.
    
    Signed-off-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- test/performance/odp_dma_perf_run.sh
+++ test/performance/odp_dma_perf_run.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #
-# Copyright (c) 2022, Nokia
+# Copyright (c) 2022-2023, Nokia
 # All rights reserved.
 #
 # SPDX-License-Identifier:	BSD-3-Clause
 
 TEST_DIR="${TEST_DIR:-$(dirname $0)}"
 BIN_NAME=odp_dma_perf
-SEG_SIZE=1024
-ROUNDS=1000
+SEGC=0
+SEGS=1024
+INFL=1
+TIME=1
 TESTS_RUN=0
 
 check_result()
@@ -23,73 +25,24 @@ check_result()
 	fi
 }
 
-echo "odp_dma_perf: synchronous transfer 1"
+echo "odp_dma_perf: synchronous transfer"
 echo "===================================="
 
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 0 -g 0 -i 6 -s $SEG_SIZE -T 0 -r $ROUNDS
-
-check_result $?
-
-echo "odp_dma_perf: synchronous transfer 2"
-echo "===================================="
-
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 0 -g 1 -i 6 -s $SEG_SIZE -T 0 -r $ROUNDS
-
-check_result $?
-
-echo "odp_dma_perf: synchronous transfer 3"
-echo "===================================="
-
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 0 -g 0 -i 6 -s $SEG_SIZE -T 1 -r $ROUNDS
-
-check_result $?
-
-echo "odp_dma_perf: synchronous transfer 4"
-echo "===================================="
-
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 0 -g 1 -i 6 -s $SEG_SIZE -T 1 -r $ROUNDS
+${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 0 -i $SEGC -o $SEGC -s $SEGS -S 0 -f $INFL -T $TIME
 
 check_result $?
 
 echo "odp_dma_perf: asynchronous transfer 1"
 echo "====================================="
 
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -g 0 -i 6 -s $SEG_SIZE -T 0 -m 0 -r $ROUNDS
+${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -i $SEGC -o $SEGC -s $SEGS -S 1 -m 0 -f $INFL -T $TIME
 
 check_result $?
 
 echo "odp_dma_perf: asynchronous transfer 2"
 echo "====================================="
 
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -g 1 -i 6 -s $SEG_SIZE -T 0 -m 0,0,0,0,0,0 -r $ROUNDS
-
-check_result $?
-
-echo "odp_dma_perf: asynchronous transfer 3"
-echo "====================================="
-
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -g 1 -i 6 -s $SEG_SIZE -T 0 -m 0,0,0,0,0,1 -r $ROUNDS
-
-check_result $?
-
-echo "odp_dma_perf: asynchronous transfer 4"
-echo "====================================="
-
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -g 0 -i 6 -s $SEG_SIZE -T 1 -m 0 -r $ROUNDS
-
-check_result $?
-
-echo "odp_dma_perf: asynchronous transfer 5"
-echo "====================================="
-
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -g 1 -i 6 -s $SEG_SIZE -T 1 -m 0,0,0,0,0,0 -r $ROUNDS
-
-check_result $?
-
-echo "odp_dma_perf: asynchronous transfer 6"
-echo "====================================="
-
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -g 1 -i 6 -s $SEG_SIZE -T 1 -m 0,0,0,0,0,1 -r $ROUNDS
+${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -i $SEGC -o $SEGC -s $SEGS -S 1 -m 1 -f $INFL -T $TIME
 
 check_result $?
 
