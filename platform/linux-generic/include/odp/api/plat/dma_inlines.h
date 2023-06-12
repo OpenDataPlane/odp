6990f9a52f511a693a771cea050de7e7eb590e4d
Matias Elo <matias.elo@nokia.com>
2023-06-12 17:32:28 +0300

    linux-gen: dma: inline completion event alloc/free functions
    Inline implementations of odp_dma_compl_alloc() and odp_dma_compl_free()
    functions.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>

--- platform/linux-generic/include/odp/api/plat/dma_inlines.h
+++ platform/linux-generic/include/odp/api/plat/dma_inlines.h
@@ -11,6 +11,7 @@
 #include <odp/api/dma_types.h>
 #include <odp/api/event_types.h>
 #include <odp/api/hints.h>
+#include <odp/api/pool_types.h>
 #include <odp/api/queue_types.h>
 
 #include <odp/api/plat/debug_inlines.h>
@@ -30,6 +31,8 @@
 	#define odp_dma_compl_result __odp_dma_compl_result
 	#define odp_dma_transfer_param_init __odp_dma_transfer_param_init
 	#define odp_dma_compl_param_init __odp_dma_compl_param_init
+	#define odp_dma_compl_alloc __odp_dma_compl_alloc
+	#define odp_dma_compl_free __odp_dma_compl_free
 #else
 	#define _ODP_INLINE
 #endif
@@ -86,6 +89,45 @@ _ODP_INLINE void odp_dma_compl_param_init(odp_dma_compl_param_t *compl_param)
 	compl_param->queue = ODP_QUEUE_INVALID;
 	compl_param->event = ODP_EVENT_INVALID;
 	compl_param->transfer_id = ODP_DMA_TRANSFER_ID_INVALID;
+}
+
+_ODP_INLINE odp_dma_compl_t odp_dma_compl_alloc(odp_pool_t pool)
+{
+	odp_buffer_t buf;
+	odp_event_t ev;
+	odp_dma_result_t *result;
+	int8_t *ev_type;
+
+	buf = odp_buffer_alloc(pool);
+	if (odp_unlikely(buf == ODP_BUFFER_INVALID))
+		return ODP_DMA_COMPL_INVALID;
+
+	result = (odp_dma_result_t *)odp_buffer_addr(buf);
+	memset(result, 0, sizeof(odp_dma_result_t));
+
+	ev = odp_buffer_to_event(buf);
+	ev_type = _odp_event_hdr_ptr(ev, int8_t, event_type);
+	*ev_type = ODP_EVENT_DMA_COMPL;
+
+	return (odp_dma_compl_t)(uintptr_t)buf;
+}
+
+_ODP_INLINE void odp_dma_compl_free(odp_dma_compl_t dma_compl)
+{
+	int8_t *ev_type;
+	odp_event_t ev;
+	odp_buffer_t buf = (odp_buffer_t)(uintptr_t)dma_compl;
+
+	if (odp_unlikely(dma_compl == ODP_DMA_COMPL_INVALID)) {
+		_ODP_ERR("Bad DMA compl handle\n");
+		return;
+	}
+
+	ev = odp_buffer_to_event(buf);
+	ev_type = _odp_event_hdr_ptr(ev, int8_t, event_type);
+	*ev_type = ODP_EVENT_BUFFER;
+
+	odp_buffer_free(buf);
 }
 
 /** @endcond */
