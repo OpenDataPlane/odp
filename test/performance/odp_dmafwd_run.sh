edc0084406f60b04bb73932058a0465e54265430
Tuomas Taipale <tuomas.taipale@nokia.com>
2023-08-25 12:46:59 +0000

    test: dmafwd: add CI integration
    Add test script and build files to setup a test run of `odp_dmafwd`
    with a few different scenarios during `make check`.
    
    Signed-off-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

---
+++ test/performance/odp_dmafwd_run.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Nokia
+
+TEST_DIR="${TEST_DIR:-$PWD}"
+TEST_SRC_DIR=$(dirname $0)
+PERF_TEST_DIR=platform/${ODP_PLATFORM}/test/performance
+PERF_TEST_DIR=${TEST_SRC_DIR}/../../${PERF_TEST_DIR}
+
+BIN_NAME=odp_dmafwd
+BATCH=10
+TIME=2
+TESTS_RUN=0
+
+check_env()
+{
+	if [ -f "./pktio_env" ]; then
+		. ./pktio_env
+	elif  [ "${ODP_PLATFORM}" = "" ]; then
+		echo "$0: ERROR: ODP_PLATFORM must be defined"
+		exit 1
+	elif [ -f ${PERF_TEST_DIR}/dmafwd/pktio_env ]; then
+		. ${PERF_TEST_DIR}/dmafwd/pktio_env
+	else
+		echo "ERROR: unable to find pktio_env"
+		echo "pktio_env has to be in current directory or in platform/\$ODP_PLATFORM/test/"
+		echo "ODP_PLATFORM=\"${ODP_PLATFORM}\""
+		exit 1
+	fi
+}
+
+check_result()
+{
+	if [ $1 -eq 0 ]; then
+		TESTS_RUN=`expr $TESTS_RUN + 1`
+	elif [ $1 -eq 1 ]; then
+		echo "Test FAILED, exiting"
+		exit 1
+	else
+		echo "Test SKIPPED"
+		return 0
+	fi
+
+	validate_result
+}
+
+check_exit()
+{
+	if [ $TESTS_RUN -eq 0 ]; then
+		exit 77
+	fi
+
+	exit 0
+}
+
+check_env
+setup_interfaces
+echo "${BIN_NAME}: SW copy"
+echo "==================="
+./${BIN_NAME}${EXEEXT} -i ${IF0} -b ${BATCH} -T ${TIME} -t 0
+check_result $?
+echo "${BIN_NAME}: DMA copy"
+echo "===================="
+./${BIN_NAME}${EXEEXT} -i ${IF0} -b ${BATCH} -T ${TIME} -t 1
+check_result $?
+cleanup_interfaces
+check_exit
