998592ba2c1f47ba5bdfaaf6161a9b70943256ed
Matias Elo <matias.elo@nokia.com>
2023-08-28 16:45:58 +0300

    test: l2fwd: shorten tests run length
    Shorten test script run from 30 sec to 5 sec.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- test/performance/odp_l2fwd_run.sh
+++ test/performance/odp_l2fwd_run.sh
@@ -85,7 +85,7 @@ run_l2fwd()
 	LOG=odp_l2fwd_tmp.log
 
 	# Max 2 workers
-	$STDBUF odp_l2fwd${EXEEXT} -i $IF1,$IF2 -m 0 -t 30 -c 2 | tee $LOG
+	$STDBUF odp_l2fwd${EXEEXT} -i $IF1,$IF2 -m 0 -t 5 -c 2 | tee $LOG
 	ret=${PIPESTATUS[0]}
 
 	kill -2 ${GEN_PID}
