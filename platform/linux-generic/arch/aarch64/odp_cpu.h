c7080bd3d072bf2cfaf85a342ac6e7ef1408af0c
Matias Elo <matias.elo@nokia.com>
2023-11-14 16:00:04 +0200

    linux-gen: cpu: optimize aarch64 error branches
    Optimize aarch64 specific CPU functions' error branches.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

--- platform/linux-generic/arch/aarch64/odp_cpu.h
+++ platform/linux-generic/arch/aarch64/odp_cpu.h
@@ -46,18 +46,18 @@ static inline uint16_t ll8(uint8_t *var, int mm)
 {
 	uint16_t old;
 
+	_ODP_ASSERT(mm == __ATOMIC_ACQUIRE || mm == __ATOMIC_RELAXED);
+
 	if (mm == __ATOMIC_ACQUIRE)
 		__asm__ volatile("ldaxrb %w0, [%1]"
 				 : "=&r" (old)
 				 : "r" (var)
 				 : "memory");
-	else if (mm == __ATOMIC_RELAXED)
+	else
 		__asm__ volatile("ldxrb %w0, [%1]"
 				 : "=&r" (old)
 				 : "r" (var)
 				 : );
-	else
-		_ODP_ABORT();
 	return old;
 }
 
@@ -65,18 +65,18 @@ static inline uint32_t ll32(uint32_t *var, int mm)
 {
 	uint32_t old;
 
+	_ODP_ASSERT(mm == __ATOMIC_ACQUIRE || mm == __ATOMIC_RELAXED);
+
 	if (mm == __ATOMIC_ACQUIRE)
 		__asm__ volatile("ldaxr %w0, [%1]"
 				 : "=&r" (old)
 				 : "r" (var)
 				 : "memory");
-	else if (mm == __ATOMIC_RELAXED)
+	else
 		__asm__ volatile("ldxr %w0, [%1]"
 				 : "=&r" (old)
 				 : "r" (var)
 				 : );
-	else
-		_ODP_ABORT();
 	return old;
 }
 
@@ -85,18 +85,18 @@ static inline uint32_t sc32(uint32_t *var, uint32_t neu, int mm)
 {
 	uint32_t ret;
 
+	_ODP_ASSERT(mm == __ATOMIC_RELEASE || mm == __ATOMIC_RELAXED);
+
 	if (mm == __ATOMIC_RELEASE)
 		__asm__ volatile("stlxr %w0, %w1, [%2]"
 				 : "=&r" (ret)
 				 : "r" (neu), "r" (var)
 				 : "memory");
-	else if (mm == __ATOMIC_RELAXED)
+	else
 		__asm__ volatile("stxr %w0, %w1, [%2]"
 				 : "=&r" (ret)
 				 : "r" (neu), "r" (var)
 				 : );
-	else
-		_ODP_ABORT();
 	return ret;
 }
 
@@ -104,18 +104,18 @@ static inline uint64_t ll64(uint64_t *var, int mm)
 {
 	uint64_t old;
 
+	_ODP_ASSERT(mm == __ATOMIC_ACQUIRE || mm == __ATOMIC_RELAXED);
+
 	if (mm == __ATOMIC_ACQUIRE)
 		__asm__ volatile("ldaxr %0, [%1]"
 				 : "=&r" (old)
 				 : "r" (var)
 				 : "memory");
-	else if (mm == __ATOMIC_RELAXED)
+	else
 		__asm__ volatile("ldxr %0, [%1]"
 				 : "=&r" (old)
 				 : "r" (var)
 				 : );
-	else
-		_ODP_ABORT();
 	return old;
 }
 
@@ -124,18 +124,18 @@ static inline uint32_t sc64(uint64_t *var, uint64_t neu, int mm)
 {
 	uint32_t ret;
 
+	_ODP_ASSERT(mm == __ATOMIC_RELEASE || mm == __ATOMIC_RELAXED);
+
 	if (mm == __ATOMIC_RELEASE)
 		__asm__ volatile("stlxr %w0, %1, [%2]"
 				 : "=&r" (ret)
 				 : "r" (neu), "r" (var)
 				 : "memory");
-	else if (mm == __ATOMIC_RELAXED)
+	else
 		__asm__ volatile("stxr %w0, %1, [%2]"
 				 : "=&r" (ret)
 				 : "r" (neu), "r" (var)
 				 : );
-	else
-		_ODP_ABORT();
 	return ret;
 }
 
@@ -148,18 +148,18 @@ static inline _odp_u128_t lld(_odp_u128_t *var, int mm)
 {
 	union i128 old;
 
+	_ODP_ASSERT(mm == __ATOMIC_ACQUIRE || mm == __ATOMIC_RELAXED);
+
 	if (mm == __ATOMIC_ACQUIRE)
 		__asm__ volatile("ldaxp %0, %1, [%2]"
 				 : "=&r" (old.i64[0]), "=&r" (old.i64[1])
 				 : "r" (var)
 				 : "memory");
-	else if (mm == __ATOMIC_RELAXED)
+	else
 		__asm__ volatile("ldxp %0, %1, [%2]"
 				 : "=&r" (old.i64[0]), "=&r" (old.i64[1])
 				 : "r" (var)
 				 : );
-	else
-		_ODP_ABORT();
 	return old.i128;
 }
 
@@ -167,6 +167,8 @@ static inline _odp_u128_t lld(_odp_u128_t *var, int mm)
 static inline uint32_t scd(_odp_u128_t *var, _odp_u128_t neu, int mm)
 {
 	uint32_t ret;
+
+	_ODP_ASSERT(mm == __ATOMIC_RELEASE || mm == __ATOMIC_RELAXED);
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpedantic"
@@ -177,15 +179,13 @@ static inline uint32_t scd(_odp_u128_t *var, _odp_u128_t neu, int mm)
 				   "r" (((*(union i128 *)&neu)).i64[1]),
 				   "r" (var)
 				 : "memory");
-	else if (mm == __ATOMIC_RELAXED)
+	else
 		__asm__ volatile("stxp %w0, %1, %2, [%3]"
 				 : "=&r" (ret)
 				 : "r" (((*(union i128 *)&neu)).i64[0]),
 				   "r" (((*(union i128 *)&neu)).i64[1]),
 				   "r" (var)
 				 : );
-	else
-		_ODP_ABORT();
 #pragma GCC diagnostic pop
 	return ret;
 }
