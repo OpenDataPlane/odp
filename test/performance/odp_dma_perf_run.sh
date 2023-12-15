c0b7fb84bdfc57b7587c2299c01efb1c3a1933a9
Tuomas Taipale <tuomas.taipale@nokia.com>
2023-12-15 14:50:53 +0000

    test: dma_perf: run with new verify option
    Add new `-v` option to the runner script transfers.
    
    Signed-off-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- test/performance/odp_dma_perf_run.sh
+++ test/performance/odp_dma_perf_run.sh
@@ -28,42 +28,42 @@ check_result()
 echo "odp_dma_perf: synchronous DMA transfer 1"
 echo "===================================="
 
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 0 -i $SEGC -o $SEGC -s $SEGS -S 0 -f $INFL -T $TIME
+${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 0 -i $SEGC -o $SEGC -s $SEGS -S 0 -f $INFL -T $TIME -v
 
 check_result $?
 
 echo "odp_dma_perf: synchronous DMA transfer 2"
 echo "===================================="
 
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 0 -i $SEGC -o $SEGC -s $SEGS -S 1 -f $INFL -T $TIME
+${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 0 -i $SEGC -o $SEGC -s $SEGS -S 1 -f $INFL -T $TIME -v
 
 check_result $?
 
 echo "odp_dma_perf: asynchronous DMA transfer 1"
 echo "====================================="
 
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -i $SEGC -o $SEGC -s $SEGS -S 2 -m 0 -f $INFL -T $TIME
+${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -i $SEGC -o $SEGC -s $SEGS -S 2 -m 0 -f $INFL -T $TIME -v
 
 check_result $?
 
 echo "odp_dma_perf: asynchronous DMA transfer 2"
 echo "====================================="
 
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -i $SEGC -o $SEGC -s $SEGS -S 3 -m 1 -f $INFL -T $TIME
+${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 1 -i $SEGC -o $SEGC -s $SEGS -S 3 -m 1 -f $INFL -T $TIME -v
 
 check_result $?
 
 echo "odp_dma_perf: SW transfer 1"
 echo "====================================="
 
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 2 -i $SEGC -o $SEGC -s $SEGS -S 0 -f $INFL -T $TIME
+${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 2 -i $SEGC -o $SEGC -s $SEGS -S 0 -f $INFL -T $TIME -v
 
 check_result $?
 
 echo "odp_dma_perf: SW transfer 2"
 echo "====================================="
 
-${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 2 -i $SEGC -o $SEGC -s $SEGS -S 2 -f $INFL -T $TIME
+${TEST_DIR}/${BIN_NAME}${EXEEXT} -t 2 -i $SEGC -o $SEGC -s $SEGS -S 2 -f $INFL -T $TIME -v
 
 check_result $?
 
