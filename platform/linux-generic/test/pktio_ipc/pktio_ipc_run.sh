3e20ca8e1b4836cab3bd0916bb8b38dc5798ee52
Janne Peltonen <janne.peltonen@nokia.com>
2023-12-21 13:34:21 +0200

    linux-gen: test: pktio_ipc: reduce test run time
    Simplify the script and reduce test run time now that start time can
    be set separately from run time.
    
    Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

--- platform/linux-generic/test/pktio_ipc/pktio_ipc_run.sh
+++ platform/linux-generic/test/pktio_ipc/pktio_ipc_run.sh
@@ -17,31 +17,22 @@ PATH=$(dirname $0)/../../../../platform/linux-generic/test/pktio_ipc:$PATH
 PATH=.:$PATH
 
 STARTTIME=30
-RUNTIME1=3
-RUNTIME2=1
-TIMEOUT=3
-if [ "${TEST}" = "coverage" ]; then
-	RUNTIME1=30
-	RUNTIME2=15
-	TIMEOUT=20
-fi
+RUNTIME=1
 
 run()
 {
 	local ret=0
 
 	echo "==== run pktio_ipc1 then pktio_ipc2 ===="
-	pktio_ipc1${EXEEXT} -s ${STARTTIME} -t ${RUNTIME1} &
+	pktio_ipc1${EXEEXT} -s ${STARTTIME} -t ${RUNTIME} &
 	IPC_PID=$!
 
-	pktio_ipc2${EXEEXT} -p ${IPC_PID}  -s ${STARTTIME} -t ${RUNTIME2}
+	pktio_ipc2${EXEEXT} -p ${IPC_PID}  -s ${STARTTIME} -t ${RUNTIME}
 	ret=$?
-	# pktio_ipc1 should do clean up and exit just
-	# after pktio_ipc2 exited. If it does not happen
-	# kill him in test.
-	sleep ${TIMEOUT}
+
 	(kill ${IPC_PID} 2>&1 > /dev/null ) > /dev/null
 	if [ $? -eq 0 ]; then
+		wait $IPC_PID
 		echo "pktio_ipc1${EXEEXT} was killed"
 		ls -l /dev/shm/${UID}/odp* 2> /dev/null
 		rm -rf /dev/shm/${UID}/odp-${IPC_PID}* 2>&1 > /dev/null
@@ -58,16 +49,15 @@ run()
 	fi
 
 	echo "==== run pktio_ipc2 then pktio_ipc1 ===="
-	pktio_ipc2${EXEEXT}  -s ${STARTTIME} -t ${RUNTIME1} &
+	pktio_ipc2${EXEEXT}  -s ${STARTTIME} -t ${RUNTIME} &
 	IPC_PID=$!
 
-	pktio_ipc1${EXEEXT} -p ${IPC_PID}  -s ${STARTTIME} -t ${RUNTIME2}
+	pktio_ipc1${EXEEXT} -p ${IPC_PID}  -s ${STARTTIME} -t ${RUNTIME}
 	ret=$?
-	# pktio_ipc2 do not exit on pktio_ipc1 disconnect
-	# wait until it exits cleanly
-	sleep ${TIMEOUT}
+
 	(kill ${IPC_PID} 2>&1 > /dev/null ) > /dev/null
 	if [ $? -eq 0 ]; then
+		wait $IPC_PID
 		echo "pktio_ipc2${EXEEXT} was killed"
 		ls -l /dev/shm/${UID}/odp* 2> /dev/null
 		rm -rf /dev/shm/${UID}/odp-${IPC_PID}* 2>&1 > /dev/null
