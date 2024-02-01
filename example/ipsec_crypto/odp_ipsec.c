773b62fb0fd31f6aa2d954d6e1d930e50f3f3269
Petri Savolainen <petri.savolainen@nokia.com>
2024-02-01 17:26:24 +0200

    example: ipsec: avoid variable name shadowing
    Fixes -Wshadow compiler warnings.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- example/ipsec_crypto/odp_ipsec.c
+++ example/ipsec_crypto/odp_ipsec.c
@@ -975,10 +975,10 @@ pkt_disposition_e do_ipsec_out_seq(odp_packet_t *pkt,
 		esp->seq_no = odp_cpu_to_be_32((*ctx->ipsec.esp_seq)++);
 	}
 	if (ctx->ipsec.tun_hdr_offset) {
-		odph_ipv4hdr_t *ip;
+		odph_ipv4hdr_t *ip_tun;
 
-		ip = (odph_ipv4hdr_t *)(ctx->ipsec.tun_hdr_offset + buf);
-		ip->id = odp_cpu_to_be_16((*ctx->ipsec.tun_hdr_id)++);
+		ip_tun = (odph_ipv4hdr_t *)(ctx->ipsec.tun_hdr_offset + buf);
+		ip_tun->id = odp_cpu_to_be_16((*ctx->ipsec.tun_hdr_id)++);
 	}
 
 	/* Issue crypto request */
