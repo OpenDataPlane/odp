8ec0a13b91f3803e4f6bcfa3f1161afecb1de5da
Matias Elo <matias.elo@nokia.com>
2024-02-01 09:30:55 +0200

    github_ci: improve checkpatch testing for push operations
    Previously, in case of push operations only the latest commit was tested to
    avoid issues with invalid commit ranges in force pushes. Read force push
    status from GitHub events and test full commit range with normal push
    operations.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>

--- scripts/ci-checkpatches.sh
+++ scripts/ci-checkpatches.sh
@@ -2,22 +2,14 @@
 
 PATCHES=$1
 echo "Run checkpatch for ${PATCHES}"
-# Generate patches provided with $1.
-# In case of force push and range is broken
-# validate only the latest commit if it's not merge commit.
+# Generate patches provided with $1. If commit range is not available validate
+# only the latest commit.
 
 if [ "$PATCHES" = "" ]; then
 	git format-patch -1 -M HEAD;
-	perl ./scripts/checkpatch.pl *.patch;
-	exit $?
+else
+	git format-patch ${PATCHES}
 fi
 
-git show --summary HEAD| grep -q '^Merge:';
-if [ $? -ne 0 ]; then
-	git format-patch -1 -M HEAD;
-	perl ./scripts/checkpatch.pl *.patch;
-	exit $?
-fi
-
-git format-patch ${PATCHES}
 perl ./scripts/checkpatch.pl *.patch;
+exit $?
