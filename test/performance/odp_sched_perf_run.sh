e38b3aa51dc566be5e6b55876b87a87abc36f3cf
Matias Elo <matias.elo@nokia.com>
2024-02-27 15:48:00 +0200

    test: sched_perf: run tests with multiple worker counts
    Improve odp_sched_perf test coverage by running the application with
    multiple core counts.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

--- test/performance/odp_sched_perf_run.sh
+++ test/performance/odp_sched_perf_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2021, Nokia
+# Copyright (c) 2021-2024, Nokia
 # All rights reserved.
 #
 # SPDX-License-Identifier:	BSD-3-Clause
@@ -8,26 +8,40 @@
 
 TEST_DIR="${TEST_DIR:-$(dirname $0)}"
 
-echo odp_sched_perf: buffer pool
-echo ===============================================
+run()
+{
+	# Maximum number of workers may be less than the number of available processors. One worker
+	# should be always available.
+	MAX_WORKERS=$(($(nproc) - 2))
+	if [ $MAX_WORKERS -lt 1 ]; then
+		MAX_WORKERS=1
+	fi
 
-$TEST_DIR/odp_sched_perf${EXEEXT} -p 0
+	if [ $MAX_WORKERS -lt $1 ]; then
+		echo "Not enough CPU cores (requested $1, available $MAX_WORKERS). Skipping test."
+	else
+		echo odp_sched_perf -p 0 -c $1
+		echo ===============================================
+		$TEST_DIR/odp_sched_perf${EXEEXT} -p 0 -c $1
+		RET_VAL=$?
+		if [ $RET_VAL -ne 0 ]; then
+			echo odp_sched_perf FAILED
+			exit $RET_VAL
+		fi
 
-RET_VAL=$?
-if [ $RET_VAL -ne 0 ]; then
-	echo odp_sched_perf -p 0: FAILED
-	exit $RET_VAL
-fi
+		echo odp_sched_perf -p 1 -c $1
+		echo ===============================================
+		$TEST_DIR/odp_sched_perf${EXEEXT} -p 1 -c $1
+		RET_VAL=$?
+		if [ $RET_VAL -ne 0 ]; then
+			echo odp_sched_perf FAILED
+			exit $RET_VAL
+		fi
+	fi
+}
 
-echo odp_sched_perf: packet pool
-echo ===============================================
-
-$TEST_DIR/odp_sched_perf${EXEEXT} -p 1
-
-RET_VAL=$?
-if [ $RET_VAL -ne 0 ]; then
-	echo odp_sched_perf -p 1: FAILED
-	exit $RET_VAL
-fi
+run 1
+run 2
+run 6
 
 exit 0
