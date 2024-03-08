cb920bc00f3725c7ec94f6ece15eb4baddd05182
Jere Leppänen <jere.leppanen@nokia.com>
2024-03-08 18:40:41 +0200

    linux-gen: tm: use new ODP_TM_NAME_LEN constant
    Use new ODP_TM_NAME_LEN constant as the maximum name length for
    traffic manager objects. This reduces the maximum name length by one
    character.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>

--- platform/linux-generic/include/odp_name_table_internal.h
+++ platform/linux-generic/include/odp_name_table_internal.h
@@ -10,6 +10,7 @@
 extern "C" {
 #endif
 
+#include <odp/api/traffic_mngr.h>
 #include <stdint.h>
 
 typedef enum {
@@ -31,7 +32,7 @@ typedef enum {
 typedef uint32_t _odp_int_name_t;
 #define ODP_INVALID_NAME   0
 
-#define _ODP_INT_NAME_LEN 32
+#define _ODP_INT_NAME_LEN (ODP_TM_NAME_LEN - 1)
 
 _odp_int_name_t _odp_int_name_tbl_add(const char *name,
 				      uint8_t     name_kind,
