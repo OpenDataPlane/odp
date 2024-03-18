85232ec4ad06e5343b2437478f6e94fd757662d6
Matias Elo <matias.elo@nokia.com>
2024-03-18 13:08:15 +0200

    helper: remove hash table API
    Remove unused hash table helper API.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>

--- helper/include/odp/helper/odph_api.h
+++ helper/include/odp/helper/odph_api.h
@@ -22,7 +22,6 @@ extern "C" {
 #include <odp/helper/chksum.h>
 #include <odp/helper/eth.h>
 #include <odp/helper/gtp.h>
-#include <odp/helper/odph_hashtable.h>
 #include <odp/helper/icmp.h>
 #include <odp/helper/igmp.h>
 #include <odp/helper/ip.h>
