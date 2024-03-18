e3147711485848a3e661625e878badac3321bde2
Matias Elo <matias.elo@nokia.com>
2024-03-18 13:13:38 +0200

    helper: remove IP lookup table API
    Remove unused IP lookup table helper API and related files.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

--- helper/include/odp/helper/odph_api.h
+++ helper/include/odp/helper/odph_api.h
@@ -28,7 +28,6 @@ extern "C" {
 #include <odp/helper/ipsec.h>
 #include <odp/helper/macros.h>
 #include <odp/helper/stress.h>
-#include <odp/helper/odph_iplookuptable.h>
 #include <odp/helper/sctp.h>
 #include <odp/helper/string.h>
 #include <odp/helper/strong_types.h>
