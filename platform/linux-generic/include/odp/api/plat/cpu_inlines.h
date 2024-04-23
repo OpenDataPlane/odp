e725b6895710502ed9a1acbe5ca89675eb301c25
Petri Savolainen <petri.savolainen@nokia.com>
2024-04-23 18:27:52 +0300

    api: cpu: add CPU data cache prefetch functions
    Added new, more specific data prefetch variants. Old prefetch
    functions are maintained but are roughly equivalent to L1 level
    prefetch functions. Moved functions into CPU header as these
    can be implemented as direct CPU instructions later.
    
    Signed-off-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- platform/linux-generic/include/odp/api/plat/cpu_inlines.h
+++ platform/linux-generic/include/odp/api/plat/cpu_inlines.h
@@ -17,11 +17,21 @@
 #ifndef _ODP_NO_INLINE
 	/* Inline functions by default */
 	#define _ODP_INLINE static inline
-	#define odp_cpu_pause             __odp_cpu_pause
-	#define odp_cpu_cycles            __odp_cpu_cycles
-	#define odp_cpu_cycles_max        __odp_cpu_cycles_max
-	#define odp_cpu_cycles_resolution __odp_cpu_cycles_resolution
-	#define odp_cpu_cycles_diff       __odp_cpu_cycles_diff
+	#define odp_cpu_pause               __odp_cpu_pause
+	#define odp_cpu_cycles              __odp_cpu_cycles
+	#define odp_cpu_cycles_max          __odp_cpu_cycles_max
+	#define odp_cpu_cycles_resolution   __odp_cpu_cycles_resolution
+	#define odp_cpu_cycles_diff         __odp_cpu_cycles_diff
+	#define odp_prefetch                __odp_prefetch
+	#define odp_prefetch_l1             __odp_prefetch_l1
+	#define odp_prefetch_l2             __odp_prefetch_l2
+	#define odp_prefetch_l3             __odp_prefetch_l3
+	#define odp_prefetch_store          __odp_prefetch_store
+	#define odp_prefetch_store_l1       __odp_prefetch_store_l1
+	#define odp_prefetch_store_l2       __odp_prefetch_store_l2
+	#define odp_prefetch_store_l3       __odp_prefetch_store_l3
+	#define odp_prefetch_strm_l1        __odp_prefetch_strm_l1
+	#define odp_prefetch_store_strm_l1  __odp_prefetch_store_strm_l1
 #else
 	#define _ODP_INLINE
 #endif
@@ -52,6 +62,62 @@ _ODP_INLINE uint64_t odp_cpu_cycles_diff(uint64_t c2, uint64_t c1)
 		return c2 - c1;
 
 	return c2 + (odp_cpu_cycles_max() - c1) + _odp_cpu_cycles_resolution();
+}
+
+_ODP_INLINE void odp_prefetch(const void *ptr)
+{
+	/*
+	 * __builtin_prefetch (const void *addr, rw, locality)
+	 *
+	 * rw 0..1       (0: read, 1: write)
+	 * locality 0..3 (0: don't leave to cache, 3: leave on all cache levels)
+	 */
+	__builtin_prefetch(ptr, 0, 3);
+}
+
+_ODP_INLINE void odp_prefetch_l1(const void *ptr)
+{
+	__builtin_prefetch(ptr, 0, 3);
+}
+
+_ODP_INLINE void odp_prefetch_l2(const void *ptr)
+{
+	__builtin_prefetch(ptr, 0, 2);
+}
+
+_ODP_INLINE void odp_prefetch_l3(const void *ptr)
+{
+	__builtin_prefetch(ptr, 0, 1);
+}
+
+_ODP_INLINE void odp_prefetch_store(const void *ptr)
+{
+	__builtin_prefetch(ptr, 1, 3);
+}
+
+_ODP_INLINE void odp_prefetch_store_l1(const void *ptr)
+{
+	__builtin_prefetch(ptr, 1, 3);
+}
+
+_ODP_INLINE void odp_prefetch_store_l2(const void *ptr)
+{
+	__builtin_prefetch(ptr, 1, 2);
+}
+
+_ODP_INLINE void odp_prefetch_store_l3(const void *ptr)
+{
+	__builtin_prefetch(ptr, 1, 1);
+}
+
+_ODP_INLINE void odp_prefetch_strm_l1(const void *ptr)
+{
+	__builtin_prefetch(ptr, 0, 0);
+}
+
+_ODP_INLINE void odp_prefetch_store_strm_l1(const void *ptr)
+{
+	__builtin_prefetch(ptr, 1, 0);
 }
 
 /** @endcond */
