0a2d0ff1093241e006b893f5d2d6bf6bd79ceabf
Jere Leppänen <jere.leppanen@nokia.com>
2024-07-24 15:13:26 +0300

    validation: chksum: avoid potential aliasing issues
    Use a pointer to may_alias type to prevent the compiler from making
    assumptions about aliasing.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>

--- test/validation/api/chksum/chksum.c
+++ test/validation/api/chksum/chksum.c
@@ -314,7 +314,8 @@ static void chksum_ones_complement_udp_long(void)
 static uint16_t chksum_rfc1071(const void *p, uint32_t len)
 {
 	uint32_t sum = 0;
-	const uint16_t *data = p;
+	typedef uint16_t ODP_ALIGNED(1) __attribute((__may_alias__)) una_ma_u16_t;
+	const una_ma_u16_t *data = p;
 
 	while (len > 1) {
 		sum += *data++;
