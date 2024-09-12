554042ec89abc901215cbe268af71d6d3a7130b7
Matias Elo <matias.elo@nokia.com>
2024-09-12 17:09:24 +0300

    linux-gen: buffer: make code platform agnostic
    Enable usage of buffer debug code with ODP-DPDK.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

--- platform/linux-generic/odp_buffer.c
+++ platform/linux-generic/odp_buffer.c
@@ -1,21 +1,21 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright (c) 2013-2018 Linaro Limited
- * Copyright (c) 2019-2022 Nokia
+ * Copyright (c) 2019-2024 Nokia
  */
 
 #include <odp/api/align.h>
 #include <odp/api/buffer.h>
 
 #include <odp/api/plat/buffer_inline_types.h>
+#include <odp/api/plat/strong_types.h>
 
-#include <odp_pool_internal.h>
 #include <odp_buffer_internal.h>
 #include <odp_debug_internal.h>
 #include <odp_string_internal.h>
 
-#include <string.h>
-#include <stdio.h>
 #include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
 
 #include <odp/visibility_begin.h>
 
@@ -28,7 +28,6 @@ const _odp_buffer_inline_offset_t _odp_buffer_inline_offset ODP_ALIGNED_CACHE = 
 
 void odp_buffer_print(odp_buffer_t buf)
 {
-	odp_buffer_hdr_t *hdr;
 	int len = 0;
 	int max_len = 512;
 	int n = max_len - 1;
@@ -39,19 +38,16 @@ void odp_buffer_print(odp_buffer_t buf)
 		return;
 	}
 
-	hdr = _odp_buf_hdr(buf);
-
 	len += _odp_snprint(&str[len], n - len, "Buffer info\n");
 	len += _odp_snprint(&str[len], n - len, "-----------\n");
 	len += _odp_snprint(&str[len], n - len, "  handle         0x%" PRIx64 "\n",
 			    odp_buffer_to_u64(buf));
-	len += _odp_snprint(&str[len], n - len, "  pool index     %u\n", hdr->event_hdr.index.pool);
-	len += _odp_snprint(&str[len], n - len, "  buffer index   %u\n",
-			    hdr->event_hdr.index.event);
-	len += _odp_snprint(&str[len], n - len, "  addr           %p\n",
-			    (void *)hdr->event_hdr.base_data);
+	len += _odp_snprint(&str[len], n - len, "  pool index     %u\n",
+			    odp_pool_index(odp_buffer_pool(buf)));
+	len += _odp_snprint(&str[len], n - len, "  buffer index   %u\n", _odp_buffer_index(buf));
+	len += _odp_snprint(&str[len], n - len, "  addr           %p\n", odp_buffer_addr(buf));
 	len += _odp_snprint(&str[len], n - len, "  size           %u\n", odp_buffer_size(buf));
-	len += _odp_snprint(&str[len], n - len, "  user area      %p\n", hdr->uarea_addr);
+	len += _odp_snprint(&str[len], n - len, "  user area      %p\n", odp_buffer_user_area(buf));
 	str[len] = 0;
 
 	_ODP_PRINT("%s\n", str);
