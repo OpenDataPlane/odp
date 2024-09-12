57464a02cd65aef41d23cf4dda757653b7f2840b
Matias Elo <matias.elo@nokia.com>
2024-09-12 17:25:44 +0300

    linux-gen: event: make code platform agnostic
    Enable usage of event code with ODP-DPDK.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Jere Leppänen <jere.leppanen@nokia.com>

---
+++ platform/linux-generic/odp_event_offsets.c
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2015-2018 Linaro Limited
+ * Copyright (c) 2020-2024 Nokia
+ */
+
+#include <odp_event_internal.h>
+
+#include <odp/api/plat/event_inline_types.h>
+
+#include <odp/visibility_begin.h>
+
+/* Fill in event header field offsets for inline functions */
+const _odp_event_inline_offset_t
+_odp_event_inline_offset ODP_ALIGNED_CACHE = {
+	.event_type = offsetof(_odp_event_hdr_t, event_type),
+	.base_data  = offsetof(_odp_event_hdr_t, base_data),
+	.subtype    = offsetof(_odp_event_hdr_t, subtype),
+	.flow_id    = offsetof(_odp_event_hdr_t, flow_id),
+	.pool       = offsetof(_odp_event_hdr_t, pool),
+};
+
+#include <odp/visibility_end.h>
