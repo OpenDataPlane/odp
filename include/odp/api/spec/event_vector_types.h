fa1c47698a13602348d8a2fb8248db7d6b20b790
Matias Elo <matias.elo@nokia.com>
2024-09-12 10:11:56 +0300

    api: event: add generic event vectors
    Add new generic event vectors that can contain events of any type,
    as opposed to the existing packet vectors, which can contain only
    packets.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>
    Acked-by: Pavan Nikhilesh <pbhagavatula@marvell.com>

---
+++ include/odp/api/spec/event_vector_types.h
@@ -0,0 +1,45 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2024-2025 Nokia
+ */
+
+/**
+ * @file
+ *
+ * ODP event vector API type definitions
+ */
+
+#ifndef ODP_API_SPEC_EVENT_VECTOR_TYPES_H_
+#define ODP_API_SPEC_EVENT_VECTOR_TYPES_H_
+#include <odp/visibility_begin.h>
+
+#include <odp/api/event_types.h>
+#include <odp/api/std_types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @defgroup odp_event_vector ODP EVENT VECTOR
+ *  @{
+ */
+
+/**
+ * @typedef odp_event_vector_t
+ * ODP event vector
+ */
+
+/**
+ * @def ODP_EVENT_VECTOR_INVALID
+ * Invalid event vector
+ */
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <odp/visibility_end.h>
+#endif
