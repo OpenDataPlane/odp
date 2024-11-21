464db937c662cac93946a3dc878c172a503a0f5b
Matias Elo <matias.elo@nokia.com>
2024-11-21 16:13:41 +0200

    api: pktio: explicitly deprecate odp_pktout_config_opt_t.bit.tx_compl_ena
    Explicitly deprecate odp_pktout_config_opt_t.bit.tx_compl_ena field.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- platform/linux-generic/pktio/ipc.c
+++ platform/linux-generic/pktio/ipc.c
@@ -922,8 +922,8 @@ static int ipc_capability(pktio_entry_t *pktio_entry ODP_UNUSED, odp_pktio_capab
 
 	capa->max_input_queues  = 1;
 	capa->max_output_queues = 1;
-	capa->config.pktout.bit.tx_compl_ena = 1;
 #if ODP_DEPRECATED_API
+	capa->config.pktout.bit.tx_compl_ena = 1;
 	capa->tx_compl.mode_all = 1;
 #endif
 	capa->tx_compl.mode_event = 1;
--- platform/linux-generic/pktio/socket.c
+++ platform/linux-generic/pktio/socket.c
@@ -586,8 +586,8 @@ static int sock_capability(pktio_entry_t *pktio_entry,
 	capa->config.pktin.bit.ts_ptp = 1;
 
 	capa->config.pktout.bit.ts_ena = 1;
-	capa->config.pktout.bit.tx_compl_ena = 1;
 #if ODP_DEPRECATED_API
+	capa->config.pktout.bit.tx_compl_ena = 1;
 	capa->tx_compl.mode_all = 1;
 #endif
 	capa->tx_compl.mode_event = 1;
