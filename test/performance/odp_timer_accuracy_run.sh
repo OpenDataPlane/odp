c1d59cd9f16ae83ea830e52e777819cdd0c31833
Jere Leppänen <jere.leppanen@nokia.com>
2025-01-24 18:03:03 +0200

    test: timer_accuracy_run: run all modes
    Run all timer_accuracy modes, not just the default.
    
    Signed-off-by: Jere Leppänen <jere.leppanen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- test/performance/odp_timer_accuracy_run.sh
+++ test/performance/odp_timer_accuracy_run.sh
@@ -6,12 +6,16 @@
 
 TEST_DIR="${TEST_DIR:-$(dirname $0)}"
 
-$TEST_DIR/odp_timer_accuracy${EXEEXT} -p 100000000 -n 10
+for mode in 0 1 2 3 4; do
 
-RET_VAL=$?
-if [ $RET_VAL -ne 0 ] ; then
-    echo odp_timer_accuracy FAILED
-    exit $RET_VAL
-fi
+    $TEST_DIR/odp_timer_accuracy${EXEEXT} -m $mode -p 100000000 -n 10
+
+    RET_VAL=$?
+    if [ $RET_VAL -ne 0 ] ; then
+        echo "odp_timer_accuracy (mode $mode) FAILED"
+        exit $RET_VAL
+    fi
+
+done
 
 exit 0
