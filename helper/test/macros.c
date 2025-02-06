9a5fc68bc667aef9faadab8bf9a86aa7c4c4b463
Tuomas Taipale <tuomas.taipale@nokia.com>
2025-02-06 13:02:50 +0000

    helper: macros: add tests for new utility macros
    Add tests for new `ODPH_DIV_ROUNDUP()` and `ODPH_ROUNDUP_MULTIPLE()`
    macros.
    
    Signed-off-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- helper/test/macros.c
+++ helper/test/macros.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright(C) 2023 Nokia
+ * Copyright(C) 2023-2025 Nokia
  */
 
 #include <odp_api.h>
@@ -10,7 +10,7 @@
 
 int main(int argc ODP_UNUSED, char *argv[] ODP_UNUSED)
 {
-	int a, b;
+	int a, b, tmp;
 	int ret = 0;
 	int arr_1[1];
 	int arr_10[10];
@@ -60,6 +60,24 @@ int main(int argc ODP_UNUSED, char *argv[] ODP_UNUSED)
 
 	a = -1;
 	if (ODPH_ABS(--a) != 2)
+		ret++;
+
+	if (ODPH_DIV_ROUNDUP(1, 2) != 1)
+		ret++;
+
+	a = 128;
+	b = 74;
+	tmp = ODPH_DIV_ROUNDUP(a += 60, b -= 10);
+	if (tmp != 3)
+		ret++;
+
+	if (ODPH_ROUNDUP_MULTIPLE(24, 64) != 64)
+		ret++;
+
+	a = 128;
+	b = 74;
+	tmp = ODPH_ROUNDUP_MULTIPLE(a += 60, b -= 10);
+	if (tmp != 192)
 		ret++;
 
 	if (!ret)
