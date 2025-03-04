ca84e2b960c539e237236ddc6bdb1ae0a6ba5df6
Janne Peltonen <janne.peltonen@nokia.com>
2025-03-04 12:04:15 +0200

    api: event: add event aggregator queues within queues
    Introduce event aggregators that are associated with queues and have
    the same life time. Event aggregators are queues which try to aggregate
    multiple events into event vectors which are then enqueued to the
    underlying queue. There can be multiple event aggregators per queue.
    
    Add a function to get a queue handle for an event aggregator so that the
    queue handle can be used in most contexts where normal queue handles can
    be used. Dequeuing and certain queue metadata related actions are not
    possible with aggregator queues.
    
    Specify that event aggregators cannot be used simultaneously with the
    existing packet vector config in the same ODP application. The intention
    is to eventually deprecate the packet vector configuration as supporting
    the two aggregation mechanisms at the same time is redundant and may be
    difficult for ODP implementations.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>
    Acked-by: Pavan Nikhilesh <pbhagavatula@marvell.com>

--- include/odp/api/spec/event_vector_types.h
+++ include/odp/api/spec/event_vector_types.h
@@ -34,6 +34,86 @@ extern "C" {
  */
 
 /**
+ * Event vector configuration
+ */
+typedef struct odp_event_aggr_config_t {
+	/** Event vector pool
+	 *
+	 * Pool from which to allocate event vectors. The pool must have been
+	 * created with the ODP_POOL_EVENT_VECTOR type.
+	 */
+	odp_pool_t pool;
+
+	/** Maximum time to wait for events
+	 *
+	 * Maximum time in nanoseconds for an event aggregator to form an event
+	 * vector. This value should be in the range of
+	 * odp_event_aggr_capability_t::min_tmo_ns to
+	 * odp_event_aggr_capability_t::max_tmo_ns.
+	 *
+	 * Value of zero means there is no timeout. Events may wait aggregation
+	 * indefinitely in the aggregation queue.
+	 */
+	uint64_t max_tmo_ns;
+
+	/** Maximum number of events in vector
+	 *
+	 * Event aggregator forms an event vector event after 'max_size' events
+	 * have been collected or 'max_tmo_ns' has passed. 'max_size' value
+	 * should be in the range of odp_event_aggr_capability_t::min_size
+	 * to odp_event_aggr_capability_t::max_size.
+	 *
+	 * The maximum number of events an event vector can hold is defined by
+	 * odp_pool_param_t::event_vector.max_size of the event vector pool.
+	 * 'max_size' must not be greater than that value.
+	 */
+	uint32_t max_size;
+
+	/** Event type
+	 *
+	 * Event type of event aggregator. If 'event_type' is ODP_EVENT_ANY,
+	 * application is allowed to enqueue any event types, except event
+	 * vectors, to the event aggregator. Otherwise, only events of type
+	 * 'event_type' are allowed. The default value is ODP_EVENT_ANY.
+	 *
+	 * Regardless of 'event_type', an application is never allowed to
+	 * enqueue event vector or packet vector events (ODP_EVENT_VECTOR or
+	 * ODP_EVENT_PACKET_VECTOR) to an event aggregator queue
+	 * (i.e. vectors within vectors).
+	 */
+	odp_event_type_t event_type;
+
+} odp_event_aggr_config_t;
+
+/**
+ * Event aggregator capabilities
+ */
+typedef struct odp_event_aggr_capability_t {
+	/** Maximum number of event aggregators for this queue type */
+	uint32_t max_num;
+
+	/** Maximum number of event aggregators per queue */
+	uint32_t max_num_per_queue;
+
+	/** Maximum number of events that can be aggregated into an event vector */
+	uint32_t max_size;
+
+	/** Minimum number of events that can be aggregated into an event vector */
+	uint32_t min_size;
+
+	/** Maximum allowed value of odp_event_aggr_config_t::max_tmo_ns */
+	uint64_t max_tmo_ns;
+
+	/** Minimum time in nanoseconds for an aggregator to form an event vector.
+	 *
+	 *  odp_event_aggr_config_t::max_tmo_ns must not be less than this
+	 *  value unless it is zero.
+	 */
+	uint64_t min_tmo_ns;
+
+} odp_event_aggr_capability_t;
+
+/**
  * @}
  */
 
--- include/odp/api/spec/schedule_types.h
+++ include/odp/api/spec/schedule_types.h
@@ -13,6 +13,7 @@
 #define ODP_API_SPEC_SCHEDULE_TYPES_H_
 #include <odp/visibility_begin.h>
 
+#include <odp/api/event_vector_types.h>
 #include <odp/api/std_types.h>
 #include <odp/api/thrmask.h>
 
@@ -241,6 +242,9 @@ typedef struct odp_schedule_capability_t {
 	/** Order wait support. If not supported, odp_schedule_order_wait()
 	 *  does nothing. */
 	odp_support_t order_wait;
+
+	/** Event aggregator capabilities for scheduled queues */
+	odp_event_aggr_capability_t aggr;
 
 } odp_schedule_capability_t;
 
