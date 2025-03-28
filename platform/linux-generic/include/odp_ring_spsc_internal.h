508d6c16a4d806f0d646027be7b177317c1511a6
Matias Elo <matias.elo@nokia.com>
2025-03-28 17:39:06 +0200

    linux-gen: ring: add support for storing pointers in spsc rings
    Add support for storing void pointers in SPSC ring.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>

--- platform/linux-generic/include/odp_ring_spsc_internal.h
+++ platform/linux-generic/include/odp_ring_spsc_internal.h
@@ -1,20 +1,23 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright (c) 2018 Linaro Limited
+ * Copyright (c) 2025 Nokia
  */
 
 #ifndef ODP_RING_SPSC_INTERNAL_H_
 #define ODP_RING_SPSC_INTERNAL_H_
 
+#include <odp/api/atomic.h>
+
+#include <odp/api/plat/atomic_inlines.h>
+
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include <stdint.h>
-
-#include <odp/api/atomic.h>
-#include <odp/api/plat/atomic_inlines.h>
-
-/* Lock-free ring for single-producer / single-consumer usage.
+/**
+ * Lock-free single-producer single-consumer ring
  *
  * Thread doing an operation may be different each time, but the same operation
  * (enq- or dequeue) must not be called concurrently. The next thread may call
@@ -24,30 +27,75 @@ extern "C" {
  *
  * Enqueue and dequeue operations can be done concurrently.
  */
-typedef struct {
+
+struct ring_spsc_common {
 	odp_atomic_u32_t head;
 	odp_atomic_u32_t tail;
 
-} ring_spsc_t;
+};
+
+typedef struct {
+	struct ring_spsc_common r;
+} ring_spsc_u32_t;
+
+typedef struct {
+	struct ring_spsc_common r;
+} ring_spsc_ptr_t;
+
+#endif /* End of include guards */
+
+#undef _ring_spsc_gen_t
+#undef _ring_spsc_data_t
+#undef _RING_SPSC_INIT
+#undef _RING_SPSC_DEQ_MULTI
+#undef _RING_SPSC_ENQ_MULTI
+#undef _RING_SPSC_IS_EMPTY
+#undef _RING_SPSC_LEN
+
+/* This header should NOT be included directly. There are no include guards for
+ * the following types and function definitions! */
+#ifndef _ODP_RING_TYPE
+#error Include type specific ring header instead of this common file.
+#endif
+
+#if _ODP_RING_TYPE == _ODP_RING_TYPE_U32
+	#define _ring_spsc_gen_t	ring_spsc_u32_t
+	#define _ring_spsc_data_t	uint32_t
+
+	#define _RING_SPSC_INIT		ring_spsc_u32_init
+	#define _RING_SPSC_DEQ_MULTI	ring_spsc_u32_deq_multi
+	#define _RING_SPSC_ENQ_MULTI	ring_spsc_u32_enq_multi
+	#define _RING_SPSC_IS_EMPTY	ring_spsc_u32_is_empty
+	#define _RING_SPSC_LEN		ring_spsc_u32_len
+#elif _ODP_RING_TYPE == _ODP_RING_TYPE_PTR
+	#define _ring_spsc_gen_t	ring_spsc_ptr_t
+	#define _ring_spsc_data_t	uintptr_t
+
+	#define _RING_SPSC_INIT		ring_spsc_ptr_init
+	#define _RING_SPSC_DEQ_MULTI	ring_spsc_ptr_deq_multi
+	#define _RING_SPSC_ENQ_MULTI	ring_spsc_ptr_enq_multi
+	#define _RING_SPSC_IS_EMPTY	ring_spsc_ptr_is_empty
+	#define _RING_SPSC_LEN		ring_spsc_ptr_len
+#endif
 
 /* Initialize ring. Ring size must be a power of two. */
-static inline void ring_spsc_init(ring_spsc_t *ring)
+static inline void _RING_SPSC_INIT(_ring_spsc_gen_t *ring)
 {
-	odp_atomic_init_u32(&ring->head, 0);
-	odp_atomic_init_u32(&ring->tail, 0);
+	odp_atomic_init_u32(&ring->r.head, 0);
+	odp_atomic_init_u32(&ring->r.tail, 0);
 }
 
 /* Dequeue data from the ring head. Max_num is smaller than ring size.*/
-static inline uint32_t ring_spsc_deq_multi(ring_spsc_t *ring,
-					   uint32_t *ring_data,
-					   uint32_t ring_mask, uint32_t data[],
-					   uint32_t max_num)
+static inline uint32_t _RING_SPSC_DEQ_MULTI(_ring_spsc_gen_t *ring,
+					    _ring_spsc_data_t *ring_data,
+					    uint32_t ring_mask, _ring_spsc_data_t data[],
+					    uint32_t max_num)
 {
 	uint32_t head, tail, idx;
 	uint32_t num, i;
 
-	tail = odp_atomic_load_acq_u32(&ring->tail);
-	head = odp_atomic_load_u32(&ring->head);
+	tail = odp_atomic_load_acq_u32(&ring->r.tail);
+	head = odp_atomic_load_u32(&ring->r.head);
 	num  = tail - head;
 
 	/* Empty */
@@ -64,23 +112,23 @@ static inline uint32_t ring_spsc_deq_multi(ring_spsc_t *ring,
 		idx = (idx + 1) & ring_mask;
 	}
 
-	odp_atomic_store_rel_u32(&ring->head, head + num);
+	odp_atomic_store_rel_u32(&ring->r.head, head + num);
 
 	return num;
 }
 
 /* Enqueue data into the ring tail. Num_data is smaller than ring size. */
-static inline uint32_t ring_spsc_enq_multi(ring_spsc_t *ring,
-					   uint32_t *ring_data,
-					   uint32_t ring_mask,
-					   const uint32_t data[],
-					   uint32_t num_data)
+static inline uint32_t _RING_SPSC_ENQ_MULTI(_ring_spsc_gen_t *ring,
+					    _ring_spsc_data_t *ring_data,
+					    uint32_t ring_mask,
+					    const _ring_spsc_data_t data[],
+					    uint32_t num_data)
 {
 	uint32_t head, tail, size, idx;
 	uint32_t num, i;
 
-	head = odp_atomic_load_acq_u32(&ring->head);
-	tail = odp_atomic_load_u32(&ring->tail);
+	head = odp_atomic_load_acq_u32(&ring->r.head);
+	tail = odp_atomic_load_u32(&ring->r.tail);
 	size = ring_mask + 1;
 	num  = size - (tail - head);
 
@@ -98,31 +146,29 @@ static inline uint32_t ring_spsc_enq_multi(ring_spsc_t *ring,
 		idx = (idx + 1) & ring_mask;
 	}
 
-	odp_atomic_store_rel_u32(&ring->tail, tail + num);
+	odp_atomic_store_rel_u32(&ring->r.tail, tail + num);
 
 	return num;
 }
 
 /* Check if ring is empty */
-static inline int ring_spsc_is_empty(ring_spsc_t *ring)
+static inline int _RING_SPSC_IS_EMPTY(_ring_spsc_gen_t *ring)
 {
-	uint32_t head = odp_atomic_load_u32(&ring->head);
-	uint32_t tail = odp_atomic_load_u32(&ring->tail);
+	uint32_t head = odp_atomic_load_u32(&ring->r.head);
+	uint32_t tail = odp_atomic_load_u32(&ring->r.tail);
 
 	return head == tail;
 }
 
 /* Return current ring length */
-static inline uint32_t ring_spsc_length(ring_spsc_t *ring)
+static inline uint32_t _RING_SPSC_LEN(_ring_spsc_gen_t *ring)
 {
-	uint32_t head = odp_atomic_load_u32(&ring->head);
-	uint32_t tail = odp_atomic_load_u32(&ring->tail);
+	uint32_t head = odp_atomic_load_u32(&ring->r.head);
+	uint32_t tail = odp_atomic_load_u32(&ring->r.tail);
 
 	return tail - head;
 }
 
 #ifdef __cplusplus
 }
-#endif
-
 #endif
