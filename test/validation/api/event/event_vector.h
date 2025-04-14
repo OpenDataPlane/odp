37d423a5fa2f0a654531f355a014a669073048a7
Janne Peltonen <janne.peltonen@nokia.com>
2025-04-14 08:06:23 +0300

    validation: event: add event vector testing
    Add testing of event vector operations.
    
    Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

---
+++ test/validation/api/event/event_vector.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2025 Nokia
+ */
+
+#ifndef EVENT_VECTOR_H
+#define EVENT_VECTOR_H
+
+#include <odp_cunit_common.h>
+
+int evv_suite_init(void);
+int evv_suite_term(void);
+extern odp_testinfo_t evv_suite[];
+
+#endif
