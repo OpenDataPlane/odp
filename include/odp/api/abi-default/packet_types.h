594facd169f6935a37130aa73bda71870f12d7ff
Matias Elo <matias.elo@nokia.com>
2025-05-30 15:50:23 +0300

    api: packet: move odp_packet_parse_result_flag_t definition to API
    Move odp_packet_parse_result_flag_t struct definition from ABI to API
    header to guarantee application portability between different platforms.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>

--- include/odp/api/abi-default/packet_types.h
+++ include/odp/api/abi-default/packet_types.h
@@ -57,69 +57,6 @@ typedef enum {
 	ODP_PACKET_CHKSUM_OK
 } odp_packet_chksum_status_t;
 
-/** Parse result flags */
-typedef struct odp_packet_parse_result_flag_t {
-	/** Flags union */
-	union {
-		/** All flags as a 64 bit word */
-		uint64_t all;
-
-		/** Flags as a bitfield struct */
-		struct {
-			/** See odp_packet_has_error() */
-			uint64_t has_error    : 1;
-			/** See odp_packet_has_l2_error() */
-			uint64_t has_l2_error : 1;
-			/** See odp_packet_has_l3_error() */
-			uint64_t has_l3_error : 1;
-			/** See odp_packet_has_l4_error() */
-			uint64_t has_l4_error : 1;
-			/** See odp_packet_has_l2() */
-			uint64_t has_l2 : 1;
-			/** See odp_packet_has_l3() */
-			uint64_t has_l3 : 1;
-			/** See odp_packet_has_l4() */
-			uint64_t has_l4 : 1;
-			/** See odp_packet_has_eth() */
-			uint64_t has_eth : 1;
-			/** See odp_packet_has_eth_bcast() */
-			uint64_t has_eth_bcast : 1;
-			/** See odp_packet_has_eth_mcast() */
-			uint64_t has_eth_mcast : 1;
-			/** See odp_packet_has_jumbo() */
-			uint64_t has_jumbo : 1;
-			/** See odp_packet_has_vlan() */
-			uint64_t has_vlan : 1;
-			/** See odp_packet_has_vlan_qinq() */
-			uint64_t has_vlan_qinq : 1;
-			/** See odp_packet_has_arp() */
-			uint64_t has_arp : 1;
-			/** See odp_packet_has_ipv4() */
-			uint64_t has_ipv4 : 1;
-			/** See odp_packet_has_ipv6() */
-			uint64_t has_ipv6 : 1;
-			/** See odp_packet_has_ip_bcast() */
-			uint64_t has_ip_bcast : 1;
-			/** See odp_packet_has_ip_mcast() */
-			uint64_t has_ip_mcast : 1;
-			/** See odp_packet_has_ipfrag() */
-			uint64_t has_ipfrag : 1;
-			/** See odp_packet_has_ipopt() */
-			uint64_t has_ipopt : 1;
-			/** See odp_packet_has_ipsec() */
-			uint64_t has_ipsec : 1;
-			/** See odp_packet_has_udp() */
-			uint64_t has_udp : 1;
-			/** See odp_packet_has_tcp() */
-			uint64_t has_tcp : 1;
-			/** See odp_packet_has_sctp() */
-			uint64_t has_sctp : 1;
-			/** See odp_packet_has_icmp() */
-			uint64_t has_icmp : 1;
-		};
-	};
-
-} odp_packet_parse_result_flag_t;
 
 /**
  * @}
