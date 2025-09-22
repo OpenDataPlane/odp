9522a17c5612063c383c44dc15c3995080c8f660
Janne Peltonen <janne.peltonen@nokia.com>
2025-09-22 10:23:59 +0300

    api: barrier: clarify thread barrier reuse
    Explicitly state that a memory barrier can be reused without initializing
    it again.
    
    Make it clear that no more than N threads may be inside odp_barrier_wait()
    for a barrier of N threads. Without this requirement, an application could
    e.g. wait a barrier in thread 1 and thread 2, and, when thread 2 returns
    from the barrier (at which point thread 1 is supposed to have been
    unblocked too), tell threads 3 and 4 to wait for the same barrier. If
    threads 3 and 4 manage to start waiting for the barrier before thread 1
    has returned from odp_barrier_wait(), thread 1 may continue to be blocked
    in the counter based implementation used in multiple ODPs.
    
    Clarify that a barrier must be initialized before first use and can be
    reinitialized if not in use. Make also a remark about the thread-unsafety
    of the initialization function.
    
    Signed-off-by: Janne Peltonen <janne.peltonen@nokia.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>
    Reviewed-by: Petri Savolainen <petri.savolainen@nokia.com>
    Reviewed-by: Matias Elo <matias.elo@nokia.com>

--- include/odp/api/spec/barrier.h
+++ include/odp/api/spec/barrier.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright (c) 2013-2018 Linaro Limited
+ * Copyright (c) 2025 Nokia
  */
 
 /**
@@ -36,6 +37,10 @@ extern "C" {
 /**
  * Initialize barrier with thread count.
  *
+ * This function must not be called by multiple threads simultaneously for
+ * the same barrier nor at the same time when any other thread is calling
+ * odp_barrier_wait() for the same barrier.
+ *
  * @param barr Pointer to a barrier variable
  * @param count Thread count
  */
@@ -43,10 +48,25 @@ void odp_barrier_init(odp_barrier_t *barr, int count);
 
 /**
  * Synchronize thread execution on barrier.
- * Wait for all threads to arrive at the barrier until they are let loose again.
+ *
+ * Wait for a number of threads to arrive at the barrier until they are
+ * let loose again.
+ *
  * Threads will block (spin) until the last thread has arrived at the barrier.
  * All memory operations before the odp_barrier_wait() call will be visible
  * to all threads when they leave the barrier.
+ *
+ * A barrier must be initialized using odp_barrier_init() before first use.
+ *
+ * A barrier can be reused without reinitializing it again. It is ok to call
+ * odp_barrier_wait() for the same barrier again immediately after returning
+ * from the previous odp_barrier_wait() even if other threads that were waiting
+ * for the barrier have not yet returned from odp_barrier_wait().
+ *
+ * If a barrier is used by more threads than the thread count the barrier was
+ * initialized with, the number of threads that have called odp_barrier_wait()
+ * but not yet returned from it must never exceed the thread count the barrier
+ * was initialized with.
  *
  * @param barr Pointer to a barrier variable
  */
