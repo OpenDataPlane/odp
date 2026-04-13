140fd68e3bf85ac73496bfcb434a55a3493a3497
Matias Elo <matias.elo@nokia.com>
2026-04-13 10:08:33 +0300

    linux-gen: dpdk: make pktio stats code platform agnostic
    Modify DPDK pktio statistics code so that it can be shared with ODP-DPDK.
    rte_stats.rx_nombuf value is now included in odp_pktio_stats_t.in_discards
    and error checks have been improved.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

--- platform/linux-generic/pktio/dpdk_common.c
+++ platform/linux-generic/pktio/dpdk_common.c
@@ -9,13 +9,18 @@
 
 #include <odp_posix_extensions.h>
 
+#include <odp/api/hints.h>
 #include <odp/api/packet_io.h>
 
+#include <odp_debug_internal.h>
 #include <odp_dpdk_common.h>
+#include <odp_string_internal.h>
 
 #include <rte_ethdev.h>
+#include <rte_version.h>
 
 #include <ctype.h>
+#include <errno.h>
 
 int _odp_dpdk_netdev_is_valid(const char *s)
 {
@@ -49,6 +54,198 @@ void _odp_dpdk_hash_proto_to_rss_conf(struct rte_eth_rss_conf *rss_conf,
 				    RTE_ETH_RSS_IPV6_EX;
 	rss_conf->rss_key = NULL;
 }
+
+int _odp_dpdk_stats_common(uint16_t port_id, odp_pktio_stats_t *stats)
+{
+	int ret;
+	struct rte_eth_stats rte_stats;
+
+	ret = rte_eth_stats_get(port_id, &rte_stats);
+	if (odp_unlikely(ret)) {
+		_ODP_ERR("rte_eth_stats_get() failed: %d\n", ret);
+		return -1;
+	}
+
+	stats->in_octets = rte_stats.ibytes;
+	stats->in_packets = rte_stats.ipackets;
+	stats->in_ucast_pkts = 0;
+	stats->in_mcast_pkts = 0;
+	stats->in_bcast_pkts = 0;
+	stats->in_discards = rte_stats.imissed + rte_stats.rx_nombuf;
+	stats->in_errors = rte_stats.ierrors;
+	stats->out_octets = rte_stats.obytes;
+	stats->out_packets = rte_stats.opackets;
+	stats->out_ucast_pkts = 0;
+	stats->out_mcast_pkts = 0;
+	stats->out_bcast_pkts = 0;
+	stats->out_discards = 0;
+	stats->out_errors = rte_stats.oerrors;
+
+	return 0;
+}
+
+int _odp_dpdk_stats_reset_common(uint16_t port_id)
+{
+	int ret;
+
+	ret = rte_eth_stats_reset(port_id);
+	if (ret) {
+		if (ret == -ENOTSUP) {
+			_ODP_DBG("rte_eth_stats_reset() not supported\n");
+		} else {
+			_ODP_ERR("rte_eth_stats_reset() failed: %d\n", ret);
+			return -1;
+		}
+	}
+
+	ret = rte_eth_xstats_reset(port_id);
+	if (ret) {
+		if (ret == -ENOTSUP) {
+			_ODP_DBG("rte_eth_xstats_reset() not supported\n");
+		} else {
+			_ODP_ERR("rte_eth_xstats_reset() failed: %d\n", ret);
+			return -1;
+		}
+	}
+	return 0;
+}
+
+int _odp_dpdk_extra_stat_info_common(uint16_t port_id, odp_pktio_extra_stat_info_t info[], int num)
+{
+	int num_stats, ret, i;
+
+	num_stats = rte_eth_xstats_get_names(port_id, NULL, 0);
+	if (num_stats < 0) {
+		_ODP_ERR("rte_eth_xstats_get_names() failed: %d\n", num_stats);
+		return num_stats;
+	} else if (info == NULL || num == 0 || num_stats == 0) {
+		return num_stats;
+	}
+
+	struct rte_eth_xstat_name xstats_names[num_stats];
+
+	ret = rte_eth_xstats_get_names(port_id, xstats_names, num_stats);
+	if (ret < 0 || ret > num_stats) {
+		_ODP_ERR("rte_eth_xstats_get_names() failed: %d\n", ret);
+		return -1;
+	}
+	num_stats = ret;
+
+	for (i = 0; i < num && i < num_stats; i++)
+		_odp_strcpy(info[i].name, xstats_names[i].name, ODP_PKTIO_STATS_EXTRA_NAME_LEN);
+
+	return num_stats;
+}
+
+int _odp_dpdk_extra_stats_common(uint16_t port_id, uint64_t stats[], int num)
+{
+	int num_stats, ret, i;
+
+	num_stats = rte_eth_xstats_get(port_id, NULL, 0);
+	if (num_stats < 0) {
+		_ODP_ERR("rte_eth_xstats_get() failed: %d\n", num_stats);
+		return num_stats;
+	} else if (stats == NULL || num == 0 || num_stats == 0) {
+		return num_stats;
+	}
+
+	struct rte_eth_xstat xstats[num_stats];
+
+	ret = rte_eth_xstats_get(port_id, xstats, num_stats);
+	if (ret < 0 || ret > num_stats) {
+		_ODP_ERR("rte_eth_xstats_get() failed: %d\n", ret);
+		return -1;
+	}
+	num_stats = ret;
+
+	for (i = 0; i < num && i < num_stats; i++)
+		stats[i] = xstats[i].value;
+
+	return num_stats;
+}
+
+int _odp_dpdk_extra_stat_counter_common(uint16_t port_id, uint32_t id, uint64_t *stat)
+{
+	uint64_t xstat_id = id;
+	int ret;
+
+	ret = rte_eth_xstats_get_by_id(port_id, &xstat_id, stat, 1);
+	if (ret != 1) {
+		_ODP_ERR("rte_eth_xstats_get_by_id() failed: %d\n", ret);
+		return -1;
+	}
+
+	return 0;
+}
+
+#if RTE_VERSION < RTE_VERSION_NUM(25, 11, 0, 0)
+int _odp_dpdk_pktin_stats_common(uint16_t port_id, uint32_t index,
+				 odp_pktin_queue_stats_t *pktin_stats)
+{
+	struct rte_eth_stats rte_stats;
+	int ret;
+
+	if (odp_unlikely(index > RTE_ETHDEV_QUEUE_STAT_CNTRS - 1)) {
+		_ODP_ERR("DPDK supports max %d per queue counters\n", RTE_ETHDEV_QUEUE_STAT_CNTRS);
+		return -1;
+	}
+
+	ret = rte_eth_stats_get(port_id, &rte_stats);
+	if (odp_unlikely(ret)) {
+		_ODP_ERR("rte_eth_stats_get() failed: %d\n", ret);
+		return -1;
+	}
+
+	memset(pktin_stats, 0, sizeof(odp_pktin_queue_stats_t));
+
+	pktin_stats->packets = rte_stats.q_ipackets[index];
+	pktin_stats->octets = rte_stats.q_ibytes[index];
+	pktin_stats->errors = rte_stats.q_errors[index];
+
+	return 0;
+}
+#else
+int _odp_dpdk_pktin_stats_common(uint16_t port_id ODP_UNUSED, uint32_t index ODP_UNUSED,
+				 odp_pktin_queue_stats_t *pktin_stats)
+{
+	memset(pktin_stats, 0, sizeof(*pktin_stats));
+	return 0;
+}
+#endif
+
+#if RTE_VERSION < RTE_VERSION_NUM(25, 11, 0, 0)
+int _odp_dpdk_pktout_stats_common(uint16_t port_id, uint32_t index,
+				  odp_pktout_queue_stats_t *pktout_stats)
+{
+	struct rte_eth_stats rte_stats;
+	int ret;
+
+	if (odp_unlikely(index > RTE_ETHDEV_QUEUE_STAT_CNTRS - 1)) {
+		_ODP_ERR("DPDK supports max %d per queue counters\n", RTE_ETHDEV_QUEUE_STAT_CNTRS);
+		return -1;
+	}
+
+	ret = rte_eth_stats_get(port_id, &rte_stats);
+	if (odp_unlikely(ret)) {
+		_ODP_ERR("rte_eth_stats_get() failed: %d\n", ret);
+		return -1;
+	}
+
+	memset(pktout_stats, 0, sizeof(odp_pktout_queue_stats_t));
+
+	pktout_stats->packets = rte_stats.q_opackets[index];
+	pktout_stats->octets = rte_stats.q_obytes[index];
+
+	return 0;
+}
+#else
+int _odp_dpdk_pktout_stats_common(uint16_t port_id ODP_UNUSED, uint32_t index ODP_UNUSED,
+				  odp_pktout_queue_stats_t *pktout_stats)
+{
+	memset(pktout_stats, 0, sizeof(*pktout_stats));
+	return 0;
+}
+#endif
 
 #else
 /* Avoid warning about empty translation unit */
