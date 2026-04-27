9c0f50a2342f233b7365e3b8a4da5dbe160f8fec
Matias Elo <matias.elo@nokia.com>
2026-04-27 15:21:06 +0300

    linux-gen: time: optimize time conversions with multiply and shift
    Replace 64-bit divisions in _odp_time_to_ns() and _odp_time_from_ns()
    with precomputed multiply-and-shift operations using 128-bit
    intermediate values. Since the timer frequency is constant after init,
    the required constants are computed once during _odp_time_init_global() and
    used for all subsequent conversions. This eliminates two divisions and a
    branch from each conversion call.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>

--- platform/linux-generic/arch/common/odp/api/abi/time_cpu_inlines.h
+++ platform/linux-generic/arch/common/odp/api/abi/time_cpu_inlines.h
@@ -22,6 +22,10 @@ typedef struct _odp_time_global_t {
 	uint64_t freq_hz;
 	uint64_t start_time;
 	uint64_t start_time_ns;
+	uint64_t mult_to_ns;
+	uint64_t mult_from_ns;
+	uint32_t shift_to_ns;
+	uint32_t shift_from_ns;
 
 } _odp_time_global_t;
 
@@ -45,9 +49,14 @@ static inline odp_time_t _odp_time_cur_strict(void)
 
 static inline uint64_t _odp_time_to_ns(odp_time_t time)
 {
+	uint64_t count = time.count;
+
+#ifdef __SIZEOF_INT128__
+	return (uint64_t)(((__uint128_t)count * _odp_time_glob.mult_to_ns) >>
+			  _odp_time_glob.shift_to_ns);
+#else
 	uint64_t nsec;
 	uint64_t freq_hz = _odp_time_glob.freq_hz;
-	uint64_t count = time.count;
 	uint64_t sec = 0;
 
 	if (count >= freq_hz) {
@@ -58,12 +67,18 @@ static inline uint64_t _odp_time_to_ns(odp_time_t time)
 	nsec = (_ODP_TIME_GIGA_HZ * count) / freq_hz;
 
 	return (sec * _ODP_TIME_GIGA_HZ) + nsec;
+#endif
 }
 
 static inline odp_time_t _odp_time_from_ns(uint64_t ns)
 {
 	odp_time_t time;
 	uint64_t count;
+
+#ifdef __SIZEOF_INT128__
+	count = (uint64_t)(((__uint128_t)ns * _odp_time_glob.mult_from_ns) >>
+			   _odp_time_glob.shift_from_ns);
+#else
 	uint64_t freq_hz = _odp_time_glob.freq_hz;
 	uint64_t sec = 0;
 
@@ -74,7 +89,7 @@ static inline odp_time_t _odp_time_from_ns(uint64_t ns)
 
 	count  = sec * freq_hz;
 	count += (ns * freq_hz) / ODP_TIME_SEC_IN_NS;
-
+#endif
 	time.count = count;
 
 	return time;
