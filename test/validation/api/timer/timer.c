f7c104039e8447bd0102b91961856e9b4758513a
Tuomas Taipale <tuomas.taipale@nokia.com>
2026-05-29 12:28:54 +0000

    validation: timer: add tests for new fields of odp_timer_pool_info_t
    Add testing for the new 'freq_hz' and 'num' fields of
    `odp_timer_pool_info_t`.
    
    Signed-off-by: Tuomas Taipale <tuomas.taipale@nokia.com>

--- test/validation/api/timer/timer.c
+++ test/validation/api/timer/timer.c
@@ -3606,6 +3606,7 @@ static void timer_test_periodic(periodic_params_t *params, odp_queue_type_t queu
 {
 	odp_queue_param_t queue_param;
 	odp_timer_pool_t timer_pool;
+	odp_timer_pool_info_t info;
 	odp_timer_periodic_start_t start_param;
 	odp_queue_t queue;
 	odp_event_t ev = ODP_EVENT_INVALID;
@@ -3616,6 +3617,8 @@ static void timer_test_periodic(periodic_params_t *params, odp_queue_type_t queu
 	const char *user_ctx = "User context";
 	uarea_data_t data;
 	uint8_t *uarea;
+	uint32_t num_freq;
+	const odp_fract_u64_t *src;
 	int num_tmo;
 	int done;
 	const int num = 200;
@@ -3627,9 +3630,31 @@ static void timer_test_periodic(periodic_params_t *params, odp_queue_type_t queu
 		params->pool_param.priority = 0;
 
 	timer_pool = odp_timer_pool_create("periodic_timer", &params->pool_param);
-	CU_ASSERT_FATAL(timer_pool != ODP_TIMER_POOL_INVALID);
-	CU_ASSERT_FATAL(odp_timer_pool_start_multi(&timer_pool, 1) == 1);
 
+	CU_ASSERT_FATAL(timer_pool != ODP_TIMER_POOL_INVALID);
+	CU_ASSERT_FATAL(odp_timer_pool_info(timer_pool, &info) == 0);
+	CU_ASSERT(info.param.periodic.freq.freq_hz == NULL);
+	CU_ASSERT(info.param.periodic.freq.num == 0);
+
+	if (params->pool_param.timer_type == ODP_TIMER_TYPE_PERIODIC_FREQ) {
+		num_freq = params->pool_param.periodic.freq.num;
+		src = params->pool_param.periodic.freq.freq_hz;
+
+		CU_ASSERT(info.param.timer_type == ODP_TIMER_TYPE_PERIODIC_FREQ);
+		CU_ASSERT_FATAL(info.freq_hz != NULL);
+		CU_ASSERT(info.num_freq_hz == num_freq);
+
+		for (uint32_t i = 0; i < num_freq; i++) {
+			CU_ASSERT(info.freq_hz[i].integer == src[i].integer);
+			CU_ASSERT(info.freq_hz[i].numer == src[i].numer);
+			CU_ASSERT(info.freq_hz[i].denom == src[i].denom);
+		}
+	} else {
+		CU_ASSERT(info.freq_hz == NULL);
+		CU_ASSERT(info.num_freq_hz == 0);
+	}
+
+	CU_ASSERT_FATAL(odp_timer_pool_start_multi(&timer_pool, 1) == 1);
 	odp_queue_param_init(&queue_param);
 
 	if (queue_type == ODP_QUEUE_TYPE_SCHED) {
