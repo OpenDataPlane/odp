8d76eaf0c829711563112f70de44867582c5dfe6
Matias Elo <matias.elo@nokia.com>
2026-06-03 14:33:19 +0300

    linux-gen: ring: add mpsc and spmc rst ring variants
    Add dedicated MPSC and SPMC restricted ring variants based on the MPMC
    restricted ring implementation. No functional changes to the current MPMC
    implementation.
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

--- platform/linux-generic/include/ring/odp_ring_spmc_rst_u64_internal.h
+++ platform/linux-generic/include/ring/odp_ring_spmc_rst_u64_internal.h
@@ -1,9 +1,9 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright (c) 2021 Nokia
+ * Copyright (c) 2026 Nokia
  */
 
-#ifndef ODP_RING_MPMC_RST_U64_INTERNAL_H_
-#define ODP_RING_MPMC_RST_U64_INTERNAL_H_
+#ifndef ODP_RING_SPMC_RST_U64_INTERNAL_H_
+#define ODP_RING_SPMC_RST_U64_INTERNAL_H_
 
 #include <ring/odp_ring_common.h>
 
@@ -11,7 +11,7 @@
 #define _ODP_RING_TYPE _ODP_RING_TYPE_U64
 
 #undef _ODP_RING_SYNC
-#define _ODP_RING_SYNC _ODP_RING_SYNC_MPMC
+#define _ODP_RING_SYNC _ODP_RING_SYNC_SPMC
 
 #include <ring/odp_ring_mpmc_rst_internal.h>
 
