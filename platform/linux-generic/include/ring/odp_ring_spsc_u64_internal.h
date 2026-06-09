d9c12fc5e5d973d6902cd672857e2649329943f7
Matias Elo <matias.elo@nokia.com>
2026-06-09 10:14:23 +0300

    linux-gen: ring: add uint64_t spsc ring variant
    Add uint64_t variant of SPSC ring. Also, add batch variants of enqueue and
    dequeue functions, and remove deprecated comments about 'max_num' having to
    be smaller than ring size
    
    Signed-off-by: Matias Elo <matias.elo@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

---
+++ platform/linux-generic/include/ring/odp_ring_spsc_u64_internal.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2026 Nokia
+ */
+
+#ifndef ODP_RING_SPSC_U64_INTERNAL_H_
+#define ODP_RING_SPSC_U64_INTERNAL_H_
+
+#include <ring/odp_ring_common.h>
+
+#undef _ODP_RING_TYPE
+#define _ODP_RING_TYPE _ODP_RING_TYPE_U64
+
+#include <ring/odp_ring_spsc_internal.h>
+
+#endif
