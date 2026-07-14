679adbd29189098516b1e09224e1735d7143e087
Tuomas Taipale <tuomas.taipale@nokia.com>
2026-07-14 12:27:26 +0000

    test: pipeline: split config header into public and internal
    Move `config_parser_get()`, used to query parsed resources such as
    queues or pools, into a new public header `odp_pipeline_config.h` and
    keep the parser registration and driving interface in the internal
    `config_parser.h`. Also move the resource domain constants into a new
    public header `odp_pipeline_domains.h`, included from
    `odp_pipeline_config.h`, so they are visible to loadable libraries.
    
    Signed-off-by: Tuomas Taipale <tuomas.taipale@nokia.com>
    Reviewed-by: Janne Peltonen <janne.peltonen@nokia.com>

---
+++ test/performance/pipeline/odp_pipeline_domains.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2025 Nokia
+ */
+
+/** @cond _ODP_HIDE_FROM_DOXYGEN_ */
+
+#ifndef ODP_PIPELINE_DOMAINS_H_
+#define ODP_PIPELINE_DOMAINS_H_
+
+#define CLASSIFICATION_DOMAIN "classification"
+#define CPUMAP_DOMAIN "cpumap"
+#define CRYPTO_DOMAIN "crypto"
+#define DMA_DOMAIN "dma"
+#define FLOW_DOMAIN "flows"
+#define PKTIO_DOMAIN "pktios"
+#define POOL_DOMAIN "pools"
+#define QUEUE_DOMAIN "queues"
+#define SCHED_DOMAIN "scheduler"
+#define STASH_DOMAIN "stash"
+#define TIMER_DOMAIN "timers"
+#define WORKER_DOMAIN "workers"
+
+#endif
