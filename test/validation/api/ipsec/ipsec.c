e4782bb797739c3bd58e40fcb8135d2e5f4cdc80
Daphne Priscilla <df@marvell.com>
2026-08-12 09:37:26 +0530

    validation: ipsec: add AH test cases for HMAC-SM3
    Add test cases for inbound and outbound AH transport mode
    using the HMAC-SM3 algorithm.
    
    Signed-off-by: Daphne Priscilla <df@marvell.com>
    Signed-off-by: Nithinsen Kaithakadan <nkaithakadan@marvell.com>

--- test/validation/api/ipsec/ipsec.c
+++ test/validation/api/ipsec/ipsec.c
@@ -384,6 +384,11 @@ int ipsec_check_esp_sm4_cbc_sm3(void)
 				ODP_AUTH_ALG_SM3_HMAC, 256);
 }
 
+int ipsec_check_ah_sm3(void)
+{
+	return ipsec_check_ah(ODP_AUTH_ALG_SM3_HMAC, 256);
+}
+
 int ipsec_check_out_ipv4_ah_sha256_frag_check(void)
 {
 	if (!capa.out_op.opt.frag_mode || ipsec_check_ah_sha256() == ODP_TEST_INACTIVE)
--- test/validation/api/ipsec/ipsec.h
+++ test/validation/api/ipsec/ipsec.h
@@ -162,6 +162,7 @@ int ipsec_check_esp_aes_gcm_128_reass_ipv4(void);
 int ipsec_check_esp_aes_gcm_128_reass_ipv6(void);
 int ipsec_check_esp_null_aes_xcbc(void);
 int ipsec_check_esp_sm4_cbc_sm3(void);
+int ipsec_check_ah_sm3(void);
 int ipsec_check_out_ipv4_ah_sha256_frag_check(void);
 int ipsec_check_out_ipv4_esp_null_sha256_frag_check(void);
 int ipsec_check_out_dummy_esp_null_sha256(void);
